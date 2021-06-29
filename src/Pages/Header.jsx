878a87a31d5419ca28953d008c4a94e217e5d325
Cristian <cristian.bugs@gmail.com>
2021-06-29 18:29:00 -0300

    search bar

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,6 +1,7 @@
 import React, { Link } from 'react';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from '../components/SearchBar';
 
 const Header = () => (
   <div>
@@ -11,6 +12,8 @@ const Header = () => (
     <h1 data-testid="profile-title">Comidas</h1>
     <img src={ searchIcon } alt="profileIcon" data-testid="search-top-btn" />
     ;
+    <SearchBar />
   </div>
+
 );
 export default Header;
