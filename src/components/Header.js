291009368184eb66894564a62b239e4ef5a9e191
Roger Franco <rogerfrancogalt@gmail.com>
2021-06-29 18:09:45 -0400

    requisito 8

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <div>
+    <h1 data-testid="page-title">Header</h1>
+    <button data-testid="page-title">Profile</button>
+  </div>
+);
+
+export default Header;
