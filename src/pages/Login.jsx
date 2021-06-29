0f9fcdfbe429ddfadf9d1a12ba06acd2f9b5fd78
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-06-29 18:29:54 -0300

    finalizado PropTypes do Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
 import useValidation from '../hooks/useValidation';
 import { setOnLocalStorage } from '../services/helpers/localStorage';
 
-const Login = (props) => {
+const Login = ({ history }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { error, checkEmail, checkPassword } = useValidation();
@@ -16,7 +18,7 @@ const Login = (props) => {
     setOnLocalStorage('mealsToken', '1');
     setOnLocalStorage('cocktailsToken', '1');
     setOnLocalStorage('user', { email });
-    props.history.push('/comidas');
+    history.push('/comidas');
   };
   return (
     <div>
@@ -55,6 +57,12 @@ const Login = (props) => {
       </button>
     </div>
   );
+};
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
 };
 
 export default Login;
