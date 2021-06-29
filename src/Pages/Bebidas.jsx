f54097728ebed1acce4b131b66d34d220c9bd3ec
Alan Tanaka <alanctnk@gmail.com>
2021-06-29 20:03:36 -0300

    Componente na page de bebidas

--- src/Pages/Bebidas.jsx
+++ src/Pages/Bebidas.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Footer from '../Components/Footer';
+import Header from '../Components/Header';
 
 function Bebidas() {
   return (
     <div>
+      <Header title="Bebidas" />
       Bebidas
       <Footer />
     </div>
