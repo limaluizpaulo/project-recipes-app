8bdc7ea77a0a22531ed0f09e05a9cd20980eb60f
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-06-29 17:06:31 -0300

    requisito 9

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import searchIcon from '../images/searchIcon.svg';
+import profileIcon from '../images/profileIcon.svg';
+
+export default function Header({ title }) {
+  return (
+    <header>
+      <img src={ searchIcon } alt="search icon" data-testid="profile-top-btn" />
+      <span data-testid="page-title">{title}</span>
+      <img src={ profileIcon } alt="profile icon" data-testid="search-top-btn" />
+    </header>
+  );
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
