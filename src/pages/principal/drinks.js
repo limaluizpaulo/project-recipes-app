0be59715c10ab9d0571fe52ccb373df04f82adeb
Huggo Parcelly <h.parcelly@gmail.com>
2021-06-29 20:01:27 -0300

    adiciona req 09 e 10

---
+++ src/pages/principal/drinks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+function Principal() {
+  return (
+    <div>
+      <Header />
+      <p>Principal</p>
+    </div>
+  );
+}
+
+export default Principal;
