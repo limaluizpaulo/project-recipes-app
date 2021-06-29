b664507b77fb6ceb50d478f8e9c8ad20198c69d1
renatogsouza <renatomacedo1995@gmail.com>
2021-06-29 18:37:40 -0300

    menu-inferior completo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Switch from 'react-bootstrap/esm/Switch';
+import { Route } from 'react-router-dom';
 import Login from './Pages/Login';
+import Comidas from './Pages/Comidas';
+import Bebidas from './Pages/Bebidas';
+import Explore from './Pages/Explore';
 
 function App() {
   return (
     <div className="meals">
-      <Login />
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/comidas" component={ Comidas } />
+        <Route path="/bebidas" component={ Bebidas } />
+        <Route path="/explorar" component={ Explore } />
+      </Switch>
+
     </div>
   );
 }
---
+++ src/Pages/Comidas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../Components/Footer';
+
+function Comidas() {
+  return (
+    <div>
+      Comidas
+      <Footer />
+    </div>
+  );
+}
+
+export default Comidas;
