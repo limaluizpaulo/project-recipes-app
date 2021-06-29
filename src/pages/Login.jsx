db3fb51005acce6ca17d99583af8ddb91c62b627
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-06-29 17:31:38 -0300

    cria estados da pagina Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <section>
       <h1>Login</h1>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            value={ email }
+            data-testid="email-input"
+            type="email"
+            placeholder="Email"
+            onChange={ (event) => setEmail(event.target.value) }
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            value={ password }
+            data-testid="password-input"
+            type="password"
+            placeholder="Senha"
+            onChange={ (event) => setPassword(event.target.value) }
+          />
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
+        <Button data-testid="login-submit-btn" variant="primary" type="submit">
+          Entrar
         </Button>
       </Form>
     </section>
