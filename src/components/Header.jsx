33ea26696b0de64fbb2d145a783a441363c0e288
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-06-29 18:03:25 -0300

    requisito 12

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import searchIcon from '../images/searchIcon.svg';
 import profileIcon from '../images/profileIcon.svg';
+import SearchBar from './SearchBar';
 
 export default function Header({ title, search = false }) {
+  const [searchBar, setSearchBar] = useState(false);
   return (
-    <header>
-      <Link to="/perfil">
-        <img src={ profileIcon } alt="profile icon" data-testid="profile-top-btn" />
-      </Link>
-      <span data-testid="page-title">{title}</span>
-      {search
-      && <img src={ searchIcon } alt="search icon" data-testid="search-top-btn" />}
-    </header>
+    <>
+      <header>
+        <Link to="/perfil">
+          <img src={ profileIcon } alt="profile icon" data-testid="profile-top-btn" />
+        </Link>
+        <span data-testid="page-title">{title}</span>
+        {search
+      && (
+        <button type="button" onClick={ () => setSearchBar(!searchBar) }>
+          <img src={ searchIcon } alt="search icon" data-testid="search-top-btn" />
+        </button>
+      )}
+      </header>
+      {searchBar && <SearchBar /> }
+    </>
   );
 }
 
