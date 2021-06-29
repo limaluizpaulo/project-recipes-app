bf55ec8b5a493a171cc9c02f43f6b6ac7059e011
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-06-29 17:26:01 -0300

    header em recipes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types';
 import searchIcon from '../images/searchIcon.svg';
 import profileIcon from '../images/profileIcon.svg';
 
-export default function Header({ title }) {
+export default function Header({ title, search = false }) {
   return (
     <header>
       <img src={ searchIcon } alt="search icon" data-testid="profile-top-btn" />
       <span data-testid="page-title">{title}</span>
-      <img src={ profileIcon } alt="profile icon" data-testid="search-top-btn" />
+      {search
+      && <img src={ profileIcon } alt="profile icon" data-testid="search-top-btn" />}
     </header>
   );
 }
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+  search: PropTypes.bool.isRequired,
 };
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default function Recipes() {
+  const { pathname } = useHistory().location;
+  const [title, setTitle] = useState('');
+  useEffect(() => {
+    if (pathname.includes('comidas')) setTitle('Comidas');
+    if (pathname.includes('bebidas')) setTitle('Bebidas');
+  }, [pathname]);
   return (
     <div>
-      <Header />
+      <Header title={ title } search />
       <h1>receita</h1>
     </div>
   );
