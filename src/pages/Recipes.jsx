b6cfb20e9e0ea0737ef1337145af51eed8a2c95c
Antenor <antenorzapata@gmail.com>
2021-06-29 19:54:48 -0300

    requisito 16

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
+import { useMealsContext } from '../context/mealsContext';
+import { useDrinksContext } from '../context/drinksContext';
 
 export default function Recipes() {
   const { pathname } = useHistory().location;
   const [title, setTitle] = useState('');
+  const { drinksFiltered } = useDrinksContext();
+  const { mealsFiltered } = useMealsContext();
+
   useEffect(() => {
     if (pathname.includes('comidas')) setTitle('Comidas');
     if (pathname.includes('bebidas')) setTitle('Bebidas');
   }, [pathname]);
+
+  if (mealsFiltered.length === 1) {
+    return <Redirect to={ `/comidas/${mealsFiltered[0].idMeal}` } />;
+  } if (drinksFiltered.length === 1) {
+    return <Redirect to={ `/bebidas/${drinksFiltered[0].idDrink}` } />;
+  }
+
   return (
     <div>
       <Header title={ title } search />
