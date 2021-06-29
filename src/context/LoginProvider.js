437918cce8e00e333e5037b64e28d48bc8dc8e96
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-06-29 17:47:53 -0300

    cria estado global de login

--- src/context/LoginProvider.js
+++ src/context/LoginProvider.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import LoginContext from './LoginContext';
 
 function LoginProvider({ children }) {
+  const [login, setLogin] = useState('');
+
+  function successLogin(email) {
+    setLogin(email);
+  }
+
   return (
-    <LoginContext.Provider>
+    <LoginContext.Provider value={ { successLogin, login } }>
       { children }
     </LoginContext.Provider>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import LoginContext from '../context/LoginContext';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { successLogin } = useContext(LoginContext);
+
+  function handleSumit(event) {
+    event.preventDefault();
+    successLogin(email);
+
+    localStorage.setItem('user:', JSON.stringify({ email }));
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+  }
 
   return (
     <section>
       <h1>Login</h1>
-      <Form>
+      <Form onSubmit={ handleSumit }>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Control
             value={ email }
@@ -27,7 +38,11 @@ function Login() {
             onChange={ (event) => setPassword(event.target.value) }
           />
         </Form.Group>
-        <Button data-testid="login-submit-btn" variant="primary" type="submit">
+        <Button
+          data-testid="login-submit-btn"
+          variant="primary"
+          type="submit"
+        >
           Entrar
         </Button>
       </Form>
