08ee8e34b0c377a5398a5084af698172ca6b214f
Thiago Marchini <thiago@msn.com>
2021-06-29 17:44:21 -0300

    Adição das rotas, Context e Provider

---
+++ src/context/RecipesProvider.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import RecipesContext from './RecipesContext';
+
+function RecipesProvider({ children }) {
+  const [state, newState] = useState();
+  return (
+    <RecipesContext.Provider value={ { state, newState } }>
+      { children }
+    </RecipesContext.Provider>
+  );
+}
+
+RecipesProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default RecipesProvider;
