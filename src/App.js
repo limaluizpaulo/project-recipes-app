366278723e765c9fe21959d00d2798fdc32a76de
brunobastosfer <brunobastos@edu.unifor.br>
2021-06-29 17:42:17 -0300

    iniciando o requisito 25

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route } from 'react-router-dom';
+import Food from './components/Food';
+import Provider from './Provider/provider';
+import Drinks from './components/Drinks';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Provider>
+      <Router>
+        <Switch>
+          <Route path="/comidas" exact component={ Food } />
+          <Route path="/bebidas" exact component={ Drinks } />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/components/Drinks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Drinks() {
+  return (
+    <div>
+      <h1>bebidas</h1>
+    </div>
+  );
+}
+
+export default Drinks;
