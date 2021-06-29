536aa370fc8f85dedb61ba8a3d779b14de2bdb61
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-06-29 18:28:35 -0300

    finalizado a criaçao de um helper para adicionar coisas no localstorage, handleSubmit do login, adiçao de chaves no localstorage e entao um redirecionamento apos isso para /comidas

--- src/hooks/useValidation.jsx
+++ src/hooks/useValidation.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-
+// Hook costumizado que checa os erros e seta os erros
 const useValidation = () => {
   const [passwordError, setPasswordError] = useState(true);
   const [emailerror, setEmailError] = useState(true);
-
+  // Verifica se há mais de 6 char e altera o estado de passwordError
   const checkPassword = (password) => {
     const MIN_CHAR = 6;
     if (password.length > MIN_CHAR) {
@@ -12,7 +12,8 @@ const useValidation = () => {
       setPasswordError(true);
     }
   };
-
+  // Verifica se o email passa pelo regex basico onde é buscado algo antes de um @ algo entre o @ e um . e algo depois do .
+  // e então altera o valor do emailError
   const checkEmail = (email) => {
     const regex = /\S+@\S+\.\S+/;
     if (regex.test(email)) {
@@ -21,7 +22,7 @@ const useValidation = () => {
       setEmailError(true);
     }
   };
-
+  // Verifica se existe erro
   const error = passwordError || emailerror;
   return {
     checkPassword,
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import useValidation from '../hooks/useValidation';
+import { setOnLocalStorage } from '../services/helpers/localStorage';
 
-const Login = () => {
+const Login = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { error, checkEmail, checkPassword } = useValidation();
+  // Recebe uma callback para alterar o estado dos inputs "setter" outra para validar o input e o valor em questão
   const handleInputChange = (setter, checker, value) => {
     setter(value);
     checker(value);
+  };
+  // Função que cuida do submit, adiciona tokens no localStorage até então
+  const handleSubmit = () => {
+    setOnLocalStorage('mealsToken', '1');
+    setOnLocalStorage('cocktailsToken', '1');
+    setOnLocalStorage('user', { email });
+    props.history.push('/comidas');
   };
   return (
     <div>
@@ -16,7 +25,9 @@ const Login = () => {
         <input
           data-testid="email-input"
           value={ email }
-          onChange={ ({ target: { value } }) => handleInputChange(setEmail, checkEmail, value) }
+          onChange={ ({ target: { value } }) => handleInputChange(
+            setEmail, checkEmail, value,
+          ) }
           type="email"
           name="email"
           placeholder="Email"
@@ -26,13 +37,22 @@ const Login = () => {
         <input
           data-testid="password-input"
           value={ password }
-          onChange={ ({ target: { value } }) => handleInputChange(setPassword, checkPassword, value) }
+          onChange={ ({ target: { value } }) => handleInputChange(
+            setPassword, checkPassword, value,
+          ) }
           type="password"
           name="password"
           placeholder="Senha"
         />
       </div>
-      <button data-testid="login-submit-btn" disabled={ error } type="button">Entrar</button>
+      <button
+        data-testid="login-submit-btn"
+        disabled={ error }
+        onClick={ handleSubmit }
+        type="button"
+      >
+        Entrar
+      </button>
     </div>
   );
 };
