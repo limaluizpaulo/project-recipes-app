71aaf1deb642214a90a56c66b6cd32a65e036049
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2021-06-29 19:55:10 -0300

    eslint2

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-
 export default function ExploreDrinks() {
   return (
     <div>
