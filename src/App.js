0faf3067b3817ad4d4da7a4b1f59b6da1a416420
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-06-29 19:15:40 -0300

    FEAT: Rotas Criadas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { Switch, Route } from 'react-router';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" component="a" />
+      <Route exact path="/comidas" component="a" />
+      <Route exact path="/bebidas" component="a" />
+      <Route exact path="/comidas/{id-da-receita}" component="a" />
+      <Route exact path="/bebidas/{id-da-receita}" component="a" />
+      <Route exact path="/comidas/{id-da-receita}/in-progress" component="a" />
+      <Route exact path="/bebidas/{id-da-receita}/in-progress" component="a" />
+      <Route exact path="/explorar" component="a" />
+      <Route exact path="/explorar/comidas" component="a" />
+      <Route exact path="/explorar/bebidas" component="a" />
+      <Route exact path="/explorar/comidas/ingredientes" component="a" />
+      <Route exact path="/explorar/bebidas/ingredientes" component="a" />
+      <Route exact path="/explorar/comidas/area" component="a" />
+      <Route exact path="/perfil" component="a" />
+      <Route exact path="/receitas-feitas" component="a" />
+      <Route exact path="/receitas-favoritas" component="a" />
+    </Switch>
   );
 }
 
