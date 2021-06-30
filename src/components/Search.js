ee58b252fbf85449625fc6d0cd5b0197015b0f59
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-06-30 14:44:20 -0300

    Teste do input search aprovado

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Search() {
   return (
     <form>
-      a
+      <label htmlFor="search">
+        <input type="text" id="search" data-testid="search-input" />
+      </label>
+
     </form>
   );
 }
