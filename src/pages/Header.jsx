72317bf798c7a9e8541823dbf7eb2b54cc2ebd33
Matheus Rubi Schreiner <matheusrubisch@gmail.com>
2021-06-30 12:04:44 -0300

    Refatoraçao do Header

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,19 +1,28 @@
-import React, { Link } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
-import SearchBar from '../components/SearchBar';
 
-const Header = () => (
-  <div>
-    <Link to="/perfil">
-      <img src={ profileIcon } alt="profileIcon" data-testid="profile-top-btn" />
-      ;
-    </Link>
-    <h1 data-testid="profile-title">Comidas</h1>
-    <img src={ searchIcon } alt="profileIcon" data-testid="search-top-btn" />
-    ;
-    <SearchBar />
-  </div>
+const Header = ({ name, search }) => {
+  return (
+    <header>
+      <Link to="/perfil">
+        <img src={ profileIcon } alt="profileIcon" data-testid="profile-top-btn" />
+      </Link>
+      <h1 data-testid="profile-title">{ name }</h1>
+      { search && (
+        <Link to="/explorar">
+        <img src={ searchIcon } alt="profileIcon" data-testid="search-top-btn" />
+        </Link>
+      )}
+    </header>
+  )
+};
 
-);
+Header.propTypes = {
+  name: PropTypes.string,
+  search: PropTypes.bool,
+}.isRequired;
+
 export default Header;
