3fa98b6238727765ba2753762e2387dd1bec5b7c
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-06-30 15:56:46 -0300

    Inserindo foodCategtories e drinkCategories no rootReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import userLogin from './userLogin';
+import foodCategories from './foodCategories';
+import drinkCategories from './drinkCategories';
 
 const rootReducers = combineReducers({
   userLogin,
+  foodCategories,
+  drinkCategories,
 });
 
 export default rootReducers;
