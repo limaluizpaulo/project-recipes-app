cc2e000bd310b5f5eb5a58854f963378407dcb89
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-06-30 16:56:08 -0300

    cria SearchBar para comidas e outro para bebidas

--- src/components/SearchBarRecipes.jsx
+++ src/components/SearchBarRecipes.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
 import RecipesContext from '../context/RecipesContext';
+import FormSearchBar from './FormSearchBar';
 
 function SearchBar() {
   const [textFilter, setTextFilter] = useState('');
@@ -31,61 +31,13 @@ function SearchBar() {
   }
 
   return (
-    <Form onSubmit={ handleSubmit }>
-      {['radio'].map((type) => (
-        <div key={ `inline-${type}` } className="mb-3">
-          <Form.Group className="mb-3" controlId="formBasic">
-            <Form.Control
-              value={ textFilter }
-              data-testid="search-input"
-              type="text"
-              placeholder="busca"
-              name="checkbox"
-              onChange={ (event) => setTextFilter(event.target.value) }
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasic">
-
-            <Form.Check
-              value={ typeOfFilter }
-              data-testid="ingredient-search-radio"
-              onChange={ (event) => setTypeOfFilter(event.target.id) }
-              inline
-              id="ingredient"
-              label="Ingrediente"
-              name="checkbox"
-              type={ type }
-            />
-            <Form.Check
-              value={ typeOfFilter }
-              data-testid="name-search-radio"
-              inline
-              id="name"
-              label="Nome"
-              onChange={ (event) => setTypeOfFilter(event.target.id) }
-              name="checkbox"
-              type={ type }
-            />
-            <Form.Check
-              value={ typeOfFilter }
-              data-testid="first-letter-search-radio"
-              inline
-              id="first-letter"
-              label="Primeira letra"
-              onChange={ (event) => setTypeOfFilter(event.target.id) }
-              name="checkbox"
-              type={ type }
-            />
-          </Form.Group>
-        </div>
-      ))}
-      <Button
-        data-testid="exec-search-btn"
-        type="submit"
-      >
-        Buscar
-      </Button>
-    </Form>
+    <FormSearchBar
+      textFilter={ textFilter }
+      handleSubmit={ handleSubmit }
+      setTextFilter={ setTextFilter }
+      setTypeOfFilter={ setTypeOfFilter }
+      typeOfFilter={ typeOfFilter }
+    />
   );
 }
 
