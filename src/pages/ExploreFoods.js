661e2981acd303322a35b917b6b35b87fea5d75a
legalvao1 <legalvao1@gmail.com>
2021-06-29 21:44:55 -0300

    Pagina explorar comidas

---
+++ src/pages/ExploreFoods.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+class ExploreFoods extends Component {
+  render() {
+    return (
+      <section>
+        <Header title="Explorar Comidas" searchIcon />
+      </section>
+    );
+  }
+}
+
+export default ExploreFoods;
