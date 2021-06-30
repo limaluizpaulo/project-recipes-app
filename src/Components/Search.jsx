6a9d18d94baa39a722d73722ccf2ab1669154567
Carlos <cemargato@gmail.com>
2021-06-30 01:04:44 -0300

    acerto de lint

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -4,21 +4,42 @@ function Search() {
   return (
     <form>
       <label htmlFor="search">
-        <input id="search" type="text" name="search" placeholder='Buscar Receita' data-testid="search-input" />
+        <input
+          id="search"
+          type="text"
+          name="search"
+          placeholder="Buscar Receita"
+          data-testid="search-input"
+        />
       </label>
       &nbsp;
       <label htmlFor="ingredient">
-        <input id="ingredient" type="radio" name="" data-testid="ingredient-search-radio" />
+        <input
+          id="ingredient"
+          type="radio"
+          name="ingredient"
+          data-testid="ingredient-search-radio"
+        />
         Ingrediente
       </label>
       &nbsp;
       <label htmlFor="name">
-        <input id="name" type="radio" name="" data-testid="name-search-radio" />
+        <input
+          id="name"
+          type="radio"
+          name="name"
+          data-testid="name-search-radio"
+        />
         Nome
       </label>
       &nbsp;
       <label htmlFor="letter">
-        <input id="letter" type="radio" name="" data-testid="first-letter-search-radio" />
+        <input
+          id="letter"
+          type="radio"
+          name="letter"
+          data-testid="first-letter-search-radio"
+        />
         Primeira letra
       </label>
       &nbsp;
