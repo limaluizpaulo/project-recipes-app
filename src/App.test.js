1d700a975e0232d85e2cdf1601777d1282e158e2
Mariana Savoldi <marianasavodi123@gmail.com>
2021-06-30 16:57:24 -0300

    Tela de Login com testes completa

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// import RenderWithRouter from './RenderWithRouter';
 import App from './App';
+import Login from './components/Login';
 
-test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('testa tela de login', () => {
+  test('verifica a existência do input de email', () => {
+    const { getByPlaceholderText } = render(<Login />);
+    const inputEmail = getByPlaceholderText(/email/i); 
+    expect(inputEmail).toBeInTheDocument();
+  });
+
+  test('verifica a existência do input de senha', () => {
+    const { getByPlaceholderText } = render(<Login />);
+    const inputPassword = getByPlaceholderText(/senha/i); 
+    expect(inputPassword).toBeInTheDocument();
+  });
+
+  test('verifica a existência do botão Entrar', () => {
+    const { getByRole } = render(<Login />);
+    const btnEntrar = getByRole('button', {
+      name: /entrar/i,
+    }); 
+    expect(btnEntrar).toBeInTheDocument();
+  });
+
+  test('verifica se o texto digitado no input-email é um email válido', () => {
+    const { getByPlaceholderText, getByRole } = render(<Login />);
+    const inputEmail = getByPlaceholderText(/email/i);
+    userEvent.type(inputEmail, 'user@email.com');
+    expect(inputEmail).toHaveValue('user@email.com');;
+
+    const inputPassword = getByPlaceholderText(/senha/i);
+    userEvent.type(inputPassword, '123456');
+    expect(inputPassword).toHaveValue('123456');
+
+    const btnEntrar = getByRole('button', {
+      name: /entrar/i,
+    });
+    expect(btnEntrar).not.toBeDisabled();
+  });
 });
---
+++ src/components/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+function Login({ history }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisable, setIsDisable] = useState(true);
+
+  useEffect(() => {
+    const isValid = () => {
+      const validEmail = email.match(/[a-z]+@[a-z]+.com/g);
+      const minLength = 6;
+      const validPassword = password.length > minLength;
+      if (validEmail) {
+        if (validPassword) {
+          setIsDisable(false);
+        }
+      } else {
+        setIsDisable(true);
+      }
+    };
+    isValid();
+  }, [email, password, isDisable]);
+
+  const isRedirect = () => {
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/comidas');
+  };
+
+  return (
+    <form>
+      <input
+        type="email"
+        name="email-input"
+        placeholder="Email"
+        data-testid="email-input"
+        onChange={ ({ target }) => setEmail(target.value) }
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        data-testid="password-input"
+        onChange={ ({ target }) => setPassword(target.value) }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ isDisable }
+        onClick={ isRedirect }
+      >
+        Entrar
+      </button>
+    </form>
+  );
+}
+
+Login.propTypes = {
+  history: PropTypes.objectOf(PropTypes.objectOf).isRequired,
+};
+
+export default Login;
