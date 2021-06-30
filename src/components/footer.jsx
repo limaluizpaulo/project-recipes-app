6e4850f0fbadd344514eb91fd820f3d194db2128
elano3004 <elano3004@gmail.com>
2021-06-30 17:03:24 -0300

    Footer - Requisito 20

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import '../css/footer.css';
 
-class Fooder extends Component {
+class Footer extends Component {
   render() {
     return (
-      <section>
-        <fooder data-testid="footer">
+      <div>
+        <footer data-testid="footer" className="footer">
           <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="drinks" />
           <img data-testid="explore-bottom-btn" src={ exploreIcon } alt="explorar" />
           <img data-testid="food-bottom-btn" src={ mealIcon } alt="food" />
-        </fooder>
-      </section>
+        </footer>
+      </div>
     );
   }
 }
 
-export default Fooder;
+export default Footer;
--- src/pages/comidas.jsx
+++ src/pages/comidas.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/header';
-import Fooder from '../components/fooder';
+import Fooder from '../components/footer';
 
 class Comidas extends Component {
   render() {
