5c92fa967018a5a2180fd4b95915969f311e8001
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-06-30 19:32:53 -0300

    Header - incompleto

---
+++ src/pages/Drinks/ExploreIngredientsDrink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+function ExploreIngredientsDrink() {
+  return (
+    <div>
+      <Header title="Explorar Ingredientes" classname="displaynone" />
+    </div>
+  );
+}
+
+export default ExploreIngredientsDrink;
---
+++ src/pages/Foods/ExploreIngredientsFood.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+export default function ExploreIngredientsFood() {
+  return (
+    <div>
+      <Header title="Explorar Ingredientes" classname="displaynone" />
+    </div>
+  );
+}
