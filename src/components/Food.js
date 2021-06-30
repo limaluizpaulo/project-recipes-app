910471e513a12b33474922088127f15fe9bd3a49
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-06-30 17:09:13 -0300

    Requisito 19 feito

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from './Footer/Footer';
 
 function Food() {
   return (
-    <div>
-      <h1>Comidas</h1>
-    </div>
+    <>
+      <div>
+        <h1>Comidas</h1>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import drinkIcon from '../../images/drinkIcon.svg';
+import exploreIcon from '../../images/exploreIcon.svg';
+import mealIcon from '../../images/mealIcon.svg';
+
+function Footer() {
+  return (
+    <footer data-testid="footer">
+      <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="drink icon" />
+      <img data-testid="explore-bottom-btn" src={ exploreIcon } alt="explore icon" />
+      <img data-testid="food-bottom-btn" src={ mealIcon } alt="meal icon" />
+    </footer>
+  );
+}
+
+export default Footer;
