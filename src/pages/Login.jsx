75113cead2b67a562994020cc4e532fc9d2b601a
joao966 <joaooliveira966@gmail.com>
2021-06-30 01:58:20 -0300

    ADD: request 06/07/08

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       active: true,
+      redirect: false,
       email: '',
       password: '',
     };
     this.handleEmail = this.handleEmail.bind(this);
     this.handlePassword = this.handlePassword.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleEmail(e) {
@@ -34,13 +37,26 @@ class Login extends Component {
     }
   }
 
+  handleClick() {
+    const { email } = this.state;
+    localStorage.setItem('mealsToken', JSON.stringify(1));
+    localStorage.setItem('cocktailsToken', JSON.stringify(1));
+    localStorage.setItem('user', JSON.stringify({ email }));
+    this.setState({ redirect: true });
+  }
+
   render() {
-    const { active } = this.state;
+    const { active, redirect } = this.state;
+    if (redirect) return <Redirect to="/comidas" />;
     return (
       <div>
         <h2>Login</h2>
         <label htmlFor="email">
-          <input onChange={ this.handleEmail } type="text" data-testid="email-input" />
+          <input
+            onChange={ this.handleEmail }
+            type="text"
+            data-testid="email-input"
+          />
         </label>
         <label htmlFor="password">
           <input
@@ -53,6 +69,7 @@ class Login extends Component {
           disabled={ active }
           data-testid="login-submit-btn"
           type="button"
+          onClick={ this.handleClick }
         >
           Entrar
         </button>
