25bf6fc9b6fde702f99de1f57b4c3e26de48642d
Adão Benites <adaobjr@gmail.com>
2021-06-30 15:11:04 -0400

    fix package-lock.json

--- src/common/components/CategoryButton.jsx
+++ src/common/components/CategoryButton.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import store from '../../context/store';
+// import React from 'react';
+// import store from '../../context/store';
 
-export default function CategoryButton() {
-  const { recipes: { foods, categoriesMeals,
-    categoriesDrinks, categoriesLimit } } = useContext(store);
+// export default function CategoryButton() {
+//   const { recipes: { foods, categoriesMeals,
+//     categoriesDrinks, categoriesLimit } } = useContext(store);
 
-  const renderRecipes = () => {
-    const newCategories = (foods) ? (
-      categoriesMeals.slice(0, categoriesLimit)) : (
-      categoriesDrinks.slice(0, categoriesLimit));
+//   const renderRecipes = () => {
+//     const newCategories = (foods) ? (
+//       categoriesMeals.slice(0, categoriesLimit)) : (
+//       categoriesDrinks.slice(0, categoriesLimit));
 
-  return (
-    newCategories.map((category, index) => (
-      <button
-        type="button"
-        key={ index }
-        data-testid={ `${category}-category-filter` }
-      >
-        {category}
-      </button>
-    ))
-  );
-  }
+//   return (
+//     newCategories.map((category, index) => (
+//       <button
+//         type="button"
+//         key={ index }
+//         data-testid={ `${category}-category-filter` }
+//       >
+//         {category}
+//       </button>
+//     ))
+//   );
+//   }
