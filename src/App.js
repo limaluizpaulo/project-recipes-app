81d1b3505d79b38dccf71fff8d021603c0713ec5
DennisMarcelo <dennismarcelo15@gmail.com>
2021-06-30 18:59:49 -0300

    criação das rotas no app.js, só falta atribuir as paginas as rotas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ import Food from './pages/Food/Food';
 import Provider from './Provider/provider';
 import Drinks from './pages/Drinks/Drinks';
 import Login from './pages/Login/Login';
+import Explorar from './pages/Explorar/Explorar';
+import ExplorarComidas from './pages/ExplorarComidas/ExplorarComidas';
+import ExplorarBebidas from './pages/ExplorarBebidas/ExplorarBebidas';
 
+const test = '';
 function App() {
   return (
     <Provider>
@@ -16,7 +20,20 @@ function App() {
         <Switch>
           <Route path="/" exact component={ Login } />
           <Route path="/comidas" exact component={ Food } />
+          <Route path="/comidas/:id" exact component={ test } />
+          <Route path="/comidas/:id/in-progress" exact component={ test } />
           <Route path="/bebidas" exact component={ Drinks } />
+          <Route path="/bebidas/:id" exact component={ test } />
+          <Route path="/bebidas/:id/in-progress" exact component={ test } />
+          <Route path="/explorar" exact component={ Explorar } />
+          <Route path="/explorar/comidas" exact component={ ExplorarComidas } />
+          <Route path="/explorar/bebidas" exact component={ ExplorarBebidas } />
+          <Route path="/explorar/comidas/ingredientes" exact component={ test } />
+          <Route path="/explorar/bebidas/ingredientes" exact component={ test } />
+          <Route path="/explorar/comidas/area" exact component={ test } />
+          <Route path="/perfil" exact component={ test } />
+          <Route path="/receitas-feitas" exact component={ test } />
+          <Route path="/receitas-favoritas" exact component={ test } />
         </Switch>
       </Router>
     </Provider>
