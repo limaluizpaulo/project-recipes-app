1864b972606c6d7c65959bd516684e5884eaa9a7
tonpasso <wellpasso@gmail.com>
2021-06-30 16:03:31 -0300

    requisitos 19-24

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import Food from './pages/Food';
+import Drink from './pages/Drink';
+import Explore from './pages/Explore';
+import ExploreFoods from './pages/ExploreFoods';
+import ExploreDrinks from './pages/ExploreDrinks';
+import FoodIngredients from './pages/FoodIngredients';
+import Profile from './pages/Profile';
+import DrinkIngredients from './pages/DrinkIngredients';
+import FoodOrigin from './pages/FoodOrigin';
 
 function App() {
   return (
@@ -12,6 +20,20 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/comidas" component={ Food } />
+        <Route exact path="/bebidas" component={ Drink } />
+        <Route exact path="/explorar" component={ Explore } />
+        <Route exact path="/explorar/comidas" component={ ExploreFoods } />
+        <Route path="/perfil" component={ Profile } />
+        <Route exact path="/explorar/bebidas" component={ ExploreDrinks } />
+        <Route
+          path="/explorar/comidas/ingredientes"
+          component={ FoodIngredients }
+        />
+        <Route
+          path="/explorar/bebidas/ingredientes"
+          component={ DrinkIngredients }
+        />
+        <Route path="/explorar/comidas/area" component={ FoodOrigin } />
 
       </Switch>
 
---
+++ src/pages/Explore.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../components/Footer';
+// import Header from '../components/Header';
+
+export default function Explore() {
+  return (
+    <div>
+      {/* <Header /> */}
+      <h2>Explorar</h2>
+      <Footer />
+    </div>
+  );
+}
