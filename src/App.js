d0bbc584386a7cd8a5bdf6746cec8d10baa011e8
Fabio Marturano <fabiocmjor@gmail.com>
2021-06-30 15:25:21 -0300

    Inicio Telas Explore

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
 import RecipesProvider from './context/RecipesProvider';
-
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
