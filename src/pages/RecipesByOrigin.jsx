5e9d622f8e8a9e53df3e11e1b227c15098d4b8dd
Mariana Mohr <marianamohr@gmail.com>
2021-06-30 17:06:41 -0300

    req 09 ao 21

--- src/pages/RecipesByOrigin.jsx
+++ src/pages/RecipesByOrigin.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
+import DownMenu from '../components/DownMenu';
 
 class RecipesByOrigin extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h2> Recipes By Origin</h2>
+        <DownMenu />
       </div>
     );
   }
