b2bcb30ccb18ac54fb5493c2322d46fae5521f43
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-06-30 13:06:01 -0300

    Feat: Criado o botão para exibir o Search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function Header({ history, title }) {
+  const [searchInput, toggleSearch] = useState();
   const isHeaderPresent = () => {
     const { location: { pathname } } = history;
     if (pathname === '/comidas'
       || pathname === '/bebidas'
       || pathname === '/explorar/comidas/area') {
       return (
-        <img
-          src={ searchIcon }
-          alt="search icon"
-          data-testid="search-top-btn"
-        />
+        <button type="button" onClick={ () => toggleSearch(!searchInput) }>
+          <img
+            src={ searchIcon }
+            alt="search icon"
+            data-testid="search-top-btn"
+          />
+        </button>
       );
     }
     return '';
@@ -34,6 +37,14 @@ export default function Header({ history, title }) {
       <h1 data-testid="page-title">{ title }</h1>
       <div>
         { isHeaderPresent() }
+        { searchInput && (
+          (
+            <label htmlFor="search">
+              Explorar:
+              <input id="search" type="text" data-testid="search-input" />
+            </label>
+          )
+        )}
       </div>
     </div>
   );
