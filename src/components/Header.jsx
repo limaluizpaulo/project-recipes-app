59210f590022b5b6ab74c0238e8133389b7df2ab
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-06-30 12:47:25 -0300

    Feat: Redirecionamento para o perfil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
@@ -22,11 +23,13 @@ export default function Header({ history, title }) {
   return (
     <div>
       <div>
-        <img
-          src={ profileIcon }
-          alt="profile icon"
-          data-testid="profile-top-btn"
-        />
+        <Link to="/perfil">
+          <img
+            src={ profileIcon }
+            alt="profile icon"
+            data-testid="profile-top-btn"
+          />
+        </Link>
       </div>
       <h1 data-testid="page-title">{ title }</h1>
       <div>
