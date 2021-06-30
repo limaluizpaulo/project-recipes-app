f8b918437cee1d1478fb6837e9c4027c9864368e
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-06-30 19:30:30 -0300

    resolvendo problemas na renderização

--- src/pages/comidas.jsx
+++ src/pages/comidas.jsx
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-<<<<<<< HEAD
 import { connect } from 'react-redux';
 import Header from '../components/header';
 import { fetchApiFoodCategories } from '../action';
-=======
-import Header from '../components/header';
 import MainScreen from '../components/MainScreen';
-
 import Cards from '../components/cards';
 import Fooder from '../components/footer';
->>>>>>> c3b4e90ed38ed0a5e794fb15943eb765ca9ea69f
 
 class Comidas extends Component {
   constructor(props) {
@@ -22,14 +17,16 @@ class Comidas extends Component {
     this.categories = this.categories.bind(this);
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     const { apiFoodCategories } = this.props;
-    apiFoodCategories();
+    console.log(apiFoodCategories);
+    // await apiFoodCategories()
+    // .then((data) => this.setState({ foodCategories: data}))
   }
 
   categories() {
     const { getFoodCategories } = this.props;
-    const teste = getFoodCategories;
+    const teste = getFoodCategories.map((elem) => elem);
     console.log(teste);
     return (
 
@@ -41,49 +38,36 @@ class Comidas extends Component {
   }
 
   render() {
-<<<<<<< HEAD
     // const { foodCategories } = this.state;
     // console.log(foodCategories);
-    return (
-      <div>
-        <Header />
-        { this.categories() }
-=======
     const { location } = this.props;
     return (
       <div>
         <Header location={ location } />
         <main>
-          <Header />
           <MainScreen />
         </main>
->>>>>>> c3b4e90ed38ed0a5e794fb15943eb765ca9ea69f
         <p>qualquer coisa</p>
+        { this.categories }
         <Fooder />
       </div>
     );
   }
 }
 
-<<<<<<< HEAD
 const mapDispatchToProps = (dispatch) => ({
-  apiFoodCategories: () => dispatch(fetchApiFoodCategories()),
+  apiFoodCategories: dispatch(fetchApiFoodCategories()),
 });
 
 const mapStateToProps = (state) => ({
   getFoodCategories: state.foodCategories.allFoodCategories,
+
 });
 
 Comidas.propTypes = {
   apiFoodCategories: PropTypes.func,
   getFoodCategories: PropTypes.objectOf(PropTypes.object),
+  location: PropTypes.shape,
 }.isRequired;
 
 export default connect(mapStateToProps, mapDispatchToProps)(Comidas);
-=======
-Comidas.propTypes = {
-  location: PropTypes.shape.isRequired,
-};
-// export default Comidas;
-export default (Comidas);
->>>>>>> c3b4e90ed38ed0a5e794fb15943eb765ca9ea69f
