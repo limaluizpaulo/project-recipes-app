adb289f5b9300abf2046e98088a953bf2e5bde73
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-06-30 15:28:34 -0300

    Feat: useEffect que faz a chamada da api

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
+import requestApi from '../services/helpers/apiServises';
 
 const RecipeProvider = ({ children }) => {
-  const context = { teste: 'teste' };
+  const [data, setData] = useState([]);
+  const [radioValue, setValueRadio] = useState('');
+  const [inputValue, setInputRadio] = useState('');
+
+  useEffect(() => {
+    async function setDataIngredient() {
+      const response = await requestApi(radioValue, inputValue);
+      setData(response);
+    }
+    setDataIngredient();
+  }, [radioValue, inputValue]);
+
   return (
-    <Context.Provider value={ context }>
+    <Context.Provider value={ { data, setData, setValueRadio, setInputRadio } }>
       {children}
     </Context.Provider>
   );
