e558c5290d29217fa979e5eff0dedf4e0d3dac2c
Matheus H. Gois <matheus.hengo@gmail.com>
2021-06-30 16:12:37 -0300

    Replaced Link's text by icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// images
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
 function Footer() {
   return (
     <footer data-testid="footer">
       {/* TODO: Remove to="comidas" */}
-      <Link to="/comidas" data-testid="drinks-bottom-btn"> drinks </Link>
-      <Link to="/comidas" data-testid="explore-bottom-btn"> explore </Link>
-      <Link to="/comidas" data-testid="food-bottom-btn"> food </Link>
+      <Link to="/comidas" data-testid="drinks-bottom-btn">
+        <img src={ drinkIcon } alt="Drink Icon" />
+      </Link>
+      <Link to="/comidas" data-testid="explore-bottom-btn">
+        <img src={ exploreIcon } alt="Explore Icon" />
+      </Link>
+      <Link to="/comidas" data-testid="food-bottom-btn">
+        <img src={ mealIcon } alt="Meal Icon" />
+      </Link>
     </footer>
   );
 }
