e807734d2cceddf0b289529c20cb1db7b4d5d00a
legalvao1 <legalvao1@gmail.com>
2021-06-29 22:07:37 -0300

    pagina Explorar ingredientes das bebidas

---
+++ src/pages/ExploreDrinksIngredients.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+class ExploreDrinksIngredients extends Component {
+  render() {
+    return (
+      <section>
+        <Header title="Explorar Ingredientes" searchIcon />
+      </section>
+    );
+  }
+}
+
+export default ExploreDrinksIngredients;
