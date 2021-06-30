11041a8f05ad8d3aa6952e24a3d075fd274a8c25
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-06-29 22:13:24 -0300

    REQUISITO 19

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function Footer() {
+  return (
+    <div className="footerContainer" data-testid="footer">
+      <Link to="/bebidas">
+        <img src={ drinkIcon } alt="Drinks" data-testid="drinks-bottom-btn" />
+      </Link>
+      <Link to="/explorar">
+        <img src={ exploreIcon } alt="Explore" data-testid="explore-bottom-btn" />
+      </Link>
+      <Link to="/comidas">
+        <img src={ mealIcon } alt="Meals" data-testid="food-bottom-btn" />
+      </Link>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 const Meals = () => (
   <div>
     <h4>Meals</h4>
+    <Footer />
   </div>
 );
 
