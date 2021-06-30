846ff4e1041f6f11dbe25f37b871c7f05b0c851f
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-06-30 14:46:24 -0300

    Teste do checkbox para ingredientes passou

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,9 @@ function Search() {
       <label htmlFor="search">
         <input type="text" id="search" data-testid="search-input" />
       </label>
-
+      <label htmlFor="ingredients">
+        <input type="checkbox" id="ingredients" data-testid="ingredient-search-radio" />
+      </label>
     </form>
   );
 }
