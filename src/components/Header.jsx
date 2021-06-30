781c436af676553e379bd84138533973493366ee
Arlen Freitas <arlenfreitas1@gmail.com>
2021-06-30 16:12:47 -0300

    header e searchBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 function Header() {
+  const [displaySearch, setDisplaySearch] = useState(false);
+  const renderSearch = () => (
+    <button
+      type="button"
+      onClick={ () => setDisplaySearch(!displaySearch) }
+    >
+      <img
+        src={ searchIcon }
+        alt="Explore"
+        data-testid="search-top-btn"
+      />
+    </button>
+  );
+
   return (
-    <h1>Header</h1>
+    <div>
+      <Link to="/perfil">
+        <img
+          src={ profileIcon }
+          alt="Profile"
+          data-testid="profile-top-btn"
+        />
+      </Link>
+      <h1 data-testid="page-title">Comidas</h1>
+      { renderSearch() }
+      { displaySearch
+        ? (
+          <SearchBar />
+        )
+        : false}
+    </div>
   );
 }
 
