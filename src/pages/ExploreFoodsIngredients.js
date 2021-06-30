2f46abe172e22cf88075c08deff56147d250bea1
legalvao1 <legalvao1@gmail.com>
2021-06-29 22:07:52 -0300

    pagina Explorar ingredientes das comidas

---
+++ src/pages/ExploreFoodsIngredients.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+class ExploreFoodsIngredients extends Component {
+  render() {
+    return (
+      <section>
+        <Header title="Explorar Ingredientes" searchIcon />
+      </section>
+    );
+  }
+}
+
+export default ExploreFoodsIngredients;
