860088f4561d90ef286d045f79780d4bf24d8ab5
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-06-30 16:52:29 -0300

    Requirement 11: redirect to profile page when click on profile button

--- src/common/components/Header/Header.jsx
+++ src/common/components/Header/Header.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import profileIcon from '../../../images/profileIcon.svg';
 import searchIcon from '../../../images/searchIcon.svg';
 import './header.css';
 
 export default function Header() {
+  const [isRedirect, setIsRedirect] = useState(false);
+
+  if (isRedirect) return <Redirect to="/perfil" />;
+
   return (
     <header>
-
-      <img src={ profileIcon } alt="perfil" data-testid="profile-top-btn" />
+      <button
+        type="button"
+        onClick={ () => setIsRedirect(true) }
+      >
+        <img src={ profileIcon } alt="perfil" data-testid="profile-top-btn" />
+      </button>
       <h1 data-testid="page-title">Comida</h1>
-      <img src={ searchIcon } alt="busca" data-testid="search-top-btn" />
-
+      <button type="button">
+        <img src={ searchIcon } alt="busca" data-testid="search-top-btn" />
+      </button>
     </header>
   );
 }
