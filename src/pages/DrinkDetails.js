7009ab5d97a76ccaf7402390113fc508e7664ec9
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-07-01 16:30:43 -0300

    Feito estrutura de detalhes da comida e bebidas

--- src/pages/DrinkDetails.js
+++ src/pages/DrinkDetails.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 function DrinkDetails() {
   return (
-    <div />
+    <div>
+      <img src="" alt="" data-testid="recipe-photo" />
+      {' '}
+      FOTO
+      <h1 data-testid="recipe-title">TITULO</h1>
+      <img src="" alt="" data-testid="share-btn" />
+      {' '}
+      Compartilhar
+      <img src="" alt="" data-testid="favorite-btn" />
+      {' '}
+      FAVORITAR
+      <span data-testid="recipe-category">sobre a categoria</span>
+      <ul>
+        <li data-testid="$-ingredient-name-and-measure">Ingredientes</li>
+      </ul>
+      <span data-testid="instructions">Instrução</span>
+      <embed data-testid="video" src="" />
+      <div data-testid="${''}-recomendation-card">receitas recomendadas</div>
+      <button type="button" data-testid="start-recipe-btn">
+        Iniciar
+      </button>
+    </div>
   );
 }
 
