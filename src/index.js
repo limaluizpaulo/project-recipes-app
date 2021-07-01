10af4b05dd4f18e8c085c7094ec33cdc99ddf3be
riquee <strikedv@hotmail.com>
2021-06-30 22:44:41 -0300

    tentando corrigir PR

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Provider from './context/Provider';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider>
@@ -13,3 +14,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
