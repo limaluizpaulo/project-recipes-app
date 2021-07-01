bc8b069998af36ff39c3fa3a6103d6b4f3c3a131
dennis <code>
2021-07-01 18:10:09 -0300

    botton profile redirect to page profile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 import './header.css';
 
 function Header({ title, haveSrc }) {
+  const history = useHistory();
   const [searchField, setSearchField] = useState(false);
   const [inputSearch, setInputSearch] = useState('');
   console.log(haveSrc);
@@ -22,7 +24,10 @@ function Header({ title, haveSrc }) {
 
   return (
     <header className="Header">
-      <button type="button">
+      <button
+        type="button"
+        onClick={ () => history.push('/perfil') }
+      >
         <img src={ profileIcon } alt="profile icon" data-testid="profile-top-btn" />
       </button>
 
