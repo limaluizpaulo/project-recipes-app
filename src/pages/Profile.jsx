cfdf3ad6aa81c0846a2d84876faf8600901ac556
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-07-01 20:28:22 -0300

    requisitos 83 a 87

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const [email, setEmail] = useState();
+  useEffect(() => {
+    const GetEmail = () => {
+      const emailUser = localStorage.getItem('user');
+      setEmail(emailUser);
+    };
+    GetEmail();
+  }, []);
+  const deleteUser = () => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('mealsToken');
+    localStorage.removeItem('cocktailsToken');
+  };
+
   return (
     <div>
       <Header title="Perfil" />
-      <h1>profile</h1>
+      <p data-testid="profile-email">{ email }</p>
+
+      <Link to="/receitas-feitas">
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ () => deleteUser() }
+        >
+          Sair
+        </button>
+      </Link>
       <Footer />
     </div>
   );
