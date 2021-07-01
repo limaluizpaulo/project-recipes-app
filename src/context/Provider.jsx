b21ba191fd5e969d19f056319ae00118d06c6463
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-07-01 18:54:52 -0300

    Feat: Faz a requisição comforme o pathname

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,23 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
-import requestApi from '../services/helpers/apiServises';
+import { apiRequestMeal, apiRequestDrink } from '../services/helpers/apiServises';
 
 const RecipeProvider = ({ children }) => {
   const [data, setData] = useState([]);
   const [radioValue, setValueRadio] = useState('');
-  const [inputValue, setInputRadio] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [pathname, setPathname] = useState('');
 
   useEffect(() => {
-    async function setDataIngredient() {
-      const response = await requestApi(radioValue, inputValue);
-      setData(response);
+    async function setDataRecipes() {
+      const NUMBER_RENDER = 12;
+
+      if (pathname === '/comidas') {
+        const response = await apiRequestMeal(radioValue, inputValue) || [];
+        setData(response.slice(0, NUMBER_RENDER));
+      }
+      if (pathname === '/bebidas') {
+        const response = await apiRequestDrink(radioValue, inputValue) || [];
+        setData(response.slice(0, NUMBER_RENDER));
+      }
     }
-    setDataIngredient();
-  }, [radioValue, inputValue]);
+    if (pathname) {
+      setDataRecipes();
+    }
+  }, [radioValue, inputValue, pathname]);
+
+  const objContext = {
+    data,
+    setData,
+    setValueRadio,
+    setInputValue,
+    setPathname,
+    inputValue,
+    radioValue,
+  };
 
   return (
-    <Context.Provider value={ { data, setData, setValueRadio, setInputRadio } }>
+    <Context.Provider value={ objContext }>
       {children}
     </Context.Provider>
   );
