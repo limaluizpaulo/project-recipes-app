3ff3080ee55d33c367774c79eb28ddee29a37eef
isTorres <ismaeltorres612@gmail.com>
2021-07-01 01:31:18 -0300

    fix links

--- src/services/FoodAPI.js
+++ src/services/FoodAPI.js
@@ -1,9 +1,9 @@
-export const FOOD_BY_NAME = 'www.themealdb.com/api/json/v1/1/search.php?s=';
-export const FOOD_BY_ID = 'www.themealdb.com/api/json/v1/1/lookup.php?i=';
-export const FOOD_BY_LETTER = 'www.themealdb.com/api/json/v1/1/search.php?f=';
-export const FOOD_BY_INGREDIENT = 'www.themealdb.com/api/json/v1/1/filter.php?i=';
-export const FOOD_BY_RANDOM = 'www.themealdb.com/api/json/v1/1/random.php';
-export const FOOD_BY_AREA = 'www.themealdb.com/api/json/v1/1/filter.php?a=';
+export const FOOD_BY_NAME = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+export const FOOD_BY_ID = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+export const FOOD_BY_LETTER = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
+export const FOOD_BY_INGREDIENT = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
+export const FOOD_BY_RANDOM = 'https://www.themealdb.com/api/json/v1/1/random.php';
+export const FOOD_BY_AREA = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=';
 
 function fetchFood(endpoint, query) {
   return fetch(`${endpoint}${query}`)
