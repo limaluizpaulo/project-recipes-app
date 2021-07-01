4922452ff7fc3578ca4e0c202a1a96b9b4f45157
lucas <lucasportellaagu@gmail.com>
2021-07-01 15:58:28 -0300

    header completed req 9,10,11,12

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 
 function Header({ title }) {
   return (
     <header>
-      <img src={ profileIcon } alt="profile icon" data-testid="profile-top-btn" />
+      <Link to="/perfil">
+        <img src={ profileIcon } alt="profile icon" data-testid="profile-top-btn" />
+      </Link>
       <h3 data-testid="page-title">{title}</h3>
     </header>
     // lógica de componentização do header baseado na lógica do próprio arquivo de teste da trybe
