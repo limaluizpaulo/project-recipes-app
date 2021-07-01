f4e4651739ac0a86f572c7d58a471776ac251b95
Daniel Ceci <danielceci96@gmail.com>
2021-07-01 10:10:50 -0300

    Feat: :sparkles: requirement 19 DONE

---
+++ src/common/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer data-testid="footer">
+      <input
+        type="image"
+        data-testid="drinks-bottom-btn"
+      />
+      <input
+        type="image"
+        data-testid="explore-bottom-btn"
+      />
+      <input
+        type="image"
+        data-testid="food-bottom-btn"
+      />
+
+    </footer>
+  );
+}
