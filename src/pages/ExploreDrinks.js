3655e1f1d84bd83c318b4a60b9ab5d1a40df28cd
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-07-01 18:02:56 -0300

    Requisito 70

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +9,14 @@ function ExploreDrinks() {
   return (
     <>
       <Header />
+      <Link to="/explorar/bebidas/ingredientes">
+        <Button variant="primary" data-testid="explore-by-ingredient">
+          Por Ingrediente
+        </Button>
+      </Link>
+      <Button variant="primary" data-testid="explore-surprise">
+        Me Surpeenda!
+      </Button>
       <Footer />
     </>
   );
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +9,19 @@ function ExploreFoods() {
   return (
     <>
       <Header />
+      <Link to="/explorar/comidas/ingredientes">
+        <Button variant="primary" data-testid="explore-by-ingredient">
+          Por Ingrediente
+        </Button>
+      </Link>
+      <Link to="/explorar/comidas/area">
+        <Button variant="primary" data-testid="explore-by-area">
+          Por Local de Origem
+        </Button>
+      </Link>
+      <Button variant="primary" data-testid="explore-surprise">
+        Me Surpeenda!
+      </Button>
       <Footer />
     </>
   );
