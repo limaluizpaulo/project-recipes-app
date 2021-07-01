d12ed00f983805fbaf1a9c1c2ee31ff0fc31dd4e
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-06-30 23:14:03 -0300

    feat: :sparkles: REQ 13

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,43 @@ function Header(props) {
     if (showSearch === false) setShowSearch(true);
     else setShowSearch(false);
   };
-  const searchBarInput = showSearch ? (<input data-testid="search-input" />) : null;
+  const searchBarInput = showSearch ? (
+    <form htmlFor="seachBar">
+      <input id="searchBar" type="text" data-testid="search-input" />
+      <label htmlFor="ingredients-radio">
+        Ingredientes
+        <input
+          name="search-radios"
+          type="radio"
+          id="ingredients-radio"
+          data-testid="ingredient-search-radio"
+          value="ingredients"
+          checked
+        />
+      </label>
+      <label htmlFor="name-radio">
+        Nome
+        <input
+          name="search-radios"
+          type="radio"
+          id="name-radio"
+          data-testid="name-search-radio"
+          value="name"
+        />
+      </label>
+      <label htmlFor="firstLetter-radio">
+        Primeira Letra
+        <input
+          name="search-radios"
+          type="radio"
+          id="firstLetter-radio"
+          data-testid="first-letter-search-radio"
+          value="firstLetter"
+        />
+      </label>
+    </form>
+  ) : null;
+
   return (
     <header>
       <Link to="/perfil">
@@ -23,7 +59,7 @@ function Header(props) {
       <h1 data-testid="page-title">{title}</h1>
       {searchBarInput}
       {searchBar && (
-        <button onClick={ showSearchChange } type="button">
+        <button onClick={ showSearchChange } type="button" data-testid="exec-search-btn">
           <img src={ searchIcon } alt="search button" data-testid="search-top-btn" />
         </button>
       ) }
