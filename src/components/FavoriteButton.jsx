6fc441ddc0f8f2b2aaf9ba2739a912844e17a05e
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-06-30 22:14:56 -0300

    Tela de perfil ok

---
+++ src/components/FavoriteButton.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FavoriteButton = ({ history }) => {
+  const handleClick = () => {
+    history.push('/receitas-favoritas');
+  };
+
+  return (
+    <button
+      data-testid="profile-favorite-btn"
+      name="favorite"
+      onClick={ handleClick }
+      type="button"
+    >
+      Receitas Favoritas
+    </button>
+  );
+};
+
+FavoriteButton.propTypes = {
+  history: PropTypes.shape().isRequired,
+};
+
+export default FavoriteButton;
---
+++ src/components/LogoutButton.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const LogoutButton = ({ history }) => {
+  const handleClick = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  return (
+    <button
+      data-testid="profile-logout-btn"
+      name="logout"
+      onClick={ handleClick }
+      type="button"
+    >
+      Sair
+    </button>
+  );
+};
+
+LogoutButton.propTypes = {
+  history: PropTypes.shape().isRequired,
+};
+
+export default LogoutButton;
