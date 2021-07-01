571d7494885386f8dc99c3d22662e5d264474e76
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-06-30 23:57:55 -0300

    Tela de explorar bebidas e comidas ok

--- src/components/ExploreSurpriseButton.jsx
+++ src/components/ExploreSurpriseButton.jsx
@@ -3,7 +3,26 @@ import PropTypes from 'prop-types';
 
 const ExploreSurpriseButton = ({ history }) => {
   const handleClick = () => {
-    console.log(history);
+    const path = history.location.pathname;
+    if (path === '/explorar/comidas') {
+      const fetchMealRandom = async () => {
+        const endpoint = 'https://www.themealdb.com/api/json/v1/1/random.php';
+        const request = await fetch(endpoint);
+        const { meals } = await request.json();
+        const surpriseId = meals[0].idMeal;
+        history.push(`/comidas/${surpriseId}`);
+      };
+      fetchMealRandom();
+    } else {
+      const fetchDrinkRandom = async () => {
+        const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+        const request = await fetch(endpoint);
+        const { drinks } = await request.json();
+        const surpriseId = drinks[0].idDrink;
+        history.push(`/bebidas/${surpriseId}`);
+      };
+      fetchDrinkRandom();
+    }
   };
 
   return (
