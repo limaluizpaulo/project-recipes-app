856df8a9587d7b48278d00f901121d64e6cef350
isTorres <ismaeltorres612@gmail.com>
2021-07-01 01:30:10 -0300

    create MealsByArea component

--- src/components/MealsByArea.jsx
+++ src/components/MealsByArea.jsx
@@ -1,41 +1,51 @@
 import React, { useEffect, useState } from 'react';
-// import { useHistory } from 'react-router-dom';
-import Footer from './Footer';
-// import { FOOD_BY_AREA } from '../services/FoodAPI';
+import fetchFood, { FOOD_BY_AREA } from '../services/FoodAPI';
 
-import fetchFood from '../services/FoodAPI';
-
-export default function Explore() {
-  const FOOD_BY_AREA = 'www.themealdb.com/api/json/v1/1/list.php?a=list';
-  const [area, setArea] = useState(null);
-  // const [meals, setMeals] = useState([]);
-
-  // const { history } = useHistory();
-
-  // function fetchFood() {
-  //   return fetch('www.themealdb.com/api/json/v1/1/filter.php?a=Canadian')
-  //     .then((res) => res.json())
-  //     .then((res) => res.meals);
-  // }
+export default function MealsByArea() {
+  const LIST_AREAS = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
+  const [areas, setAreas] = useState(null);
+  const [meals, setMeals] = useState([]);
+  const [selectArea, setSelectArea] = useState('American');
 
   useEffect(() => {
-    fetchFood(FOOD_BY_AREA, null)
-      .then((res) => setArea(res));
+    fetchFood(LIST_AREAS)
+      .then((res) => setAreas(res));
   }, []);
 
-  // if (area === null) {
-  //   return (
-  //     <p>
-  //       Loading profile...
-  //     </p>
-  //   );
-  // }
+  useEffect(() => {
+    fetchFood(FOOD_BY_AREA, selectArea)
+      .then((res) => setMeals(res));
+  }, [selectArea]);
+
+  if (areas === null) {
+    return (
+      <p>
+        Loading...
+      </p>
+    );
+  }
+
   return (
-    <>
-      {/* <Header /> */}
-      {/* {console.log(`${FOOD_BY_AREA}${area}`)} */}
-      { console.log(area) }
-      <Footer />
-    </>
+    <section>
+      <select
+        onChange={ (e) => setSelectArea(e.target.value) }
+        data-testid="explore-by-area-dropdown"
+      >
+        { areas.map((area, index) => (
+          <option
+            data-testid={ `${area.strArea}-option` }
+            key={ index }
+          >
+            { area.strArea }
+          </option>
+        )) }
+      </select>
+      { meals.map((meal, i) => (
+        <div key={ i }>
+          <img src={ meal.strMealThumb } alt={ meal.strMeal } />
+          <h3>{ meal.strMeal }</h3>
+        </div>
+      )) }
+    </section>
   );
 }
