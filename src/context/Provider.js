6126610c2a955b44358f49c049753fc4061916b9
riquee <strikedv@hotmail.com>
2021-06-30 22:40:19 -0300

    Create utils

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,39 +1,17 @@
 import React, { useState, useEffect, createContext } from 'react';
 import PropTypes from 'prop-types';
+import { endPoint } from '../utils';
 
 export const GlobalContext = createContext();
-
-const checkOption = ({ inputSearch, option, food }) => {
-  let query;
-  let db;
-  switch (option) {
-  case 'ingredient':
-    query = `filter.php?i=${inputSearch}`;
-    break;
-  case 'name':
-    query = `search.php?s=${inputSearch}`;
-    break;
-  case 'firstLetter':
-    query = `search.php?f=${inputSearch}`;
-    break;
-  default:
-    return false;
-  }
-
-  if (food) db = 'themealdb';
-  if (!food) db = 'thecocktaildb';
-
-  return `https://www.${db}.com/api/json/v1/1/${query}`;
-};
 
 const Provider = ({ children }) => {
   const [searchOp, setSearchOp] = useState({});
   const [recipes, setRecipes] = useState({});
 
   useEffect(() => {
-    const endPoint = checkOption(searchOp);
-    if (endPoint) {
-      fetch(endPoint)
+    const URL = endPoint(searchOp);
+    if (URL) {
+      fetch(URL)
         .then((res) => res.json())
         .then(setRecipes);
     }
@@ -44,11 +22,7 @@ const Provider = ({ children }) => {
     recipes,
   };
 
-  return (
-    <GlobalContext.Provider value={ value }>
-      { children }
-    </GlobalContext.Provider>
-  );
+  return <GlobalContext.Provider value={ value }>{children}</GlobalContext.Provider>;
 };
 
 Provider.propTypes = {
