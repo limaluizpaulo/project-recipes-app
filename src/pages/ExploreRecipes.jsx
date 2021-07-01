a3bc3afc942dadd76aef6b0b024370154b6a01da
isTorres <ismaeltorres612@gmail.com>
2021-07-01 01:31:02 -0300

    try to fix navigation in explore page

--- src/pages/ExploreRecipes.jsx
+++ src/pages/ExploreRecipes.jsx
@@ -1,44 +1,95 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 import Footer from '../components/Footer';
-import MealsByArea from '../components/MealsByArea';
+// import MealsByArea from '../components/MealsByArea';
 
-export default function Explore() {
-  // handleComponent({target}) {
-  //   switch (target.name) {
-  //     case "MealsByIngredients":
-  //       return <MealsByIngredients />;
-  //     case "MealsByArea":
-  //       return <MealsByArea />;
-  //     case "MealsRandom":
-  //       return <MealsRandom />;
-  //     default:
-  //       // return null;
-  //   }
+export default function ExploreRecipes() {
+  // const history = useHistory();
+  // const path = history.location.pathname;
+
+  // function handleRedirect({ target }) {
+  //   const page = target.name;
+  //   const { push } = history;
+  //   return push(`${path}/${page}`);
   // }
 
+  // function buttons() {
+  //   if (path === '/explorar/comidas') {
+  //     return (
+  //       <>
+  //         <Button
+  //           name="ingredientes"
+  //           type="button"
+  //           data-testid="explore-by-ingredient"
+  //         >
+  //           Por Ingrediente
+  //         </Button>
+  //         <Link to={ `${path}/area` }>
+  //           <Button
+  //             name="area"
+  //             type="button"
+  //             data-testid="explore-by-area"
+  //           >
+  //             Por Local de Origem
+  //           </Button>
+  //         </Link>
+  //         <Button
+  //           name="surpresa"
+  //           type="button"
+  //           data-testid="explore-surprise"
+  //         >
+  //           Me Surpreenda!
+  //         </Button>
+  //       </>
+  //     );
+  //   }
+  //   return (
+  //     <>
+  //       <Button
+  //         name="MealsByIngradient"
+  //         type="button"
+  //         data-testid="explore-by-ingredient"
+  //       >
+  //         Por Ingrediente
+  //       </Button>
+  //       <Button
+  //         name="MealsByIngradient"
+  //         type="button"
+  //         data-testid="explore-surprise"
+  //       >
+  //         Me Surpreenda!
+  //       </Button>
+  //     </>
+
+  //   );
+  // }
   return (
     <main>
-      {/* <Header /> */}
-      <button
-        name="MealsByIngradient"
+      <Button
+        name="ingredientes"
         type="button"
-        // onClick={}
+        data-testid="explore-by-ingredient"
       >
         Por Ingrediente
-      </button>
-      <MealsByArea />
-      <button
-        name="MealsByIngradient"
+      </Button>
+      <Link to="/explorar/comidas/area">
+        <Button
+          name="area"
+          type="button"
+          data-testid="explore-by-area"
+        >
+          Por Local de Origem
+        </Button>
+      </Link>
+      <Button
+        name="surpresa"
         type="button"
-      >
-        Por Local de Origem
-      </button>
-      <button
-        name="MealsByIngradient"
-        type="button"
+        data-testid="explore-surprise"
       >
         Me Surpreenda!
-      </button>
+      </Button>
+      {/* { buttons() } */}
       <Footer />
     </main>
   );
