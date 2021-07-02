0a356e744e9b0dcefc3de0fe73f232b87d4a4578
Fernando <fhomaia@gmail.com>
2021-07-02 17:40:01 -0300

    fix: warnings do lint  (alerts)

--- src/services/fetchApi.jsx
+++ src/services/fetchApi.jsx
@@ -3,7 +3,7 @@ function fetchAPI(url) {
     .then((response) => response.json())
     .then((response) => response)
     .catch(() => {
-      alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+      global.alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
       return {};
     });
 }
