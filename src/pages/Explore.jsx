6d5c5715b23375986d5966571b68178d5fa7a0a8
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-02 14:19:11 -0300

    cria redirect ao clicar nos botoes da pagina

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
 import { Button, Container } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Explore() {
+  const history = useHistory();
   return (
     <Container>
       <Header profile name="Explorar" />
-
       <Container>
-        <Button
-          data-testid="explore-food"
-          variant="outline-secondary"
-          size="lg"
-        >
-          Explorar Comidas
-
-        </Button>
+        <Container>
+          <Button
+            data-testid="explore-food"
+            variant="outline-secondary"
+            size="lg"
+            onClick={ () => history.push('/explorar/comidas') }
+          >
+            Explorar Comidas
+          </Button>
+        </Container>
+        <Container>
+          <Button
+            data-testid="explore-drinks"
+            variant="outline-secondary"
+            size="lg"
+            onClick={ () => history.push('/explorar/bebidas') }
+          >
+            Explorar Bebidas
+          </Button>
+        </Container>
       </Container>
-
-      <Container>
-        <Button
-          data-testid="explore-drinks"
-          variant="outline-secondary"
-          size="lg"
-        >
-          Explorar Bebidas
-
-        </Button>
-      </Container>
-
       <Footer />
     </Container>
   );
