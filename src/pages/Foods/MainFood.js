28c87ca7c18854efff4845924173b711d3acde4b
Hugo Simoes <hugolfsimoes@gmail.com>
2021-07-01 22:09:40 -0300

    feito os requisitos do componente Footer

--- src/pages/Foods/MainFood.js
+++ src/pages/Foods/MainFood.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
 export default function MainFood() {
   return (
     <div
       className="main-food-class"
-      style={ { width: '100%', position: 'absolute', top: '0' } }
     >
       <Header title="Comidas" classname="display" />
+      <Footer />
     </div>
   );
 }
