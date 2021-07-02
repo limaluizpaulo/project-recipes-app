e74e7e9eee067fb8f0638f9bdf5cac6efc75c6c7
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-07-01 21:37:03 -0300

    Refactor: Adiciona Try/catch nas requisições para tratamento de erros

--- src/services/helpers/apiServises.js
+++ src/services/helpers/apiServises.js
@@ -4,36 +4,68 @@ const BASE_URL_DRINKS = 'https://www.thecocktaildb.com/api/json/v1/1/';
 export async function apiRequestMeal(buttonRadio, searchInput) {
   if (buttonRadio === 'Ingrediente') {
     const fetchIngrediente = await fetch(`${BASE_URL_MEAL}filter.php?i=${searchInput}`);
-    const { meals } = await fetchIngrediente.json();
-    return meals;
+
+    try {
+      const { meals } = await fetchIngrediente.json();
+      return meals;
+    } catch (error) {
+      return [];
+    }
   }
+
   if (buttonRadio === 'Nome') {
     const fetchName = await fetch(`${BASE_URL_MEAL}search.php?s=${searchInput}`);
-    const { meals } = await fetchName.json();
-    return meals;
+
+    try {
+      const { meals } = await fetchName.json();
+      return meals;
+    } catch (error) {
+      return [];
+    }
   }
+
   if (buttonRadio === 'Primeira letra') {
     const fetchLetra = await fetch(`${BASE_URL_MEAL}search.php?f=${searchInput}`);
-    const { meals } = await fetchLetra.json();
-    return meals;
+
+    try {
+      const { meals } = await fetchLetra.json();
+      return meals;
+    } catch (error) {
+      return [];
+    }
   }
 }
 
 export async function apiRequestDrink(buttonRadio, searchInput) {
   if (buttonRadio === 'Ingrediente') {
     const fetchIngrediente = await fetch(`${BASE_URL_DRINKS}filter.php?i=${searchInput}`);
-    const { drinks } = await fetchIngrediente.json();
-    return drinks;
+
+    try {
+      const { drinks } = await fetchIngrediente.json();
+      return drinks;
+    } catch (error) {
+      return [];
+    }
   }
 
   if (buttonRadio === 'Nome') {
     const fetchName = await fetch(`${BASE_URL_DRINKS}search.php?s=${searchInput}`);
-    const { drinks } = await fetchName.json();
-    return drinks;
+
+    try {
+      const { drinks } = await fetchName.json();
+      return drinks;
+    } catch (error) {
+      return [];
+    }
   }
   if (buttonRadio === 'Primeira letra') {
     const fetchLetra = await fetch(`${BASE_URL_DRINKS}search.php?f=${searchInput}`);
-    const { drinks } = await fetchLetra.json();
-    return drinks;
+
+    try {
+      const { drinks } = await fetchLetra.json();
+      return drinks;
+    } catch (error) {
+      return [];
+    }
   }
 }
