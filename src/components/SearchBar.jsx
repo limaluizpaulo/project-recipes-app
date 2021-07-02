1b1f026c6ea5d8955b46bb4668bd6e2abfb5bf5f
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-07-02 00:15:17 -0300

    verificando erros de botao nos requisitos que falharam

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 
-import { Form, Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import RecipesContext from '../context/RecipesContext';
 import DrinksContext from '../context/DrinksContext';
