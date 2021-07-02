af9451774e6ea59e7d042cab1d5da0e4f9c22ccf
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-02 14:15:35 -0300

    cria estrutura basica pagina Explore

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Explore() {
   return (
-    <>
+    <Container>
       <Header profile name="Explorar" />
-      <p>Comidas</p>
+
+      <Container>
+        <Button
+          data-testid="explore-food"
+          variant="outline-secondary"
+          size="lg"
+        >
+          Explorar Comidas
+
+        </Button>
+      </Container>
+
+      <Container>
+        <Button
+          data-testid="explore-drinks"
+          variant="outline-secondary"
+          size="lg"
+        >
+          Explorar Bebidas
+
+        </Button>
+      </Container>
+
       <Footer />
-    </>
+    </Container>
   );
 }
 
