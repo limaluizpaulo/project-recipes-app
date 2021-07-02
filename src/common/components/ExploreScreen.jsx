5e5d458c0622c70706c4b4524bca16c692fd4721
Flavio Andrade <foa12381@gmail.com>
2021-07-02 16:18:00 -0300

    implematation exploreScreen

---
+++ src/common/components/ExploreScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function ExploreScreen() {
+  return (
+    <div>
+      <div>
+        <p data-testid="explore-food">Explorar Comidas</p>
+        <button type="button">Explorar Comidas</button>
+      </div>
+      <div>
+        <p data-testid="explore-drinks">Explorar Bebidas</p>
+        <button type="button">Explorar Bebidas</button>
+      </div>
+    </div>
+  );
+}
