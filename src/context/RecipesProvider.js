797486dbe322f40ca2633c61a2b6832ccbb20724
Aladino Borges <eualadino@gmail.com>
2021-07-02 20:48:07 -0300

    Redirecionar a partir do estado global

--- src/context/RecipesProvider.js
+++ src/context/RecipesProvider.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { useRouteMatch, useHistory } from 'react-router-dom';
+
 import PropTypes from 'prop-types';
 import RecipesContext from './RecipesContext';
 
@@ -6,6 +9,24 @@ export default function RecipesProvider({ children }) {
   const [globalState, setGlobalState] = useState([]);
   const [userInfo, setInfo] = useState({ email: '' });
   const context = { setInfo, userInfo, globalState, setGlobalState };
+
+  const { path } = useRouteMatch();
+  const history = useHistory();
+
+  // Requisito chato
+  useEffect(() => {
+    let targetId = 'idDrink';
+    if (path === '/comidas') { targetId = 'idMeal'; }
+    function test() {
+      history.push(`/${globalState[0][targetId]}`);
+      // history.push(`/${globalState[0][targetId]}`);
+    }
+
+    if (globalState.length === 1) {
+      test();
+    }
+  }, [path, history, globalState]);
+  // Requisito chato
 
   return (
     <RecipesContext.Provider value={ context }>
