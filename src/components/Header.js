74f74fe6bf115b9849c4e62f37eb6f1f572990d5
Aladino Borges <eualadino@gmail.com>
2021-07-02 16:17:49 -0300

    Componentiza o searchBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@ import { // useParams,
   useRouteMatch,
 } from 'react-router-dom';
 
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 export default function Header() {
   const { path } = useRouteMatch();
   const [isHidden, setHidden] = useState(true);
+
   // Source: https://stackoverflow.com/questions/26574388/boolean-logic-in-switch-case-statement-java/26574416
 
   // function dinamicSearchBar() {
@@ -48,38 +50,7 @@ export default function Header() {
             >
               <img data-testid="search-top-btn" src={ searchIcon } alt="search-icon" />
             </button>
-            {!isHidden && (
-              <div className="search-container">
-                <input type="text" data-testid="search-input" />
-                <label htmlFor="ingredient">
-                  Ingredientes
-                  <input
-                    id="ingredient"
-                    name="search"
-                    type="radio"
-                    data-testid="ingredient-search-radio"
-                  />
-                </label>
-                <label htmlFor="name">
-                  Nome
-                  <input
-                    id="name"
-                    name="search"
-                    type="radio"
-                    data-testid="name-search-radio"
-                  />
-                </label>
-                <label htmlFor="first-letter">
-                  Primeira letra
-                  <input
-                    id="first-letter"
-                    name="search"
-                    type="radio"
-                    data-testid="first-letter-search-radio"
-                  />
-                </label>
-                <Button type="button" data-testid="exec-search-btn">Search</Button>
-              </div>)}
+            {!isHidden && (<SearchBar />)}
           </Container>
         </Navbar>);
     case '/explorar':
