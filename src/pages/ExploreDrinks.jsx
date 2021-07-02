46bff355bf0ef5007a56493248ba9ac0546cc4bb
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-02 14:45:02 -0300

    cria estrutura basica pagina Explorar Comidas/Bebidas

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExploreDrinks() {
+  const history = useHistory();
+
   return (
-    <>
+    <Container>
       <Header profile name="Explorar Bebidas" />
-      <p>Comidas</p>
+
+      <Container>
+
+        <Button
+          data-testid="explore-by-ingredient"
+          variant="outline-secondary"
+          size="lg"
+          onClick={ () => history.push('/explorar/bebidas/ingredientes') }
+        >
+          Por Ingredientes
+
+        </Button>
+
+        <Button
+          data-testid="explore-by-area"
+          variant="outline-secondary"
+          size="lg"
+          onClick={ () => history.push('/explorar/bebidas/area') }
+        >
+          Por Local de Origem
+
+        </Button>
+
+        <Button
+          data-testid="explore-surprise"
+          variant="outline-secondary"
+          size="lg"
+          onClick=""/* { () => history.push() } */
+        >
+          Me Surpreenda!
+
+        </Button>
+
+      </Container>
+
       <Footer />
-    </>
+    </Container>
   );
 }
 
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExploreFoods() {
+  const history = useHistory();
+
   return (
-    <>
+    <Container>
       <Header profile name="Explorar Comidas" />
-      <p>Comidas</p>
+      <Container>
+
+        <Button
+          data-testid="explore-by-ingredient"
+          variant="outline-secondary"
+          size="lg"
+          onClick={ () => history.push('/explorar/comidas/ingredientes') }
+        >
+          Por Ingredientes
+
+        </Button>
+
+        <Button
+          data-testid="explore-by-area"
+          variant="outline-secondary"
+          size="lg"
+          onClick={ () => history.push('/explorar/comidas/area') }
+        >
+          Por Local de Origem
+
+        </Button>
+
+        <Button
+          data-testid="explore-surprise"
+          variant="outline-secondary"
+          size="lg"
+          onClick=""/* { () => history.push() } */
+        >
+          Me Surpreenda!
+
+        </Button>
+
+      </Container>
       <Footer />
-    </>
+    </Container>
   );
 }
 
