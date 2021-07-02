5b339156571d01f0441731a51d187b58c1525ef5
vinicius <viniciuspalenske@gmail.com>
2021-07-01 21:53:36 -0300

    [feat][requisitos 25 e 26] tela principal com 12 cards

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import Footer from '../components/Footer';
 import Header from '../components/Header';
 import HeaderSearchButton from '../components/HeaderSearchButton';
+import Footer from '../components/Footer';
+import { drinks } from '../mocks/drinks';
+import RecipeCard from '../components/RecipeCard';
 
 function Drinks() {
+  const maxLength = 12;
+  const drinkList = () => drinks.slice(0, maxLength).map((drink, index) => (
+    RecipeCard(drink, index)));
   return (
     <>
       <div>Tela de Bebidas</div>
       <Header title="Bebidas" />
       <HeaderSearchButton />
+      <div className="recipe-list">
+        {drinkList()}
+      </div>
       <Footer />
     </>
   );
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import Header from '../components/Header';
 import HeaderSearchButton from '../components/HeaderSearchButton';
 import Footer from '../components/Footer';
+import { meals } from '../mocks/meals';
+import RecipeCard from '../components/RecipeCard';
 
 function Foods() {
+  const maxLength = 12;
+  const foodList = () => meals.slice(0, maxLength).map((meal, index) => (
+    RecipeCard(meal, index)));
   return (
     <>
       <div>Tela de Comidas</div>
       <Header title="Comidas" />
       <HeaderSearchButton />
+      <div className="recipe-list">
+        {foodList()}
+      </div>
       <Footer />
     </>
   );
