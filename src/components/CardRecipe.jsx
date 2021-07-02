cf6e7111c9649c51e528c73258745a8614c700ce
Antenor <antenorzapata@gmail.com>
2021-07-02 10:50:59 -0300

    requisito 32

--- src/components/CardRecipe.jsx
+++ src/components/CardRecipe.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export default function CardRecipe({ data, index }) {
   const { strMealThumb, strMeal, strDrink, strDrinkThumb } = data;
   return (
     <div data-testid={ `${index}-recipe-card` } className="recipe-card">
-      <img
-        className="card_image"
-        src={ strMealThumb || strDrinkThumb }
-        alt={ strMeal || strDrink }
-        data-testid={ `${index}-card-img` }
-      />
-      <p data-testid={ `${index}-card-name` }>{strMeal || strDrink}</p>
+      <Link to={ data.idDrink ? `/bebidas/${data.idDrink}` : `/comidas/${data.idMeal}` }>
+        <img
+          className="card_image"
+          src={ strMealThumb || strDrinkThumb }
+          alt={ strMeal || strDrink }
+          data-testid={ `${index}-card-img` }
+        />
+        <p data-testid={ `${index}-card-name` }>{strMeal || strDrink}</p>
+      </Link>
     </div>
   );
 }
