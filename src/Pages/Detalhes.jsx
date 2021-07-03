e6e57aeeaee0b43055e7c271be93f32c8ba963b2
Anderson <trkz.anderson@gmail.com>
2021-07-02 22:43:33 -0300

    Ajustes no array de dependencias em detailspage

--- src/Pages/Detalhes.jsx
+++ src/Pages/Detalhes.jsx
@@ -14,7 +14,7 @@ export default function DetalhesComida({ location }) {
 
   useEffect(() => {
     storeCurrentRecipe(location.pathname.split('/')[2]);
-  }, []);
+  }, [storeCurrentRecipe, location]);
 
   return (
     <Container>
