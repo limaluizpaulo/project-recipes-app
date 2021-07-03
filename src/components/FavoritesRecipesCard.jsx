6752a093e6a44b53260b182079ccd318197f81f5
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-07-03 03:24:44 -0300

    Feat: Tela de Receitas favoritas concluida

---
+++ src/components/FavoritesRecipesCard.jsx
@@ -0,0 +1,126 @@
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Context from '../context/Context';
+import { getRecipesFavorites } from '../services/helpers/localStorage';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+
+export default function FavoriteRecipesCard() {
+  const { selectedTypeItem } = useContext(Context);
+  const copyLink = async ({ target }, data) => {
+    await navigator.clipboard.writeText(data);
+    const { id } = target;
+    const divButtonsGet = document.getElementById(`${id}-div-buttons`);
+    const messageExist = document.getElementById(`${id}-message-span`);
+    if (!messageExist) {
+      const spamFromMessage = document.createElement('div');
+      spamFromMessage.id = `${id}-message-span`;
+      spamFromMessage.textContent = 'Link copiado!';
+      divButtonsGet.appendChild(spamFromMessage);
+    } else {
+      messageExist.remove();
+    }
+  };
+
+  const getFromLocalStorage = (key) => {
+    const resFromLocalStorage = localStorage.getItem(key);
+    return JSON.parse(resFromLocalStorage);
+  };
+
+  const setOnLocalStorage = (value) => {
+    let itemToBeStored = value;
+    if (typeof value === 'object') itemToBeStored = JSON.stringify(value);
+    localStorage.setItem('favoriteRecipes', itemToBeStored);
+  };
+
+  const removeFromLocalStorage = (value) => {
+    const itemStored = getFromLocalStorage('favoriteRecipes');
+    const newItemsToBeStored = itemStored.filter(({ id }) => id !== value.id);
+    setOnLocalStorage(newItemsToBeStored);
+  };
+
+  const favoriteRecipe = ({ target }, value) => {
+    const { className, id } = target;
+    if (!className) {
+      target.className = 'favorited-recipe';
+    } else {
+      const idFound = id.split('-')[0];
+      const recipeFromRemove = document.getElementById(`${idFound}-recipe-div`);
+      recipeFromRemove.remove();
+      removeFromLocalStorage(value);
+    }
+  };
+  useEffect(() => {
+    getRecipesFavorites();
+  }, []);
+  let recipesIsFavorite = getRecipesFavorites();
+  if (!recipesIsFavorite) recipesIsFavorite = [];
+  const filtredRecipesDone = recipesIsFavorite
+    .filter((recipe) => recipe.type !== selectedTypeItem);
+  return (
+    <>
+      {filtredRecipesDone.map((recipe, index) => (
+        <div
+          key={ index }
+          className="card-recipes"
+          id={ `${index}-recipe-div` }
+        >
+          <div className="class-image">
+            <Link to={ `/${recipe.type}s/${recipe.id}` }>
+              <img
+                className="card-img-done"
+                src={ recipe.image }
+                alt={ recipe.name }
+                data-testid={ `${index}-horizontal-image` }
+              />
+            </Link>
+          </div>
+          <div
+            className="class-items"
+            id={ `${index}-div-buttons` }
+          >
+            <div className="card-combined-itens">
+              <span
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                { recipe.type === 'comida'
+                  ? `${recipe.area} - ${recipe.category}`
+                  : recipe.alcoholicOrNot }
+              </span>
+            </div>
+            <Link to={ `/${recipe.type}s/${recipe.id}` }>
+              <h5 data-testid={ `${index}-horizontal-name` }>{ recipe.name }</h5>
+            </Link>
+            <div
+              className="buttons-tags"
+            >
+              <button
+                type="button"
+                onClick={ (event) => copyLink(event, `http://localhost:3000/${recipe.type}s/${recipe.id}`) }
+              >
+                <img
+                  id={ index }
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  src={ shareIcon }
+                  alt={ recipe.name }
+                />
+              </button>
+              <button
+                type="button"
+                onClick={ (event) => favoriteRecipe(event, recipe) }
+              >
+                <img
+                  id={ `${index}-favorite-img` }
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                  src={ blackHeartIcon }
+                  alt={ recipe.name }
+                  className="favorited-recipe"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
--- src/pages/FavoritesRecipes.jsx
+++ src/pages/FavoritesRecipes.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Context from '../context/Context';
+
+import FavoriteRecipesCard from '../components/FavoritesRecipesCard';
 
 // Tela de receitas favoritas: /receitas-favoritas
 export default function FavoritesRecipes({ history }) {
+  const { setSelectedTypeItem } = useContext(Context);
+
+  const setFilterRecipesDone = (data) => {
+    setSelectedTypeItem(data);
+  };
+
   return (
-    <Header history={ history } title="Receitas Favoritas" />
+    <div>
+      <Header history={ history } title="Receitas Favoritas" />
+      <section className="profile-buttons">
+        <button
+          type="button"
+          className="btn btn-sm color-button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => setFilterRecipesDone('all') }
+        >
+          All
+        </button>
+        <button
+          type="button"
+          className="btn btn-sm color-button"
+          data-testid="filter-by-food-btn"
+          onClick={ () => setFilterRecipesDone('bebida') }
+        >
+          Foods
+        </button>
+        <button
+          type="button"
+          className="btn btn-sm color-button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setFilterRecipesDone('comida') }
+        >
+          Drinks
+        </button>
+      </section>
+      <section>
+        <FavoriteRecipesCard />
+      </section>
+    </div>
   );
 }
 
