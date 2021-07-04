a368e11571b4fe53b7baa2db881cab09cef1ca1f
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2021-07-04 18:26:35 -0300

    Adicionado estado que armazena o localstorage ao logar.

--- src/components/ButtonStartRecipe.jsx
+++ src/components/ButtonStartRecipe.jsx
@@ -1,38 +1,58 @@
-import React from 'react';
+import React, { useCallback, useContext, useState, useEffect } from 'react';
 import { useLocation } from 'react-router';
 import { useHistory, useParams } from 'react-router-dom';
+import LoginContext from '../context/LoginContext';
 
 export default function ButtonStartRecipe() {
-  const style = {
-    position: 'fixed',
-    bottom: 0,
-  };
+  const [buttonVisible, setButtonVisible] = useState(true);
+  const { idsDoneRecipes } = useContext(LoginContext);
 
   const { pathname } = useLocation();
   const { id } = useParams();
   const history = useHistory();
 
   const NUMBER_TO_VERIFICATION = -1;
+  const style = {
+    position: 'fixed',
+    bottom: 0,
+  };
 
+  const verifyId = useCallback(() => {
+    const search = idsDoneRecipes.find((item) => item === id);
+    if (typeof search === 'string') {
+      setButtonVisible(false);
+    }
+  }, [id, idsDoneRecipes]);
+
+  useEffect(() => {
+    verifyId();
+  }, [verifyId]);
   const getDrinksDetails = pathname.indexOf('bebidas') > NUMBER_TO_VERIFICATION;
   return getDrinksDetails ? (
 
-    <button
-      type="button"
-      data-testid="start-recipe-btn"
-      style={ style }
-      onClick={ () => history.push(`/bebidas/${id}/in-progress`) }
-    >
-      <span>Iniciar Receita</span>
-    </button>
+    buttonVisible && (
+      <button
+        type="button"
+        data-testid="start-recipe-btn"
+        style={ style }
+        onClick={ () => history.push(`/bebidas/${id}/in-progress`) }
+      >
+        <span>Iniciar Receita</span>
+      </button>
+    )
+
   ) : (
-    <button
-      type="button"
-      data-testid="start-recipe-btn"
-      style={ style }
-      onClick={ () => history.push(`/comidas/${id}/in-progress`) }
-    >
-      <span>Iniciar Receita</span>
-    </button>
+
+    buttonVisible && (
+      <button
+        type="button"
+        data-testid="start-recipe-btn"
+        style={ style }
+        onClick={ () => history.push(`/comidas/${id}/in-progress`) }
+      >
+        <span>Iniciar Receita</span>
+      </button>
+    )
+
   );
 }
--- src/context/LoginProvider.js
+++ src/context/LoginProvider.js
@@ -5,13 +5,63 @@ import LoginContext from './LoginContext';
 
 function LoginProvider({ children }) {
   const [login, setLogin] = useState('');
+  const [idsDoneRecipes, setIdsDoneRecipes] = useState([]);
 
   function successLogin(email) {
     setLogin(email);
   }
 
+  function getDoneRecipes() {
+    const ids = JSON.parse(localStorage.getItem('doneRecipes'));
+    if (ids !== null) {
+      setIdsDoneRecipes(ids);
+    } else {
+      localStorage.setItem('doneRecipes', JSON.stringify([{
+        id: '178319',
+        type: 'bebida',
+        area: '',
+        category: 'Cocktail',
+        alcoholicOrNot: 'Alcoholic',
+        name: 'Aquamarine',
+        image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+        doneDate: '23/6/2020',
+        tags: [],
+      },
+      {
+        id: '52771',
+        type: 'comida',
+        area: 'Italian',
+        category: 'Vegetarian',
+        alcoholicOrNot: '',
+        name: 'Spicy Arrabiata Penne',
+        image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+        doneDate: '22/6/2020',
+        tags: ['Pasta', 'Curry'],
+      }]));
+
+      localStorage.setItem('inProgressRecipes', JSON.stringify(
+        {
+          meals: {
+            52771: [],
+          },
+          cocktails: {
+            178319: [],
+          },
+        },
+      ));
+      setIdsDoneRecipes(['178319', '52771']);
+    }
+  }
+
   return (
-    <LoginContext.Provider value={ { successLogin, login } }>
+    <LoginContext.Provider
+      value={ {
+        successLogin,
+        login,
+        idsDoneRecipes,
+        getDoneRecipes,
+      } }
+    >
       { children }
     </LoginContext.Provider>
   );
