d5d342d69f1eed5976ec7f40956f600b4551b3b5
Alan Tanaka <alanctnk@gmail.com>
2021-07-04 18:39:53 -0300

    corrigindo lint

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import '../styles/Footer.css';
 
 function Footer() {
   return (
