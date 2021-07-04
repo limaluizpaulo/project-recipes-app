b214ecbf1aa4e6c7bbbbe5590db8be39183ca91c
isTorres <ismaeltorres612@gmail.com>
2021-07-04 00:35:11 -0300

    fix name of variables in api file

--- src/services/FoodAPI.js
+++ src/services/FoodAPI.js
@@ -1,9 +1,9 @@
-export const FOOD_BY_NAME = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-export const FOOD_BY_ID = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
-export const FOOD_BY_LETTER = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
-export const FOOD_BY_INGREDIENT = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
-export const FOOD_BY_RANDOM = 'https://www.themealdb.com/api/json/v1/1/random.php';
-export const FOOD_BY_AREA = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=';
+export const BY_NAME = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+export const BY_ID = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+export const BY_LETTER = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
+export const BY_INGREDIENT = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
+export const BY_RANDOM = 'https://www.themealdb.com/api/json/v1/1/random.php';
+export const BY_AREA = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=';
 
 function fetchFood(endpoint, query) {
   return fetch(`${endpoint}${query}`)
