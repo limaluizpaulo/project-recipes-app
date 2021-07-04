cc986c4fbe8734228483b887979219493ad9f4a8
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-07-04 16:06:56 -0300

    Feat: Implementa as funções que cria os botões e renderiza os cards

--- src/pages/MainFood.jsx
+++ src/pages/MainFood.jsx
@@ -1,15 +1,72 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RecipeContext from '../context/Context';
-// import messageAlert from '../services/helpers/alertMessage';
+import { apiRequestFoods } from '../services/api/getMealsDrink';
+import { apiCategoriesFoods } from '../services/api/getCategories';
 
 // Tela principal de receitas de comidas: /comidas
 export default function MainFood({ history }) {
   const { data } = useContext(RecipeContext);
+  const [dataFood, setDataFood] = useState([]);
+  const [filterFood, setfilterFood] = useState([]);
+  const [categoriesFood, setCategoriesFood] = useState([]);
+  const [filterCategoriesFood, setFilterCategoriesFood] = useState([]);
+
+  console.log(filterCategoriesFood);
+
+  useEffect(() => {
+    async function apiRequest() {
+      const { meals } = await apiRequestFoods();
+      setDataFood(meals);
+    }
+    apiRequest();
+  }, []);
+
+  useEffect(() => {
+    async function apiRequestCategories() {
+      const { meals } = await apiCategoriesFoods();
+      setCategoriesFood(meals);
+    }
+    apiRequestCategories();
+  }, []);
+
+  useEffect(() => {
+    const NUMBER_FILTER = 12;
+    setfilterFood(dataFood.slice(0, NUMBER_FILTER));
+  }, [dataFood]);
+
+  useEffect(() => {
+    const NUMBER_FILTER_CATEGORIES = 5;
+    setFilterCategoriesFood(categoriesFood.slice(0, NUMBER_FILTER_CATEGORIES));
+  }, [categoriesFood]);
+
+  function buttonCategories() {
+    return filterCategoriesFood.map((categ, i) => (
+      <button
+        type="button"
+        key={ i }
+        data-testid={ `${categ.strCategory}-category-filter` }
+      >
+        {categ.strCategory}
+      </button>
+    ));
+  }
 
   function renderMeal() {
+    if (data.length === 0) {
+      return filterFood.map((item, index) => (
+        <div key={ index } data-testid={ `${index}-recipe-card` }>
+          <p data-testid={ `${index}-card-name` }>{item.strMeal}</p>
+          <img
+            src={ item.strMealThumb }
+            alt={ item.strMealThumb }
+            data-testid={ `${index}-card-img` }
+          />
+        </div>
+      ));
+    }
     if (data.length === 1) {
       history.push(`/comidas/${data[0].idMeal}`);
     }
@@ -18,7 +75,7 @@ export default function MainFood({ history }) {
         <p data-testid={ `${index}-card-name` }>{item.strMeal}</p>
         <img
           src={ item.strMealThumb }
-          alt="receita"
+          alt={ item.strMealThumb }
           data-testid={ `${index}-card-img` }
         />
       </div>
@@ -29,9 +86,8 @@ export default function MainFood({ history }) {
     <div>
       <h4>Meals</h4>
       <Header history={ history } title="Comidas" />
-      {
-        data && renderMeal()
-      }
+      {buttonCategories()}
+      {renderMeal()}
       <Footer />
     </div>
   );
