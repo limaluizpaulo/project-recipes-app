32f0e1035eb3bb196b5e158cbb45e54fe2b19f44
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-07-04 13:05:40 -0300

    Done: Tela de Pefil

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,9 +6,33 @@ export default function Profile() {
   return (
     <div>
       <span data-testid="profile-email">{ email }</span>
-      <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
-      <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
-      <button type="button" data-testid="profile-logout-btn">Sair</button>
+      <Link to="/receitas-feitas">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => localStorage.clear() }
+        >
+          Sair
+
+        </button>
+      </Link>
     </div>
   );
 }
