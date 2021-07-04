6b05e4aa9e87b4564323f73d97a7298d58b1234b
Carlos <cemargato@gmail.com>
2021-07-04 19:43:41 -0300

    aplicando-css

--- src/Pages/ExploreDrinks.jsx
+++ src/Pages/ExploreDrinks.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import '../styles/Explore.css';
 
 function ExploreDrinks() {
   return (
@@ -9,12 +10,14 @@ function ExploreDrinks() {
       <Link
         to="/explorar/bebidas/ingredientes"
         data-testid="explore-by-ingredient"
+        className="btn-explore"
       >
         Por Ingredientes
       </Link>
       <Link
         to="/explorar/bebidas/suprise"
         data-testid="explore-surprise"
+        className="btn-explore"
       >
         Me Surpreenda!
       </Link>
--- src/Pages/ExploreFoods.jsx
+++ src/Pages/ExploreFoods.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Explore.css';
 
 function ExploreFoods() {
   return (
@@ -7,18 +8,21 @@ function ExploreFoods() {
       <Link
         to="/explorar/comidas/ingredientes"
         data-testid="explore-by-ingredient"
+        className="btn-explore"
       >
         Por Ingredientes
       </Link>
       <Link
         to="/explorar/comidas/area"
         data-testid="explore-by-area"
+        className="btn-explore"
       >
         Por Local de Origem
       </Link>
       <Link
         to="/explorar/comidas/suprise"
         data-testid="explore-surprise"
+        className="btn-explore"
       >
         Me Surpreenda!
       </Link>
