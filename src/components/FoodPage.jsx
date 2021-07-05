2a5422f31bc41953687a16c11c58f20fd2765fd6
Camila Arruda <camilaam1802@gmail.com>
2021-07-05 19:55:03 -0300

    requisito 14, mais alterações

--- src/components/FoodPage.jsx
+++ src/components/FoodPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import SBElements from './SBElements';
 
 function FoodPage() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <SBElements />
+    </div>
   );
 }
 
--- src/components/SBElements.jsx
+++ src/components/SBElements.jsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+/* function foo() {
+  const alert = myCustomLib.customAlert;
+  alert();
+} */
 
 function SBElements() {
+  const [ingredients, setIngredients] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const [FirstLetter, setFirstLetter] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const getIngredients = () => {
+    const endpoint = 'https://www.themealdb.com/api/json/v1/1/filter.php?i={ingrediente}';
+    fetch(endpoint)
+      .then((response) => response.json()
+        .then((results) => setIngredients(results.meals)));
+    console.log('feito requisição 1');
+  };
+
+  const getRecipes = () => {
+    const endpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s={nome}';
+    fetch(endpoint)
+      .then((response) => response.json()
+        .then((results) => setRecipes(results.meals)));
+    console.log('feito requisição 2');
+  };
+
+  const getFirstLetter = () => {
+    const endpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?f={primeira-letra}';
+    fetch(endpoint)
+      .then((response) => response.json()
+        .then((results) => setFirstLetter(results.meals)));
+    console.log('feito requisição 3');
+  };
+
+  const handleClick = () => {
+    const searchInput = document.getElementById('searchInput').innerText;
+    switch (filter) {
+    case 'ingredientes':
+      getIngredients();
+      break;
+    case 'recipe':
+      getRecipes();
+      break;
+    case 'firstLetter':
+      if (searchInput.length !== 1) {
+        alert('Sua busca deve conter somente 1 (um) caracter');
+      } else {
+        getFirstLetter();
+      }
+      break;
+    default:
+      console.log('nada aconteceu');
+      break;
+    }
+  };
+
   return (
     <div>
       <input
+        id="searchInput"
         type="text"
         data-testid="search-input"
       />
@@ -11,35 +68,42 @@ function SBElements() {
         htmlFor="ingredientes"
       >
         {'Ingredientes '}
+        <input
+          id="ingredientes"
+          name="searchBy"
+          type="radio"
+          data-testid="ingredient-search-radio"
+          onChange={ (e) => setFilter(e.target.id) }
+        />
       </label>
-      <input
-        name="ingredientes"
-        type="radio"
-        data-testid="ingredient-search-radio"
-      />
       <label
-        htmlFor="recipe-name-search"
+        htmlFor="recipe"
       >
         {'Receita '}
+        <input
+          id="recipe"
+          name="searchBy"
+          type="radio"
+          data-testid="name-search-radio"
+          onChange={ (e) => setFilter(e.target.id) }
+        />
       </label>
-      <input
-        name="recipe-name-search"
-        type="radio"
-        data-testid="name-search-radio"
-      />
       <label
-        htmlFor="first-letter-search"
+        htmlFor="firstLetter"
       >
         {'Primeira letra '}
       </label>
       <input
-        name="first-letter-search"
+        id="firstLetter"
+        name="searchBy"
         type="radio"
         data-testid="first-letter-search-radio"
+        onChange={ (e) => setFilter(e.target.id) }
       />
       <button
         type="button"
         data-testid="exec-search-btn"
+        onClick={ handleClick }
       >
         Pesquisar
       </button>
