6036fda98ef54218741597c07b40b2a571769acd
Italo <italomatos764@gmail.com>
2021-07-05 13:44:03 -0400

    Done recipes page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Login, MainPage } from './pages';
-import AppRecipeProvider from './context/AppRecipeProvider';
+import { Login, MainPage, DoneRecipes } from './pages';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -10,15 +9,14 @@ import './App.css';
 function App() {
   return (
     <Provider store={ store }>
-      <AppRecipeProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ Login } />
-            <Route path="/comidas" component={ MainPage } />
-            <Route path="/bebidas" component={ MainPage } />
-          </Switch>
-        </BrowserRouter>
-      </AppRecipeProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/comidas" component={ MainPage } />
+          <Route path="/bebidas" component={ MainPage } />
+          <Route path="/receitas-feitas" component={ DoneRecipes } />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   );
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import user from './user';
+import done from './done';
 
-const listReducer = combineReducers({ user });
+const listReducer = combineReducers({ user, done });
 
 export default listReducer;
