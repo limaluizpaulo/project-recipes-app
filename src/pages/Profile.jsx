d0c75576f1960ebaf68ba45931ef9fc241259372
denis-rossati <denis.rossati@hotmail.com>
2021-07-05 17:11:52 -0300

    Disponibiliza componentes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
   return (
     <main>
-      <h1>ola</h1>
+      <p data-testid="profile-email">{ email }</p>
+      <button data-testid="profile-done-btn">Receitas Feitas</button>
+      <button data-testid="profile-favorite-btn">Receitas Favoritas</button>
+      <button data-testid="profile-logout-btn">Sair</button>
     </main>
   );
 }
