bccc31381fce61e2ac4215736e65f0c9e0fbc036
Antenor <antenorzapata@gmail.com>
2021-07-05 10:38:17 -0300

    requisitos 47-49

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -2,29 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import useIngredients from '../hooks/useIngredients';
 
-export default function Ingredients({ recipe }) {
+export default function Ingredients({ recipe, radioBtn }) {
   const { createIngredientsAndMesure } = useIngredients();
   const ingredients = createIngredientsAndMesure(recipe, 'ingredients');
   const mesure = createIngredientsAndMesure(recipe, 'mesure');
 
   return (
     <div>
-      {ingredients.map((ingredient, index) => (
+      {ingredients.map((ingredient, index) => (radioBtn ? (
+        <div key={ index } className="ingredients-check">
+          <label htmlFor="ingre" data-testid={ `${index}-ingredient-step` }>
+            <input
+              id={ index }
+              value={ index }
+              type="checkbox"
+            />
+            {ingredient}
+          </label>
+        </div>
+      ) : (
         <p
           key={ index }
           data-testid={ `${index}-ingredient-name-and-measure` }
         >
           {ingredient}
-          {' '}
           -
-          {' '}
           {mesure[index]}
         </p>
-      ))}
+      )))}
     </div>
   );
 }
 
+Ingredients.defaultProps = {
+  radioBtn: null,
+};
+
 Ingredients.propTypes = {
   recipe: PropTypes.shape({}).isRequired,
+  radioBtn: PropTypes.bool,
 };
