f24159775d5373b8819ca70ce15ac4ba1e7c9d32
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-07-04 22:21:57 -0300

    Feat: Logica desenvolvida dos filtros de Drinks

--- src/pages/MainDrink.jsx
+++ src/pages/MainDrink.jsx
@@ -1,20 +1,25 @@
 import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RecipeContext from '../context/Context';
 import { apiRequestDrinks } from '../services/api/getMealsDrink';
-import { apiCategoriesDrinks } from '../services/api/getCategories';
+import { apiCategoriesDrinks } from '../services/api/getList';
+import { requestCategoriesDrink } from '../services/api/getCategories';
 
 // Tela principal de receitas de bebidas: /bebidas;
 export default function MainDrink({ history }) {
   const { data } = useContext(RecipeContext);
+  const [valueButton, setValueButton] = useState('');
   const [dataDrinks, setDataDrinks] = useState([]);
   const [filterDrinks, setFiltersDrinks] = useState([]);
   const [categoriesDrinks, setCategoriesDrinks] = useState([]);
   const [filterCategoriesDrinks, setFilterCategoriesDrinks] = useState([]);
-
-  console.log(data);
+  const [dataCategories, setDataCategories] = useState([]);
+  const [filterDataCategories, setFilterDataCategories] = useState([]);
+  const [toogle, setToogle] = useState(false);
+  const [allValue, setAllValue] = useState('');
 
   useEffect(() => {
     async function apiRequest() {
@@ -25,11 +30,6 @@ export default function MainDrink({ history }) {
   }, []);
 
   useEffect(() => {
-    const FILTER_NUMBER = 12;
-    setFiltersDrinks(dataDrinks.slice(0, FILTER_NUMBER));
-  }, [dataDrinks]);
-
-  useEffect(() => {
     async function apiRequestCategories() {
       const { drinks } = await apiCategoriesDrinks();
       setCategoriesDrinks(drinks);
@@ -38,16 +38,49 @@ export default function MainDrink({ history }) {
   }, []);
 
   useEffect(() => {
+    async function categories() {
+      if (valueButton !== '') {
+        const { drinks: cateDrinks } = await requestCategoriesDrink(valueButton) || [];
+        setDataCategories(cateDrinks);
+      }
+    }
+    categories();
+  }, [valueButton]);
+
+  useEffect(() => {
+    const FILTER_NUMBER = 12;
     const NUMBER_FILTER_CATEGORIES = 5;
+    setFiltersDrinks(dataDrinks.slice(0, FILTER_NUMBER));
     setFilterCategoriesDrinks(categoriesDrinks.slice(0, NUMBER_FILTER_CATEGORIES));
-  }, [categoriesDrinks]);
+    setFilterDataCategories(dataCategories.slice(0, FILTER_NUMBER));
+  }, [dataDrinks, categoriesDrinks, dataCategories]);
+
+  function handleOnClick({ target }) {
+    setValueButton(target.value);
+    setAllValue('');
+    if (target.value !== valueButton) {
+      setToogle(true);
+    }
+    if (target.value === valueButton) {
+      setToogle(false);
+      setValueButton('');
+    }
+  }
+
+  async function handleAllOnclick({ target }) {
+    setAllValue(target.value);
+    setValueButton('');
+    setToogle(false);
+  }
 
   function buttonCategories() {
     return filterCategoriesDrinks.map((categ, i) => (
       <button
         type="button"
         key={ i }
+        value={ categ.strCategory }
         data-testid={ `${categ.strCategory}-category-filter` }
+        onClick={ handleOnClick }
       >
         {categ.strCategory}
       </button>
@@ -55,19 +88,49 @@ export default function MainDrink({ history }) {
   }
 
   function renderDrinks() {
-    if (data.length === 0) {
+    if (allValue === 'All' && valueButton === '') {
+      return filterDrinks.map((itemAll, indexAll) => (
+        <div key={ indexAll } data-testid={ `${indexAll}-recipe-card` }>
+          <Link to={ `/comidas/${itemAll.idDrink}` }>
+            <p data-testid={ `${indexAll}-card-name` }>{itemAll.strDrink}</p>
+            <img
+              src={ itemAll.strDrinkThumb }
+              alt={ itemAll.strDrinkThumb }
+              data-testid={ `${indexAll}-card-img` }
+            />
+          </Link>
+        </div>
+      ));
+    }
+    if (data.length === 0 && valueButton === '') {
       return filterDrinks.map((item, index) => (
         <div key={ index } data-testid={ `${index}-recipe-card` }>
-          <p
-            data-testid={ `${index}-card-name` }
-          >
-            {item.strDrink}
-          </p>
-          <img
-            src={ item.strDrinkThumb }
-            alt={ item.strDrinkThumb }
-            data-testid={ `${index}-card-img` }
-          />
+          <Link to={ `/bebidas/${item.idDrink}` }>
+            <p
+              data-testid={ `${index}-card-name` }
+            >
+              {item.strDrink}
+            </p>
+            <img
+              src={ item.strDrinkThumb }
+              alt={ item.strDrinkThumb }
+              data-testid={ `${index}-card-img` }
+            />
+          </Link>
+        </div>
+      ));
+    }
+    if (valueButton !== '' && toogle) {
+      return filterDataCategories.map((itemValue, i) => (
+        <div key={ i } data-testid={ `${i}-recipe-card` }>
+          <Link to={ `/bebidas/${itemValue.idDrink}` }>
+            <p data-testid={ `${i}-card-name` }>{itemValue.strDrink}</p>
+            <img
+              src={ itemValue.strDrinkThumb }
+              alt={ itemValue.strDrinkThumb }
+              data-testid={ `${i}-card-img` }
+            />
+          </Link>
         </div>
       ));
     }
@@ -77,15 +140,17 @@ export default function MainDrink({ history }) {
     if (data.length > 1) {
       return data.map((item, index) => (
         <div key={ index } data-testid={ `${index}-recipe-card` }>
-          <p data-testid={ `${index}-card-name` }>
-            {' '}
-            {item.strDrink}
-          </p>
-          <img
-            src={ item.strDrinkThumb }
-            alt={ item.strDrinkThumb }
-            data-testid={ `${index}-card-img` }
-          />
+          <Link to={ `/bebidas/${item.idDrink}` }>
+            <p data-testid={ `${index}-card-name` }>
+              {' '}
+              {item.strDrink}
+            </p>
+            <img
+              src={ item.strDrinkThumb }
+              alt={ item.strDrinkThumb }
+              data-testid={ `${index}-card-img` }
+            />
+          </Link>
         </div>
       ));
     }
@@ -95,7 +160,17 @@ export default function MainDrink({ history }) {
     <div>
       <h4>Drinks</h4>
       <Header history={ history } title="Bebidas" />
-      {buttonCategories()}
+      <div>
+        <button
+          data-testid="All-category-filter"
+          type="button"
+          value="All"
+          onClick={ handleAllOnclick }
+        >
+          All
+        </button>
+        {buttonCategories()}
+      </div>
       {renderDrinks()}
       <Footer />
     </div>
