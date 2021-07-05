7c42d10b7720759eb0a41a558736b66196537e08
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2021-07-05 12:14:41 -0300

    main-group-21-tela-de-explorar-comidas-e-bebidas

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function ExploreDrinks() {
   return (
-    <div>
-      <Header title="Explorar Bebidas" />
+    <>
+      <Header />
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas/:id">
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
       <Footer />
-      <h1>explore drinks</h1>
-    </div>
+    </>
   );
 }
--- src/pages/ExploreMeals.jsx
+++ src/pages/ExploreMeals.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ExploreMeals() {
   return (
-    <div>
-      <Header title="Explorar Comidas" />
-      <h1>explore meals</h1>
+    <>
+      <Header />
+      <Link to="/explorar/comidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/area">
+        <button
+          type="button"
+          data-testid="explore-by-area"
+        >
+          Por Local de Origem
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/:id">
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
       <Footer />
-    </div>
+    </>
   );
 }
