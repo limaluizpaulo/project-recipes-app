f0ee1c6f591d7a8472ec71eb8a142ea28c340643
walterlbf <walter.lbf.f@gmail.com>
2021-07-05 18:14:59 -0400

    voltei do inicio

--- src/Pages/Detalhes.jsx
+++ src/Pages/Detalhes.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { Container, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
