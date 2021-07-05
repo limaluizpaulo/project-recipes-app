03a713f28831034f95b9ee645afa65c074968f90
denis-rossati <denis.rossati@hotmail.com>
2021-07-05 17:22:44 -0300

    Finaliza o perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Profile() {
   const { email } = JSON.parse(localStorage.getItem('user'));
+  const clearLocalStorage = () => {
+    localStorage.clear();
+  };
+
   return (
     <main>
-      <p data-testid="profile-email">{ email }</p>
-      <button data-testid="profile-done-btn">Receitas Feitas</button>
-      <button data-testid="profile-favorite-btn">Receitas Favoritas</button>
-      <button data-testid="profile-logout-btn">Sair</button>
+      <p data-testid="profile-email">{email}</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        <Link to="/receitas-feitas">
+          Receitas Feitas
+        </Link>
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        <Link to="/receitas-favoritas">
+          Receitas Favoritas
+        </Link>
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ clearLocalStorage }
+      >
+        <Link to="/">
+          Sair
+        </Link>
+      </button>
     </main>
   );
 }
