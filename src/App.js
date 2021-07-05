6f8b64527056272bba0d712ab6452efc5fc0ba23
Camila Arruda <camilaam1802@gmail.com>
2021-07-05 18:42:25 -0300

    requisito 14 em andamento

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
 import FoodPage from './components/FoodPage';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Switch>
       <Route exact path="/" render={ (props) => <Login { ...props } /> } />
       <Route exact path="/comidas" component={ FoodPage } />
+      <Route exact path="/profile" render={ (props) => <Profile { ...props } /> } />
     </Switch>
     // </div>
   );
