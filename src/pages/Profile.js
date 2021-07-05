846360dd104321b4811a6f01dbec20b6b4a8351c
Lara Capila <laracapilak@gmail.com>
2021-07-05 16:29:09 -0300

    Tela de perfil - feita (agora sim)

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,49 +1,49 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const history = useHistory();
   const email = JSON.parse(localStorage.getItem('user'));
 
-  // https://developer.mozilla.org/pt-BR/docs/Web/API/Storage/clear
-  const handleClick = () => {
-    localStorage.clear();
+  const clickButton = ({ target: { name } }) => {
+    const domain = name === 'login' ? '' : name;
+    history.push(`/${domain}`);
   };
 
   return (
     <>
       <Header />
       <p data-testid="profile-email">
-        { Object.values(email) }
+        { email && Object.values(email) }
       </p>
-      <Link to="/receitas-feitas">
-        <Button
-          data-testid="profile-done-btn"
-          type="button"
-        >
-          Receitas Feitas
-        </Button>
-      </Link>
-      <Link to="/receitas-favoritas">
-        <Button
-          data-testid="profile-favorite-btn"
-          type="button"
-        >
-          Receitas Favoritas
-        </Button>
-      </Link>
-      <Link to="/">
-        <Button
-          data-testid="profile-logout-btn"
-          type="button"
-          onClick={ handleClick }
-        >
-          Sair
-        </Button>
-      </Link>
+      <Button
+        data-testid="profile-done-btn"
+        name="receitas-feitas"
+        onClick={ clickButton }
+        type="button"
+      >
+        Receitas Feitas
+      </Button>
+      <Button
+        data-testid="profile-favorite-btn"
+        type="button"
+        name="receitas-favoritas"
+        onClick={ clickButton }
+      >
+        Receitas Favoritas
+      </Button>
+      <Button
+        data-testid="profile-logout-btn"
+        type="button"
+        name="login"
+        onClick={ (e) => { clickButton(e); localStorage.clear(); } }
+      >
+        Sair
+      </Button>
       <Footer />
     </>
   );
