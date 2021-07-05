da6a28c3aabf811a442fec8352f94e79c83a1d45
isTorres <ismaeltorres612@gmail.com>
2021-07-04 23:15:18 -0300

    ingredients component refatored

--- src/components/MealsByIngredients.jsx
+++ src/components/MealsByIngredients.jsx
@@ -1,27 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import fetchFood, { BY_INGREDIENT } from '../services/FoodAPI';
+import fetchFood from '../services/FoodAPI';
 import fetchDrink from '../services/DrinkAPI';
 import Footer from './Footer';
 
 export default function MealsByIngredients() {
+  const MEALS = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+  const COCKTAIL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
   const history = useHistory();
   const path = history.location.pathname;
-  const [ingredients, setIngredients] = useState(null);
+  const [ingredients, setIngredients] = useState([]);
   // const [drink, setDrink] = useState(null);
 
   useEffect(() => {
     if (path === '/explorar/comidas/ingredientes') {
-      fetchFood(BY_INGREDIENT)
+      fetchFood(MEALS)
+      // .then((res) => console.log(res))
         .then((res) => setIngredients(res));
-    } if (path === '/explorar/bebidas/ingredientes') {
-      fetchDrink(BY_INGREDIENT)
+      console.log('comidas');
+    } else if (path === '/explorar/bebidas/ingredientes') {
+      fetchDrink(COCKTAIL)
+      // .then((res) => console.log(res))
         .then((res) => setIngredients(res));
+      console.log('bebidas');
     }
   }, [path]);
 
   return (
     <div>
+      {/* {console.log(path)} */}
       {console.log(ingredients)}
       <Footer />
     </div>
