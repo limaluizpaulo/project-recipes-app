3c9acf0fd7cb4cdd94e19b1e58cadb33a09a0549
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2021-07-05 18:52:30 -0300

    adicionada funcionalidade de copiar para o clipboard.

--- src/components/HeaderDetails.jsx
+++ src/components/HeaderDetails.jsx
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useLocation } from 'react-router';
 import DrinksContext from '../context/DrinksContext';
 import RecipesContext from '../context/RecipesContext';
+import '../styles/DoneRecipes.css';
 
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
@@ -9,12 +10,19 @@ import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 function HeaderDetails() {
   const { drinkDetails } = useContext(DrinksContext);
   const { foodDetails } = useContext(RecipesContext);
+  const [copied, setCopied] = useState(false);
 
   const { pathname } = useLocation();
 
   const NUMBER_TO_VERIFICATION = -1;
 
   const getDrinksDetails = pathname.indexOf('bebidas') > NUMBER_TO_VERIFICATION;
+
+  const shareRecipe = () => {
+    // https://stackoverflow.com/questions/39501289/in-reactjs-how-to-copy-text-to-clipboard
+    navigator.clipboard.writeText(`http://localhost:3000${pathname}`);
+    setCopied(true);
+  };
 
   return getDrinksDetails ? (
     <header>
@@ -29,9 +37,20 @@ function HeaderDetails() {
           <span data-testid="recipe-category">{ drinkDetails.strAlcoholic }</span>
         </div>
         <div className="Like-and-Share">
-          <button type="button" data-testid="share-btn">
+          <button
+            type="button"
+            data-testid="share-btn"
+            onClick={ () => shareRecipe() }
+          >
             <img src={ shareIcon } alt="Icon Share" />
           </button>
+          <span
+            className={ `${copied
+              ? 'alert-show' : 'alert-hidden'}` }
+            onTransitionEnd={ () => setCopied(false) }
+          >
+            Link copiado!
+          </span>
           <button type="button" data-testid="favorite-btn">
             <img src={ whiteHeartIcon } alt="Icon Like" />
           </button>
@@ -52,9 +71,20 @@ function HeaderDetails() {
           <span data-testid="recipe-category">{ foodDetails.strCategory }</span>
         </div>
         <div className="Like-and-Share">
-          <button type="button" data-testid="share-btn">
+          <button
+            type="button"
+            data-testid="share-btn"
+            onClick={ () => shareRecipe() }
+          >
             <img src={ shareIcon } alt="Icon Share" />
           </button>
+          <span
+            className={ `${copied
+              ? 'alert-show' : 'alert-hidden'}` }
+            onTransitionEnd={ () => setCopied(false) }
+          >
+            Link copiado!
+          </span>
           <button type="button" data-testid="favorite-btn">
             <img src={ whiteHeartIcon } alt="Icon Like" />
           </button>
