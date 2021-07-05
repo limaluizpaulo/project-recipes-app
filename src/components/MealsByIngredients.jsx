7586e3dc4cea8393344a4cb1183efbf053efe286
isTorres <ismaeltorres612@gmail.com>
2021-07-05 17:15:58 -0300

    refatored function to get ingredients

--- src/components/MealsByIngredients.jsx
+++ src/components/MealsByIngredients.jsx
@@ -4,26 +4,46 @@ import fetchFood from '../services/FoodAPI';
 import fetchDrink from '../services/DrinkAPI';
 
 export default function MealsByIngredients() {
-  const MEALS = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
-  const COCKTAIL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
+  const MEALS_INGREDIENTS = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+  const COCKTAIL_INGREDIENTS = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
   const history = useHistory();
   const path = history.location.pathname;
-  const [ingredients, setIngredients] = useState([]);
-  // const [drink, setDrink] = useState(null);
+  const [ingredients, setIngredients] = useState(null);
 
   useEffect(() => {
     if (path === '/explorar/comidas/ingredientes') {
-      fetchFood(MEALS)
-      // .then((res) => console.log(res))
+      fetchFood(MEALS_INGREDIENTS)
         .then((res) => setIngredients(res));
       console.log('comidas');
-    } else if (path === '/explorar/bebidas/ingredientes') {
-      fetchDrink(COCKTAIL)
-      // .then((res) => console.log(res))
+    } else {
+      fetchDrink(COCKTAIL_INGREDIENTS)
         .then((res) => setIngredients(res));
       console.log('bebidas');
     }
   }, [path]);
+
+  function renderIngredients() {
+    const strIngredient = path === '/explorar/comidas/ingredientes'
+      ? 'strIngredient' : 'strIngredient1';
+    return (
+      <>
+        {ingredients.map((el) => (
+          <div key={ el[strIngredient] }>
+            <h3
+              data-testid={ `${el[strIngredient]}-card-name` }
+            >
+              { el[strIngredient] }
+            </h3>
+            <img
+              data-testid={ `${el[strIngredient]}-card-img` }
+              src={ `https://www.themealdb.com/images/ingredients/${el[strIngredient]}-Small.png` }
+              alt={ el[strIngredient] }
+            />
+          </div>
+        ))}
+      </>
+    );
+  }
 
   if (ingredients === null) {
     return (
@@ -35,17 +55,7 @@ export default function MealsByIngredients() {
 
   return (
     <div>
-      {console.log(ingredients)}
-      {ingredients.map((el) => (
-        <div key={ el.idIngredient }>
-          <h3 data-testid={ `${el.strIngredient}-card-name` }>{ el.strIngredient }</h3>
-          <img
-            data-testid={ `${el.strIngredient}-card-img` }
-            src={ `https://www.themealdb.com/images/ingredients/${el.strIngredient}-Small.png` }
-            alt={ el.strIngredient }
-          />
-        </div>
-      ))}
+      { renderIngredients() }
     </div>
   );
 }
