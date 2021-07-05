74822b2c3fb8057d407116a043c0b475c9dd3695
RenatoGSouza <renatomacedo1995@gmail.com>
2021-07-05 19:15:59 -0300

    add testid

--- src/Pages/DrinkDetails.jsx
+++ src/Pages/DrinkDetails.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 export default function DrinkDetails() {
+  const index = 0;
   return (
     <div>
-      Detalhes bebida
+      <img data-testid="recipe-photo" alt="comida" />
+      <h1 data-testid="recipe-title">title</h1>
+      <button type="button" data-testid="share-btn">share</button>
+      <button type="button" data-testid="favorite-btn">favorite</button>
+      <h6 data-testid="recipe-category">category</h6>
+      <ul>
+        <li data-testid={ `${index}-ingredient-name-and-measure` }>show</li>
+      </ul>
+      <p data-testid="instructions">instructions</p>
+      <img data-testid="video" alt="video" />
+      <div data-testid={ `${index}-recomendation-card` }>
+        cards
+      </div>
+      <button type="button" data-testid="start-recipe-btn">iniciar receita</button>
     </div>
   );
 }
