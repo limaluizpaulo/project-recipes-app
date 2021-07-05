0ba5dcdd0c540c6684cd482f51517161e07b5e34
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-07-05 18:34:20 -0300

    incluir botoes e renderizar email do local storage

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -3,11 +3,50 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 class Perfil extends Component {
+  constructor() {
+    super();
+    this.mailUser = this.mailUser.bind(this);
+  }
+
+  mailUser() {
+    // codigo para localStorage conseguido https://josiaspereira.com.br/como-usar-localstorage-no-reactjs/
+    const emailStorage = localStorage.getItem('user');
+    const objEmail = JSON.parse(emailStorage);
+    const { email } = objEmail;
+    if (email !== null) {
+      return email;
+    }
+    return null;
+  }
+
   render() {
     return (
       <>
         <section>
           <Header title="Perfil" searchIcon />
+        </section>
+        <section>
+          <p data-testid="profile-email">
+            {this.mailUser}
+          </p>
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Receitas Feitas
+          </button>
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Receitas Favoritas
+          </button>
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+          >
+            Sair
+          </button>
         </section>
         <Footer />
       </>
