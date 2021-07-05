6da154a6e917071001fa36f01e6c13e526bf6692
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-05 19:15:22 -0300

    renderiza comidas de acordo com area de origem

--- src/pages/ExploreFoodsArea.jsx
+++ src/pages/ExploreFoodsArea.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { fetchAreaRecipes } from '../services/RecipesServices';
+import { fetchAreaRecipes, fetchRecipesByArea } from '../services/RecipesServices';
 
 function ExploreFoodsArea() {
   const [areas, setAreas] = useState([]);
+  const [valueArea, setValueArea] = useState('All');
+  const [recipesByArea, setRecipesByArea] = useState([]);
+
+  const history = useHistory();
 
   useEffect(() => {
     async function getAreas() {
@@ -17,23 +22,65 @@ function ExploreFoodsArea() {
     getAreas();
   }, []);
 
+  useEffect(() => {
+    async function getRecipesByArea() {
+      if (valueArea !== 'All') {
+        const NUMBER_OF_RECIPES = 12;
+        const data = await fetchRecipesByArea(valueArea);
+        const twelveRecipes = data.meals.slice(0, NUMBER_OF_RECIPES);
+        setRecipesByArea(twelveRecipes);
+      }
+    }
+    getRecipesByArea();
+  }, [valueArea]);
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setValueArea(value);
+  }
+
+  function handleClick(idMeal) {
+    history.push(`/comidas/${idMeal}`);
+  }
+
   return (
     <>
       <Header profile name="Explorar Origem" search />
-      <select data-testid="explore-by-area-dropdown">
-        <option data-testid="All-option">All</option>
+      <select data-testid="explore-by-area-dropdown" onChange={ handleChange }>
+        <option data-testid="All-option" value="All">All</option>
         {
           areas.map((area, index) => (
             <option
               data-testid={ `${area}-option` }
               key={ index }
-
+              value={ area }
             >
               {area}
             </option>
           ))
         }
       </select>
+
+      <section>
+        {
+          recipesByArea.map((recipe, index) => (
+            <button
+              type="button"
+              data-testid={ `${index}-recipe-card` }
+              key={ index }
+              onClick={ () => handleClick(recipe.idMeal) }
+            >
+              <img
+                data-testid={ `${index}-card-img` }
+                src={ recipe.strMealThumb }
+                alt={ recipe.strMeal }
+              />
+              <h6 data-testid={ `${index}-card-name` }>{ recipe.strMeal }</h6>
+
+            </button>
+          ))
+        }
+      </section>
       <Footer />
     </>
   );
