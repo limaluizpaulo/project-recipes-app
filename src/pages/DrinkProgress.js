565914415247da37a6e6f2a5ce89abc910a59f81
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-07-06 19:30:39 -0300

    req 51 concluido

--- src/pages/DrinkProgress.js
+++ src/pages/DrinkProgress.js
@@ -1,18 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import copy from 'clipboard-copy';
 import { recipeById } from '../services/requests';
 import { renderCheckBox } from '../utils';
+import { checkFavoriteId } from '../services/localStorage';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 const DrinkProgress = ({ match }) => {
   const {
     params: { id },
   } = match;
   const [drink, setDrink] = useState({});
+  const [msgCopy, setMsgCopy] = useState(false);
+  const [iconFavorit, setIconFavorit] = useState(false);
+
+  const isFavorite = checkFavoriteId(id);
 
   useEffect(() => {
     recipeById(id).then(setDrink);
   }, [id, setDrink]);
+
+  const blackOrWhite = () => (iconFavorit ? blackHeartIcon : whiteHeartIcon);
+
+  useEffect(() => {
+    if (isFavorite) setIconFavorit(true);
+  }, [isFavorite]);
+
+  useEffect(() => {
+    recipeById(id).then(setDrink);
+  }, [id, setDrink]);
+
+  const addFavorite = () => {
+    const favorites = localStorage.favoriteRecipes
+      ? JSON.parse(localStorage.favoriteRecipes) : [];
+
+    if (!iconFavorit) {
+      const { idDrink, strArea, strCategory, strDrink, strDrinkThumb } = drink;
+      const add = [...favorites, {
+        id: idDrink,
+        type: 'bebida',
+        area: strArea || '',
+        category: strCategory,
+        alcoholicOrNot: 'Alcoholic',
+        name: strDrink,
+        image: strDrinkThumb,
+      }];
+      localStorage.favoriteRecipes = JSON.stringify(add);
+    } else {
+      const remove = favorites.filter(({ id: idL }) => idL !== id);
+      localStorage.favoriteRecipes = JSON.stringify(remove);
+    }
+    setIconFavorit(!iconFavorit);
+  };
 
   console.log(drink);
   return (
@@ -30,11 +71,17 @@ const DrinkProgress = ({ match }) => {
       </ul>
       <p data-testid="instructions">{drink.strInstructions}</p>
       <p data-testid="0-recomendation-card">recomendation</p>
-      <button type="button" data-testid="share-btn">
-        Compartilhar
+      <button
+        onClick={ () => copy(`http://localhost:3000/bebidas/${id}`).then(() => {
+          setMsgCopy(true);
+        }) }
+        type="button"
+        data-testid="share-btn"
+      >
+        { msgCopy ? 'Link copiado!' : 'Compartilhar' }
       </button>
-      <button type="button" data-testid="favorite-btn">
-        Favoritar
+      <button onClick={ addFavorite } type="button">
+        <img data-testid="favorite-btn" src={ blackOrWhite() } alt={ blackOrWhite() } />
       </button>
       <Link to="/receitas-feitas">
         <button type="button" data-testid="finish-recipe-btn">
