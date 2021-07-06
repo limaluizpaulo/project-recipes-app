b40e311014e126563a9be034deb3f328335ecb16
Miguel Dantas <miiguelalb@gmail.com>
2021-07-06 17:22:36 -0300

    Todos os requisitos da tela de explorar

--- src/pages/Explorar.jsx
+++ src/pages/Explorar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,7 +7,12 @@ export default function Explorar() {
   return (
     <section>
       <Header />
-      Explorar
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-food">Explorar Comidas</button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+      </Link>
       <Footer />
     </section>
   );
