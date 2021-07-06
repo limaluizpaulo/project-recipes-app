01a5ab6febf0423d040c60c48253e15fcc273d17
denis-rossati <denis.rossati@hotmail.com>
2021-07-06 17:23:10 -0300

    Excluindo redux desnecessario

--- src/actions/saveCardsContent.js
+++
@@ -1,8 +0,0 @@
-export const CARD_CONTENT = 'card content';
-
-const saveContentAction = (content) => ({
-  type: CARD_CONTENT,
-  content,
-});
-
-export default saveContentAction;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,10 @@ import { combineReducers } from 'redux';
 import user from './user';
 import done from './done';
 import recipes from './recipes';
-import saveCardsContent from './cardsContent';
 
-const listReducer = combineReducers({ user, done, recipes, saveCardsContent });
+const listReducer = combineReducers({
+  user,
+  done,
+  recipes,
+});
 export default listReducer;
