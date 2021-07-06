a6cb8acbe7334a68ce6afe43d086d68648f54075
isTorres <ismaeltorres612@gmail.com>
2021-07-06 19:27:28 -0300

    refactor component

--- src/components/MealsByIngredients.jsx
+++ src/components/MealsByIngredients.jsx
@@ -1,43 +1,62 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import fetchFood from '../services/FoodAPI';
-import fetchDrink from '../services/DrinkAPI';
 
 export default function MealsByIngredients() {
   const MEALS_INGREDIENTS = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
   const COCKTAIL_INGREDIENTS = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
+  const comidasPath = '/explorar/comidas/ingredientes';
   const history = useHistory();
   const path = history.location.pathname;
   const [ingredients, setIngredients] = useState(null);
+  const limitMap = 12;
+
+  function handlefetch(endpoint) {
+    return fetch(`${endpoint}`)
+      .then((res) => res.json());
+  }
 
   useEffect(() => {
-    if (path === '/explorar/comidas/ingredientes') {
-      fetchFood(MEALS_INGREDIENTS)
+    if (path === comidasPath) {
+      handlefetch(MEALS_INGREDIENTS)
+        .then((res) => res.meals)
         .then((res) => setIngredients(res));
-      console.log('comidas');
     } else {
-      fetchDrink(COCKTAIL_INGREDIENTS)
+      handlefetch(COCKTAIL_INGREDIENTS)
+        .then((res) => res.drinks)
         .then((res) => setIngredients(res));
-      console.log('bebidas');
     }
   }, [path]);
 
   function renderIngredients() {
-    const strIngredient = path === '/explorar/comidas/ingredientes'
-      ? 'strIngredient' : 'strIngredient1';
+    if (path === comidasPath) {
+      return (
+        <>
+          {ingredients.slice(0, limitMap).map((el, i) => (
+            <div data-testid={ `${i}-ingredient-card` } key={ i }>
+              <h3 data-testid={ `${i}-card-name` }>
+                { el.strIngredient }
+              </h3>
+              <img
+                data-testid={ `${i}-card-img` }
+                src={ `https://www.themealdb.com/images/ingredients/${el.strIngredient}-Small.png` }
+                alt={ el.strIngredient }
+              />
+            </div>
+          ))}
+        </>
+      );
+    }
     return (
       <>
-        {ingredients.map((el) => (
-          <div key={ el[strIngredient] }>
-            <h3
-              data-testid={ `${el[strIngredient]}-card-name` }
-            >
-              { el[strIngredient] }
+        {ingredients.slice(0, limitMap).map((el, i) => (
+          <div data-testid={ `${i}-ingredient-card` } key={ i }>
+            <h3 data-testid={ `${i}-card-name` }>
+              { el.strIngredient1 }
             </h3>
             <img
-              data-testid={ `${el[strIngredient]}-card-img` }
-              src={ `https://www.themealdb.com/images/ingredients/${el[strIngredient]}-Small.png` }
-              alt={ el[strIngredient] }
+              data-testid={ `${i}-card-img` }
+              src={ `https://www.thecocktaildb.com/images/ingredients/${el.strIngredient1}-Small.png` }
+              alt={ el.strIngredient1 }
             />
           </div>
         ))}
