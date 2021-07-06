d98c2f294eff4c108c3572ab52b8e7772d0927cb
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-07-05 23:14:17 -0300

    resolvendo complexidade criando um componente

--- src/pages/FavoritesRecipes.jsx
+++ src/pages/FavoritesRecipes.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+
 import Header from '../components/Header';
-import shareIcon from '../images/shareIcon.svg';
-import unfavIcon from '../images/blackHeartIcon.svg';
 
 import '../styles/DoneRecipes.css';
+import Favorited from '../components/Favorited';
 
 function FavoritesRecipes() {
   const [favoriteList, setFavoriteList] = useState([]);
@@ -13,46 +12,6 @@ function FavoritesRecipes() {
   const [category, setCategory] = useState('all');
 
   useEffect(() => {
-    // const favoriteRecipes = [
-    //   {
-    //     id: '52771',
-    //     type: 'comida',
-    //     area: 'Italian',
-    //     category: 'Vegetarian',
-    //     alcoholicOrNot: '',
-    //     name: 'Spicy Arrabiata Penne',
-    //     image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    //   },
-    //   {
-    //     id: '178319',
-    //     type: 'bebida',
-    //     area: '',
-    //     category: 'Cocktail',
-    //     alcoholicOrNot: 'Alcoholic',
-    //     name: 'Aquamarine',
-    //     image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    //   },
-    //   {
-    //     id: '151555',
-    //     type: 'comida',
-    //     area: 'blabla',
-    //     category: 'blabla',
-    //     alcoholicOrNot: '',
-    //     name: 'blabla',
-    //     image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    //   },
-    //   {
-    //     id: '121',
-    //     type: 'bebida',
-    //     area: '',
-    //     category: 'blibli',
-    //     alcoholicOrNot: 'Alcoholic',
-    //     name: 'blibli',
-    //     image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    //   },
-    // ];
-    // localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
-
     const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
     if (favorites) {
       setFavoriteList(favorites);
@@ -83,103 +42,10 @@ function FavoritesRecipes() {
         favoritos.splice(i, 1);
       }
     }
-    console.log(favoritos);
     localStorage.setItem('favoriteRecipes', JSON.stringify(favoritos));
     const newFav = JSON.parse(localStorage.getItem('favoriteRecipes'));
     setFavoriteList(newFav);
   };
-
-  const renderAllRecipes = () => (
-    <main>
-      {favoriteList.map((favoriteRecipe, index) => (
-        <div key={ index } className="card">
-          <Link to={ `${favoriteRecipe.type}s/${favoriteRecipe.id}` }>
-            <img
-              src={ favoriteRecipe.image }
-              alt="recipe"
-              data-testid={ `${index}-horizontal-image` }
-              width="150px"
-            />
-            <h4 data-testid={ `${index}-horizontal-name` }>{favoriteRecipe.name}</h4>
-          </Link>
-          <h6 data-testid={ `${index}-horizontal-top-text` }>
-            {favoriteRecipe.area}
-            {favoriteRecipe.alcoholicOrNot}
-            {' - '}
-            {favoriteRecipe.category}
-          </h6>
-          <button type="button" onClick={ () => unfavoriteRecipe(index) }>
-            <img
-              src={ unfavIcon }
-              alt="unfav"
-              data-testid={ `${index}-horizontal-favorite-btn` }
-            />
-          </button>
-          <button type="button" onClick={ () => shareRecipe(favoriteRecipe, index) }>
-            <img
-              src={ shareIcon }
-              alt="share"
-              data-testid={ `${index}-horizontal-share-btn` }
-            />
-          </button>
-          <span
-            id={ index }
-            className={ `${copied && indexNumber === index
-              ? 'alert-show' : 'alert-hidden'}` }
-            onTransitionEnd={ () => setCopied(false) }
-          >
-            Link copiado!
-          </span>
-        </div>
-      ))}
-    </main>);
-
-  const renderByCategory = () => (
-    <main>
-      {favoriteList.filter((recipe, index) => recipe.type === category)
-        .map((favoriteRecipe, index) => (
-          <div key={ index } className="card">
-            <Link to={ `${favoriteRecipe.type}s/${favoriteRecipe.id}` }>
-              <img
-                src={ favoriteRecipe.image }
-                alt="recipe"
-                data-testid={ `${index}-horizontal-image` }
-                width="150px"
-              />
-              <h4 data-testid={ `${index}-horizontal-name` }>{favoriteRecipe.name}</h4>
-            </Link>
-            <h6 data-testid={ `${index}-horizontal-top-text` }>
-              {favoriteRecipe.area}
-              {favoriteRecipe.alcoholicOrNot}
-              {' - '}
-              {favoriteRecipe.category}
-            </h6>
-            <button type="button" onClick={ () => unfavoriteFilteredRecipe(favoriteRecipe.id) }>
-              <img
-                src={ unfavIcon }
-                alt="unfav"
-                data-testid={ `${index}-horizontal-favorite-btn` }
-              />
-            </button>
-            <button type="button" onClick={ () => shareRecipe(favoriteRecipe, index) }>
-              <img
-                src={ shareIcon }
-                alt="share"
-                data-testid={ `${index}-horizontal-share-btn` }
-              />
-            </button>
-            <span
-              id={ index }
-              className={ `${copied && indexNumber === index
-                ? 'alert-show' : 'alert-hidden'}` }
-              onTransitionEnd={ () => setCopied(false) }
-            >
-              Link copiado!
-            </span>
-          </div>
-        ))}
-    </main>
-  );
 
   return (
     <>
@@ -211,7 +77,16 @@ function FavoritesRecipes() {
           Drinks
         </button>
       </section>
-      {category === 'all' ? renderAllRecipes() : renderByCategory()}
+      <Favorited
+        favoriteList={ favoriteList }
+        copied={ copied }
+        setCopied={ setCopied }
+        indexNumber={ indexNumber }
+        category={ category }
+        shareRecipe={ shareRecipe }
+        unfavoriteRecipe={ unfavoriteRecipe }
+        unfavoriteFilteredRecipe={ unfavoriteFilteredRecipe }
+      />
     </>
   );
 }
