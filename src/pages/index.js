4b657d2dc4f5de9d838c16c53d4119ffe8ff8ec3
Cristian <cristian.bugs@gmail.com>
2021-07-06 19:38:49 -0300

    git commit -m"changes in index"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,14 @@
 export { default as Login } from './Login';
 export { default as Meals } from './Meals';
-export { default as MealsDetails } from './MealsDetails';
-export { default as DrinksDetails } from './DrinksDetails';
+// export { default as MealsDetails } from './MealsDetails';
+// export { default as DrinksDetails } from './DrinksDetails';
 export { default as Drinks } from './Drinks';
 export { default as Profile } from './Profile';
 export { default as Explore } from './Explore';
 export { default as ExploreMeals } from './ExploreMeals';
 export { default as ExploreDrinks } from './ExploreDrinks';
 export { default as MealsByIngredients } from './MealsByIngredients';
-<<<<<<< HEAD
 export { default as MealsByArea } from './MealsByArea';
-export { default as DrinksDetails } from './DrinksDetails';
-=======
-export { default as MealsByAreas } from './MealsByAreas';
->>>>>>> d6092079d668e0a8596d62ded9afcc864d7748e7
 export { default as DrinksByIngredients } from './DrinksByIngredients';
 export { default as Done } from './Done';
 export { default as Favorites } from './Favorites';
