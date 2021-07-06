c38965695160e39d64f2302e1ad79d73da1f1a51
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-07-06 03:55:31 -0300

    tentando resolver bugs do lint? vs2

--- src/contexts/RecipesProvider.jsx
+++ src/contexts/RecipesProvider.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useLocation } from 'react-router-dom';
