fa12a7625317cba8652967403febf552d0183814
Renan0211 <renan.antunes.rocha@gmail.com>
2021-07-06 17:57:37 -0300

    Implementa a logica dos botoes de filtro

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import DoneRecipe from '../components/DoneRecipe';
 import Header from '../components/Header';
 
 function DoneRecipes() {
   const doneRecipes = localStorage.getItem('doneRecipes');
-  const recipesToMap = doneRecipes === null ? [] : JSON.parse(doneRecipes);
-  console.log(recipesToMap);
-  // const arrTeste = [1, 2, 3, 4, 5];
+
+  const [filterBy, setfilterBy] = useState('nenhum');
+  const recipesToMap = (doneRecipes === null ? [] : JSON.parse(doneRecipes))
+    .filter((recipe) => {
+      if (filterBy === 'nenhum') return true;
+      return recipe.type === filterBy;
+    });
+  useEffect(() => {}, [filterBy]);
   return (
     <div>
       <Header title="Receitas Feitas" />
       <div>
-        <button type="button" data-testid="filter-by-all-btn">All</button>
-        <button type="button" data-testid="filter-by-food-btn">Food</button>
-        <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => setfilterBy('nenhum') }
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          onClick={ () => setfilterBy('comida') }
+        >
+          Food
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setfilterBy('bebida') }
+        >
+          Drinks
+        </button>
       </div>
       {recipesToMap.map((recipe, index) => (
         <DoneRecipe
