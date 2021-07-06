e11f2185d44877d22a10cddbe3db7d79ab50742c
Thiago Marchini <thiago@msn.com>
2021-07-06 15:33:23 -0300

    coreção de erros

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipesProvider from './context/RecipesProvider';
 import ComidasArea from './pages/ComidasArea';
-import Ingredients from './pages/Ingredientes';
+import Ingredients from './components/Ingredients';
 import Comidas from './pages/Comidas';
 import Bebidas from './pages/Bebidas';
 
@@ -19,7 +19,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/comidas" component={ Comidas } />
-        { /* <Route exact path="/bebidas" component={ Bebidas } /> */ }
+        <Route exact path="/bebidas" component={ Bebidas } />
         <Route exact path="/comidas/:id" component={ ReceitaDetalhes } />
         <Route exact path="/bebidas/:id" component={ ReceitaDetalhes } />
         {/* <Route path="/comidas/:id/in-progress" component={ ReceitaEmProgresso } />
@@ -27,24 +27,14 @@ function App() {
         { /* <Route path="/explorar" component={ Explorar } /> */ }
         <Route exact path="/explorar/comidas" component={ ExplorarComidas } />
         <Route exact path="/explorar/bebidas" component={ ExplorarBebidas } />
-        <Route exact path="/explorar/comidas/ingredientes" component={ ComidasIngredientes } />
-        <Route exact path="/explorar/bebidas/ingredientes" component={ BebidasIngredientes } />
+        <Route exact path="/explorar/comidas/ingredientes" component={ Ingredients } />
+        <Route exact path="/explorar/bebidas/ingredientes" component={ Ingredients } />
         <Route exact path="/explorar/comidas/area" component={ ComidasArea } />
         { /* <Route path="/perfil" component={Perfil} /> */ }
         { /* <Route path="/receitas-feitas" component={ReceitasFeitas} /> */ }
         { /* <Route path="/receitas-favoritas" component={ReceitasFavoritas} /> */ }
       </Switch>
     </RecipesProvider>
-    // <div className="meals">
-    //   <span className="logo">TRYBE</span>
-    //   <object
-    //     className="rocksGlass"
-    //     type="image/svg+xml"
-    //     data={ rockGlass }
-    //   >
-    //     Glass
-    //   </object>
-    // </div>
   );
 }
 
