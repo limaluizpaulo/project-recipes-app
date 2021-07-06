05b9c392ea2c16841533155a565908a3c8e6b367
Jonathan Souza <souzajonathan570@gmail.com>
2021-07-06 20:55:33 -0300

    implementado a validaçao do dictionaryApi.js

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 import { VscCircleOutline } from 'react-icons/vsc';
 import Table from 'react-bootstrap/Table';
-import dictionaryFood from '../help/dictionaryApi';
+import identification from '../helper/dictionaryApi';
 
 class Ingredients extends Component {
   checkIngredient() {
@@ -15,19 +15,21 @@ class Ingredients extends Component {
 
   render() {
     const { data, isStart } = this.props;
+    const dictionary = identification(data);
+    // console.log(dictionary);
     return (
       <section>
         <Table responsive="sm">
           <tbody>
             {
-              dictionaryFood.Ingredients.map((ingredient, index) => (
-                data[ingredient[0]] !== '' && (
+              dictionary.Ingredients.map((ingredient, index) => (
+                (data[ingredient[0]] !== null && data[ingredient[0]] !== '') ? (
                   <tr key={ index }>
                     <td>{isStart ? this.checkIngredient() : <VscCircleOutline />}</td>
                     <td>{data[ingredient[0]]}</td>
                     <td>{data[ingredient[1]]}</td>
                   </tr>
-                )
+                ) : null
               ))
             }
           </tbody>
