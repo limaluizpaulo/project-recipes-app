3140e62c568fa50d4c5b6553e52f5501001e1ac2
isTorres <ismaeltorres612@gmail.com>
2021-07-06 19:35:03 -0300

    add footer component

--- src/pages/Bebidas.js
+++ src/pages/Bebidas.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Bebidas() {
   return (
-    <Header />
+    <main>
+      <Header />
+      <Footer />
+    </main>
   );
 }
 
