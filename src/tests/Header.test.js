eb625910c87d9d3d364875a4020d32aa82e65b60
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-06 20:01:28 -0300

    finaliza testes pagina de Perfil

--- src/tests/Header.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import Header from '../components/Header';
-
-describe('Checks Header', () => {
-  it('Checks if the paga has two buttons', () => {
-    render(<Header />);
-  });
-});
---
+++ src/tests/Perfil.test.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import RecipesProvider from '../context/RecipesProvider';
+import renderWithRouter from '../renderWithRouter';
+import Profile from '../pages/Profile';
+
+describe('Checks Page Login', () => {
+  it('Cheks if the page has a profile button', () => {
+    renderWithRouter(
+      <RecipesProvider>
+        <Profile />
+      </RecipesProvider>,
+    );
+
+    const buttonProfile = screen.getByTestId('profile-top-btn');
+    expect(buttonProfile).toBeInTheDocument();
+  });
+
+  it('Checks if the page has a title', () => {
+    renderWithRouter(
+      <RecipesProvider>
+        <Profile />
+      </RecipesProvider>,
+    );
+
+    const titlePage = screen.getByTestId('page-title');
+    expect(titlePage.innerHTML).toBe('Perfil');
+  });
+
+  it('Checks if the page has two button about recipes ', () => {
+    renderWithRouter(
+      <RecipesProvider>
+        <Profile />
+      </RecipesProvider>,
+    );
+
+    const recipesMadeButton = screen.getByTestId('profile-done-btn');
+    expect(recipesMadeButton).toBeInTheDocument();
+
+    const favoriteRecipesButton = screen.getByTestId('profile-favorite-btn');
+    expect(favoriteRecipesButton).toBeInTheDocument();
+  });
+
+  it('Checks if the buttons redirect to respectively pages', () => {
+    const { history } = renderWithRouter(
+      <RecipesProvider>
+        <Profile />
+      </RecipesProvider>,
+    );
+
+    const recipesMadeButton = screen.getByTestId('profile-done-btn');
+    expect(recipesMadeButton).toBeInTheDocument();
+
+    userEvent.click(recipesMadeButton);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/receitas-feitas');
+
+    const favoriteRecipesButton = screen.getByTestId('profile-favorite-btn');
+    expect(favoriteRecipesButton).toBeInTheDocument();
+
+    userEvent.click(favoriteRecipesButton);
+
+    const { pathname: pathname2 } = history.location;
+    expect(pathname2).toBe('/receitas-favoritas');
+  });
+
+  it('Check if  the page has a Logout button and redirect to the Login page', () => {
+    const { history } = renderWithRouter(
+      <RecipesProvider>
+        <Profile />
+      </RecipesProvider>,
+    );
+
+    const logOutButton = screen.getByTestId('profile-logout-btn');
+    expect(logOutButton).toBeInTheDocument();
+
+    userEvent.click(logOutButton);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Check if the page has a footer', () => {
+    renderWithRouter(
+      <RecipesProvider>
+        <Profile />
+      </RecipesProvider>,
+    );
+
+    const footer = screen.getByTestId('footer');
+    expect(footer).toBeInTheDocument();
+  });
+});
