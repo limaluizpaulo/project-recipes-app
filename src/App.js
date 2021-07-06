7099722f9945878d921c9a55e8e7f72e6b0fcd9d
Thiago Marchini <thiago@msn.com>
2021-07-06 14:32:57 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import ReceitaDetalhes from './pages/ReceitaDetalhes';
 // import ReceitaEmProgresso from './pages/ReceitaEmProgresso';
 import Login from './pages/Login';
-import ExplorarReceitas from './pages/ExploreRecipes';
+// import ExplorarReceitas from './pages/ExploreRecipes';
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -19,19 +19,19 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/comidas" component={ Comidas } />
-        { /* <Route exact path="/bebidas" component={Bebidas} /> */ }
+        { /* <Route exact path="/bebidas" component={ Bebidas } /> */ }
         <Route exact path="/comidas/:id" component={ ReceitaDetalhes } />
         <Route exact path="/bebidas/:id" component={ ReceitaDetalhes } />
         {/* <Route path="/comidas/:id/in-progress" component={ ReceitaEmProgresso } />
         <Route path="/bebidas/:id/in-progress" component={ ReceitaEmProgresso } /> */}
-        { /* <Route path="/explorar" component={Explorar} /> */ }
-        <Route exact path="/explorar/comidas" component={ExplorarComidas} />
-        <Route exact path="/explorar/bebidas" component={ExplorarBebidas} />
+        { /* <Route path="/explorar" component={ Explorar } /> */ }
+        <Route exact path="/explorar/comidas" component={ ExplorarComidas } />
+        <Route exact path="/explorar/bebidas" component={ ExplorarBebidas } />
         <Route exact path="/explorar/comidas/ingredientes"
-        component={ComidasIngredientes} />
+        component={ ComidasIngredientes } />
         <Route exact path="/explorar/bebidas/ingredientes"
-        component={BebidasIngredientes} />
-        <Route exact path="/explorar/comidas/area" component={ComidasArea} />
+        component={ BebidasIngredientes } />
+        <Route exact path="/explorar/comidas/area" component={ ComidasArea } />
         { /* <Route path="/perfil" component={Perfil} /> */ }
         { /* <Route path="/receitas-feitas" component={ReceitasFeitas} /> */ }
         { /* <Route path="/receitas-favoritas" component={ReceitasFavoritas} /> */ }
