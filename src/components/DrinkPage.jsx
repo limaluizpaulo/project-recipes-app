6076d6c2dfe4a533a6b74014671c50214f330199
Mariana Savoldi <marianasavodi123@gmail.com>
2021-07-06 19:47:34 -0300

    Inicio dos reqs 15 a 18

--- src/components/DrinkPage.jsx
+++ src/components/DrinkPage.jsx
@@ -6,7 +6,8 @@ import SBElements from './SBElements';
 import ContextRecipes from '../context/contextRecipes';
 
 function DrinkPage({ history }) {
-  const { goSearch, setTitle } = useContext(ContextRecipes);
+  const { goSearch, setTitle, drinks } = useContext(ContextRecipes);
+  const maxLength = 11;
 
   useEffect(() => {
     setTitle('Bebidas');
@@ -15,7 +16,16 @@ function DrinkPage({ history }) {
   return (
     <div>
       <Header history={ history } />
-      { goSearch && <SBElements /> }
+      { goSearch && <SBElements history={ history } /> }
+      <section>
+        { drinks
+        // https://stackoverflow.com/questions/42374873/limit-items-in-a-map-loop/42374933
+          .map(({ strDrink, strDrinkThumb }, index) => index <= maxLength && (
+            <article key={ index }>
+              <img src={ strDrinkThumb } alt={ strDrink } width="150" />
+              <p>{ strDrink }</p>
+            </article>))}
+      </section>
       <Footer history={ history } />
     </div>
   );
