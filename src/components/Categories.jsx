cced908efadd985d91fce9fefdcba407aaa8a756
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-07-06 03:51:05 -0300

    tentando resolver bugs do lint?

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useState } from 'react';
 import Button from '../helpers/Button';
 import RecipesContext from '../contexts/RecipesContext';
