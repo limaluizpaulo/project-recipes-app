de6c55a0e0725d139827c697a4bdfde9e56e8321
CamilaCode7 <CamilaCode7>
2021-07-07 19:12:41 -0300

    page recipe favorite

--- src/pages/FavoritesRecipes.jsx
+++ src/pages/FavoritesRecipes.jsx
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderExplore from '../components/HeaderExplore';
+import FavoriteRecipeCard from '../components/FavoriteRecipeCard';
 
 function FavouritesRecipes() {
+  const recipesFromStorage = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+  const [favoriteRecipes, setFavoriteRecipes] = useState(recipesFromStorage);
+
+  useEffect(() => {
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
+  }, [favoriteRecipes]);
+
   return (
     <div>
       <HeaderExplore title="Receitas Favoritas" />
       <button
         type="button"
         data-testid="filter-by-all-btn"
-        // onClick={ }
+        onClick={ () => setFavoriteRecipes(recipesFromStorage) }
       >
         All
       </button>
       <button
         type="button"
         data-testid="filter-by-food-btn"
-        // onClick={ }
+        onClick={ () => setFavoriteRecipes(recipesFromStorage
+          .filter((recipe) => recipe.type === 'comida')) }
       >
         Food
       </button>
       <button
         type="button"
         data-testid="filter-by-drink-btn"
-        // onClick={ }
+        onClick={ () => setFavoriteRecipes(recipesFromStorage
+          .filter((recipe) => recipe.type === 'bebida')) }
       >
         Drink
       </button>
+      <div>
+        { favoriteRecipes.map((recipe, index) => (
+          <FavoriteRecipeCard
+            key={ recipe.id }
+            favoriteRecipes={ recipe }
+            index={ index }
+            setFavoriteRecipes={ setFavoriteRecipes }
+          />
+        )) }
+      </div>
     </div>
   );
 }
