9061d4f3572ccf100b949b3dda86c0d91da399a6
Daniel Alves <araujodaniel321@gmail.com>
2021-07-07 15:12:03 -0300

    pagina explorar feita (exercicios 67 ao 69)

--- src/pages/Explorer.jsx
+++ src/pages/Explorer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import DownMenu from '../components/DownMenu';
 
@@ -7,7 +8,24 @@ class Explorer extends Component {
     return (
       <div>
         <Header header="Explorar" explorer={ false } />
-        <h2>Explorer</h2>
+        {/* referencias usadas no codigo abaixo https://stackoverflow.com/questions/42463263/wrapping-a-react-router-link-in-an-html-button */}
+        <Link to="/explorar/comidas">
+          <button
+            type="button"
+            data-testid="explore-food"
+          >
+            Explorar Comidas
+          </button>
+        </Link>
+
+        <Link to="explorar/bebidas">
+          <button
+            type="button"
+            data-testid="explore-drinks"
+          >
+            Explorar Bebidas
+          </button>
+        </Link>
         <DownMenu />
       </div>
     );
