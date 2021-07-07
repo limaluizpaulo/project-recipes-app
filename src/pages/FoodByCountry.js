306f7ea03806b37e41eb42de1178497dd22335d3
Renan0211 <renan.antunes.rocha@gmail.com>
2021-07-07 18:53:47 -0300

    Implementa a logica de filtrar as refeiçoes por area

--- src/pages/FoodByCountry.js
+++ src/pages/FoodByCountry.js
@@ -5,20 +5,31 @@ import { Card } from 'react-bootstrap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { Context } from '../context/ContextForm';
-import { requestMeal } from '../services/api';
+import { requestAreas, requestMeal, requestMealByAreas } from '../services/api';
 
 function FoodByCountry() {
   const { setFirstMeals, firstMeals, area, setArea } = useContext(Context);
+  // const [filterBy, setFilterBy] = useState('All');
   const numOfMeals = 12;
 
   useEffect(() => {
     const fetchMeals = async () => {
       const meals = await requestMeal();
+      const areas = await requestAreas();
       setFirstMeals(meals.slice(0, numOfMeals));
-      setArea(meals);
+      setArea(areas);
     };
     fetchMeals();
   }, [setFirstMeals, setArea]);
+
+  async function handleClick({ target }) {
+    const { value } = target;
+    console.log('teste');
+    // setFilterBy(value);
+    const mealsOfTheArea = await requestMealByAreas(value);
+    setFirstMeals(mealsOfTheArea);
+    console.log(mealsOfTheArea);
+  }
 
   return (
     <div>
@@ -27,6 +38,7 @@ function FoodByCountry() {
         <select
           className="selectByCountry"
           data-testid="explore-by-area-dropdown"
+          onChange={ handleClick }
         >
           <option>All</option>
           {area.map((item, index) => (
@@ -41,31 +53,34 @@ function FoodByCountry() {
         </select>
       </div>
       <div className="card-container">
-        {firstMeals.map((meal, index) => (
-          <Link
-            to={ `/comidas/${meal.idMeal}` }
-            key={ meal.strMeal }
-          >
-            <Card
-              data-testid={ `${index}-recipe-card` }
-              className="card"
+        {firstMeals.filter((meal, index) => index < numOfMeals).map((meal, index) => {
+          console.log(meal);
+          return (
+            <Link
+              to={ `/comidas/${meal.idMeal}` }
+              key={ meal.strMeal }
             >
-              <Card.Img
-                data-testid={ `${index}-card-img` }
-                src={ meal.strMealThumb }
-                alt={ meal.strMeal }
-              />
-              <Card.Body>
-                <Card.Title
-                  className="cardTitle"
-                  data-testid={ `${index}-card-name` }
-                >
-                  {meal.strMeal}
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </Link>
-        ))}
+              <Card
+                data-testid={ `${index}-recipe-card` }
+                className="card"
+              >
+                <Card.Img
+                  data-testid={ `${index}-card-img` }
+                  src={ meal.strMealThumb }
+                  alt={ meal.strMeal }
+                />
+                <Card.Body>
+                  <Card.Title
+                    className="cardTitle"
+                    data-testid={ `${index}-card-name` }
+                  >
+                    {meal.strMeal}
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Link>
+          );
+        })}
       </div>
       <Footer />
     </div>
