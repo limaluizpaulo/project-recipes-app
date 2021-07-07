e24d7e971fe843463bb552e6706bf96ce2558068
riquee <strikedv@hotmail.com>
2021-07-07 14:00:24 -0300

    telas explorar drinks e mals criados

--- src/pages/SearchDrinks.js
+++ src/pages/SearchDrinks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -7,8 +8,12 @@ const SearchDrinks = () => {
   return (
     <div>
       <Header title="Explorar Bebidas" />
-      <button data-testid="explore-by-ingredient" type="button">Por Ingredientes</button>
-      <button data-testid="explore-surprise" type="button">Me Surpreenda!</button>
+      <Link data-testid="explore-by-ingredient" to="/explorar/bebidas/ingredientes">
+        Por Ingredientes
+      </Link>
+      <Link data-testid="explore-surprise" to="/bebidas/178319">
+        Me Surpreenda!
+      </Link>
       <Footer />
     </div>
   );
--- src/pages/SearchFoods.js
+++ src/pages/SearchFoods.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -7,9 +8,15 @@ const SearchFoods = () => {
   return (
     <div>
       <Header title="Explorar Comidas" />
-      <button data-testid="explore-by-ingredient" type="button">Por Ingredientes</button>
-      <button data-testid="explore-by-area" type="button">Por Local de Origem</button>
-      <button data-testid="explore-surprise" type="button">Me Surpreenda!</button>
+      <Link data-testid="explore-by-ingredient" to="/explorar/comidas/ingredientes">
+        Por Ingredientes
+      </Link>
+      <Link data-testid="explore-by-area" to="/explorar/comidas/area">
+        Por Local de Origem
+      </Link>
+      <Link data-testid="explore-surprise" to="/comidas/52771">
+        Me Surpreenda!
+      </Link>
       <Footer />
     </div>
   );
