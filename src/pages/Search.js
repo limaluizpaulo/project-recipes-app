b4541648c95490c1263a7983a0d6516c38cd7e49
riquee <strikedv@hotmail.com>
2021-07-07 13:42:31 -0300

    tela explorar criada

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -7,7 +8,13 @@ const Search = () => {
   return (
     <div>
       <Header title="Explorar" />
-      <h1>Search</h1>
+      <Link data-testid="explore-food" to="/explorar/comidas">
+        Explorar Comidas
+      </Link>
+
+      <Link data-testid="explore-drinks" to="/explorar/bebidas">
+        Explorar Bebidas
+      </Link>
       <Footer />
     </div>
   );
