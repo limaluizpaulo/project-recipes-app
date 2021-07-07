34341ebfc69680b8757457595d872a46cb35df50
Alan Tanaka <alanctnk@gmail.com>
2021-07-07 20:39:45 -0300

    upando

--- src/redux/reducers/foods.js
+++ src/redux/reducers/foods.js
@@ -3,7 +3,7 @@ import { FOODS, FOOD_CATEGORIES } from '../actions/index';
 const initialState = {
   list: [],
   goToFoodsPage: false,
-  // foodNotFound: false,
+
 };
 
 export default (state = initialState, { type, payload }) => {
@@ -13,14 +13,13 @@ export default (state = initialState, { type, payload }) => {
       ...state,
       list: payload,
       goToFoodsPage: payload && payload.length === 1,
-      // foodNotFound: !payload,
+
     };
 
   case FOOD_CATEGORIES:
     return {
       ...state,
       list: payload,
-      // foodNotFound: false,
     };
 
   default:
