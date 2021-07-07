9c68b03564246992b1dde96d30b808528a07deaa
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-07-07 16:01:48 -0300

    adicionando a função que inicializa o localStorage da aplicação

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,3 +1,19 @@
+export function initializeLocalStorage(email) {
+  const initialData = [
+    { key: 'mealsToken', keyValue: 1 },
+    { key: 'cocktailsToken', keyValue: 1 },
+    { key: 'user', keyValue: { email } },
+    { key: 'doneRecipes', keyValue: [] },
+    { key: 'favoriteRecipes', keyValue: [] },
+    { key: 'inProgressRecipes', keyValue: { cocktails: {}, meals: {} } },
+  ];
+
+  initialData.forEach(({ key, keyValue }) => {
+    const stringifyValue = JSON.stringify(keyValue);
+    localStorage.setItem(key, stringifyValue);
+  });
+}
+
 // Setar item no LocalStorage
 export function setItemLocalStorage(key, value) {
   let result = value;
