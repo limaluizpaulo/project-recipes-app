c383c1586397a62b70c5ccf716daada94cf62ee2
João Victor Pistorio <joao.pistorio@gmail.com>
2021-07-07 15:11:45 -0300

    Requisito 67 e 68

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -9,6 +9,21 @@ class Explore extends Component {
         <section>
           <Header title="Explorar" searchIcon />
         </section>
+
+        <section>
+          <button
+            type="button"
+            data-testid="explore-food"
+          >
+            Explorar Comidas
+          </button>
+          <button
+            type="button"
+            data-testid="explore-drinks"
+          >
+            Explorar Bebidas
+          </button>
+        </section>
         <Footer />
       </>
     );
