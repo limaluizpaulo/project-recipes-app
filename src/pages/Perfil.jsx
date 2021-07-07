4f29537432c600f0502391ca6dfc3305b634ee67
aythan azevedo <aythan16@gmail.com>
2021-07-07 19:39:51 -0300

    falta o 87

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Perfil() {
   Perfil.displayName = 'Perfil';
+  const [userEmail, setState] = useState();
+  const [redirect, setRedirect] = useState(false);
+
+  const handleClick = () => {
+    localStorage.setItem('mealsToken', JSON.stringify(null));
+    localStorage.setItem('cocktailsToken', JSON.stringify(null));
+    localStorage.setItem('user', JSON.stringify(null));
+    // localStorage.setItem('doneRecipes', JSON.stringify(null));
+    // localStorage.setItem('favoriteRecipes', JSON.stringify(null));
+    // localStorage.setItem('inProgressRecipes', JSON.stringify(null));
+    setRedirect(true);
+    return redirect;
+  };
+
+  useEffect(() => {
+    const getEmail = () => {
+      const email = JSON.parse(localStorage.getItem('user'));
+      setState(email.email);
+    };
+    getEmail();
+  },
+
+  []);
+
   return (
 
     <div>
       <Header title={ Perfil.displayName } />
-      <Link to="/receitas-favoritas" className="btn">receitas favoritas</Link>
+      <p data-testid="profile-email">{ userEmail }</p>
+      <Link
+        to="/receitas-favoritas"
+        data-testid="profile-favorite-btn"
+      >
+        Receitas Favoritas
+      </Link>
+      <br />
+      <Link
+        to="/receitas-feitas"
+        data-testid="profile-done-btn"
+      >
+        Receitas Feitas
+      </Link>
+      <br />
+      <Link
+        to="/"
+        data-testid="profile-logout-btn"
+        onClick={ handleClick }
+      >
+        Sair
+      </Link>
       <Footer />
     </div>
   );
 }
-
-// <Link to="/receitas-favoritas" className="btn">receitas favoritas</Link>
-// <Redirect to="/receitas-feitas" />
-// <Redirect to="/receitas-favoritas" />
 
 export default Perfil;
