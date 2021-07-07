96cb86ba032901af6a5173ecd0a7fa8cd5db759e
Aladino Borges <eualadino@gmail.com>
2021-07-07 19:54:55 -0300

    Lógica requisito 35: 85%.

--- src/pages/FoodAndDrinkById.js
+++ src/pages/FoodAndDrinkById.js
@@ -32,14 +32,11 @@ export default function FoodAndDrinkById() {
       const recResolved = await fetchAPI(URL_RECOMENDATION);
 
       setSingleContent(resolved[firstKey] || []);
-
       setRecomentation(recResolved[recFirstKey].filter((_e, index) => index < SIX));
 
       const list = Object.entries(resolved[firstKey][0]).filter((el) => (
         (el[0].includes('Ingredient')
         || el[0].includes('Measure')) && el[1]) && el[1] !== ' ');
-
-      console.log(list);
 
       setIngridientsList(list);
     }
@@ -63,33 +60,64 @@ export default function FoodAndDrinkById() {
             src={ singleContent[0][imgSrc] }
             alt={ singleContent[0][title] }
           />
-          <h3 data-testid="recipe-title">{singleContent[0][title]}</h3>
-          <Button><img data-testid="share-btn" src={ shareIcon } alt="" /></Button>
-          <Button onClick={ handleFavorite }>
-            <img data-testid="favorite-btn" src={ blackHeartIcon } alt="" />
-          </Button>
-          <p data-testid="recipe-category">{singleContent[0].strCategory}</p>
-          {ingredientsList.map((el, i) => (
-            <p
-              key={ i }
-              data-testid={ `${i}-ingredient-name-and-measure` }
-            >
-              {el[1]}
+          <div className="recipe-heading-container">
+            <div className="info-heading">
+              <h3 data-testid="recipe-title">{singleContent[0][title]}</h3>
+              <p data-testid="recipe-category">{singleContent[0].strCategory}</p>
+            </div>
+            <Button>
+              <img data-testid="share-btn" src={ shareIcon } alt="" />
+            </Button>
+            <Button onClick={ handleFavorite }>
+              <img data-testid="favorite-btn" src={ blackHeartIcon } alt="" />
+            </Button>
+          </div>
+          <div className="ingredients-container">
+            <h4>
+              Ingredients
+            </h4>
+            {ingredientsList.map((el, i, arr) => (
+              <div key={ i }>
+                <p
+                  data-testid={ `${i}-ingredient-name-and-measure` }
+                >
+                  <span>
+                    {el[0].includes('Ingredient') && el[1]}
+                  </span>
+                  {arr.filter((elt) => elt[0] === (`strMeasure${i + 1}`))
+                    .map((result) => (
+                      <span
+                        data-testid={ `${i}-ingredient-name-and-measure` }
+                        key={ result }
+                      >
+                        { ` - ${result[1]}` }
+                      </span>
+                    ))}
+                </p>
+              </div>
+            ))}
+          </div>
+
+          <div className="instructions-video-container">
+            <p data-testid="instructions">
+              {singleContent[0].strInstructions}
             </p>
-          ))}
-          <p data-testid="instructions">{singleContent[0].strInstructions}</p>
-          <p data-testid="video">{singleContent[0].strYoutube}</p>
+            { path.includes('/comidas') && (
+              <p data-testid="video">{singleContent[0].strYoutube}</p>
+            ) }
+          </div>
         </>
       )}
-
-      { recomendations.map((item, i) => (
-        <Card
-          key={ i }
-          mealOrDrink={ item }
-          index={ i }
-          testId="recomendation"
-        />
-      ))}
+      <div className="recomendations-container">
+        { recomendations.map((item, i) => (
+          <Card
+            key={ i }
+            mealOrDrink={ item }
+            index={ i }
+            testId="recomendation"
+          />
+        ))}
+      </div>
       <Button data-testid="start-recipe-btn">Iniciar Receita</Button>
     </>
   );
