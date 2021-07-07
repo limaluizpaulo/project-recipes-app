c39dc7da3e2d65117f98e6faf2ce339946fd6b83
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-07-07 17:20:41 -0300

    teste ExploreMeals

--- src/test/renderWithRouter.js
+++ src/test/renderWithRouter.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
+import MealsContextProvider from '../context/mealsContext';
+import DrinksContextProvider from '../context/drinksContext';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
-    ...render(<Router history={ history }>{component}</Router>), history,
+    ...render(
+      <Router history={ history }>
+        <MealsContextProvider>
+          <DrinksContextProvider>
+            {component}
+          </DrinksContextProvider>
+        </MealsContextProvider>
+      </Router>,
+    ),
+    history,
   });
 };
 
