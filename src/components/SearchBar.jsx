639653a17c475e0c0debab2b7d90085f8542e0c9
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-07 14:42:18 -0300

    corrige proptypes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -72,7 +72,7 @@ function SearchBar() {
   return (
     <form onSubmit={ handleSubmit }>
       <div>
-        <div controlId="formBasicEmail">
+        <div>
           <input
             value={ textFilter }
             data-testid="search-input"
