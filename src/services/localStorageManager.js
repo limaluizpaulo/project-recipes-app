04d2d2685dbb03f658477161e7762d52ee77cf4e
Thiago Leite <leite.tjs@gmail.com>
2021-07-07 14:40:51 -0300

    lint fixed localstorage

--- src/services/localStorageManager.js
+++ src/services/localStorageManager.js
@@ -74,6 +74,5 @@ export const checkDoneRecipes = (id) => {
 
 export const checkProgress = (id, type) => {
   const data = JSON.parse(localStorage.getItem('inProgressRecipes')) || [];
-  return [];
-  // return Object.keys(data[type]).some((recipeInProgress) => recipeInProgress === id);
+  return Object.keys(data[type]).some((recipeInProgress) => recipeInProgress === id);
 };
