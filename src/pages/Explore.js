e2a3865beef7cdb07aaa3acf1e03a87cf7bdb159
João Victor Pistorio <joao.pistorio@gmail.com>
2021-07-07 15:19:52 -0300

    Requisito 69

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -11,18 +12,23 @@ class Explore extends Component {
         </section>
 
         <section>
-          <button
-            type="button"
-            data-testid="explore-food"
-          >
-            Explorar Comidas
-          </button>
-          <button
-            type="button"
-            data-testid="explore-drinks"
-          >
-            Explorar Bebidas
-          </button>
+          <Link to="/explorar/comidas">
+            <button
+              type="button"
+              data-testid="explore-food"
+            >
+              Explorar Comidas
+            </button>
+          </Link>
+
+          <Link to="/explorar/bebidas">
+            <button
+              type="button"
+              data-testid="explore-drinks"
+            >
+              Explorar Bebidas
+            </button>
+          </Link>
         </section>
         <Footer />
       </>
