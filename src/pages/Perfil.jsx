6c2fe3a970573d80327c61760836b72624fadab0
aythan azevedo <aythan16@gmail.com>
2021-07-07 15:17:51 -0300

    botao link

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Perfil() {
   Perfil.displayName = 'Perfil';
   return (
+
     <div>
       <Header title={ Perfil.displayName } />
+      <Link to="/receitas-favoritas" className="btn">receitas favoritas</Link>
       <Footer />
     </div>
   );
 }
+
+// <Link to="/receitas-favoritas" className="btn">receitas favoritas</Link>
+// <Redirect to="/receitas-feitas" />
+// <Redirect to="/receitas-favoritas" />
 
 export default Perfil;
