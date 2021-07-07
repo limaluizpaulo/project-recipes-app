27914d31675ac2f5f95dac1f22279e6dbdb66bdd
Jorge Alves <alvesdjorge@gmail.com>
2021-07-07 14:18:58 -0300

    atualizando

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import RecipesContext from '../context/RecipesContext';
+import Header from '../components/Header/Header';
+import FoodCard from '../components/FoodCard';
 
 function Recipes() {
+  const { recipes } = useContext(RecipesContext);
+  const cardMaximun = 12;
   return (
     <div>
-      .
+      <Header title="Comidas" search="" />
+      {
+        recipes
+          && RecipesContext.map((recipe, i) => (
+            i < cardMaximun
+             && <FoodCard key={ i } order={ i } recipes={ recipe } />
+          ))
+      }
     </div>
   );
 }
