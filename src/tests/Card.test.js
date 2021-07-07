a9e4361b968d5264c262c6bcab8606211b2e077f
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-07-07 17:40:49 -0300

    altera estilizacao para se tornar totalmente responsiva

--- src/tests/Card.test.js
+++ src/tests/Card.test.js
@@ -1,5 +1,4 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import React from 'react';
 import Card from '../components/Card';
 import renderWithRouter from '../renderWithRouter';
