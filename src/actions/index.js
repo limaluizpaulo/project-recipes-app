521490d02f96a001ba1bb5e0373d490054ea9b08
Leandro Reis <lleandrofr@gmail.com>
2021-07-08 18:19:21 -0300

    save filtered recipes on global state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
-export default finishedRecipe = (value) => ({ type: 'FINISHED', value });
+export const UPDATE_RECIPES = 'UPDATE_RECIPES';
+export const RENDER_FILTERED = 'RENDER_FILTERED';
+
+export const finishedRecipe = (value) => ({ type: 'FINISHED', value });
+
+export const updateRecipes = (payload) => ({
+  type: UPDATE_RECIPES,
+  payload,
+});
+
+export const renderFiltered = (payload) => ({
+  type: RENDER_FILTERED,
+  payload,
+});
