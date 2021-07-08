1640a6c912b90dbc067ff16e088a9acba9e13bc3
Mariana Mohr <marianamohr@gmail.com>
2021-07-08 19:11:00 -0300

    finalizado menos 39 e 40

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-// import Embed from 'react-embed';
 import '../Style/RecipeDetails.css';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
+import Ingredients from '../components/Ingredients';
+
+const copy = require('clipboard-copy');
 
 class RecipeDetails extends Component {
   constructor(props) {
@@ -16,20 +18,29 @@ class RecipeDetails extends Component {
       redirect: false,
       favoriteRecipes: [],
       heart: whiteHeartIcon,
+      doneRecipes: [],
+      testRecipeDone: true,
+      linkCopiado: false,
     };
     this.getRecipe = this.getRecipe.bind(this);
     this.getDrinks = this.getDrinks.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.getLocalStoreFavorites = this.getLocalStoreFavorites.bind(this);
+    this.getLocalStoreDone = this.getLocalStoreDone.bind(this);
     this.handleClickFavorite = this.handleClickFavorite.bind(this);
+    this.setIntialHeart = this.setIntialHeart.bind(this);
   }
 
   componentDidMount() {
     this.getRecipe();
     this.getDrinks();
     const favorite = localStorage.getItem('favoriteRecipes');
+    const done = localStorage.getItem('doneRecipes');
     if (favorite) {
-      this.getLocalStoreFavorites(JSON.parse(favorite).newFavorite);
+      this.getLocalStoreFavorites(JSON.parse(favorite));
+    }
+    if (done) {
+      this.getLocalStoreDone(JSON.parse(done));
     }
   }
 
@@ -62,7 +73,7 @@ class RecipeDetails extends Component {
           newFavorite.push(modelo);
           this.setState({ favoriteRecipes: newFavorite },
             () => {
-              localStorage.setItem('favoriteRecipes', JSON.stringify({ newFavorite }));
+              localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorite));
             });
         });
     } else {
@@ -71,7 +82,7 @@ class RecipeDetails extends Component {
           const teste = favoriteRecipes.filter((model) => model.id !== recipe.idMeal);
           this.setState({ favoriteRecipes: teste },
             () => {
-              localStorage.setItem('favoriteRecipes', JSON.stringify({ teste }));
+              localStorage.setItem('favoriteRecipes', JSON.stringify(teste));
             });
         });
     }
@@ -81,7 +92,9 @@ class RecipeDetails extends Component {
     const { match: { params: { id } } } = this.props;
     const result = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
     const { meals } = await result.json();
-    this.setState({ recipe: meals[0] });
+    this.setState({ recipe: meals[0] }, () => {
+      this.setIntialHeart();
+    });
   }
 
   async getDrinks() {
@@ -96,8 +109,33 @@ class RecipeDetails extends Component {
     this.setState({ favoriteRecipes: favorite });
   }
 
+  getLocalStoreDone(done) {
+    this.setState({ doneRecipes: done });
+  }
+
+  setIntialHeart() {
+    const { recipe, favoriteRecipes, doneRecipes } = this.state;
+    const testHeart = favoriteRecipes.some((fav) => fav.id === recipe.idMeal);
+    if (testHeart) {
+      this.setState({ heart: blackHeartIcon });
+    } else {
+      this.setState({ heart: whiteHeartIcon });
+    }
+    console.log(recipe);
+    console.log(doneRecipes);
+    const testDone = doneRecipes.some((fav) => fav.id === recipe.idMeal);
+    console.log(testDone);
+    if (testDone) {
+      console.log('true');
+      this.setState({ testRecipeDone: true });
+    } else {
+      console.log('false');
+      this.setState({ testRecipeDone: false });
+    }
+  }
+
   render() {
-    const { recipe, drinks, redirect, favoriteRecipes, heart } = this.state;
+    const { recipe, drinks, redirect, heart, testRecipeDone, linkCopiado } = this.state;
     const { match: { params: { id } } } = this.props;
     if (redirect) {
       return <Redirect to={ `/comidas/${id}/in-progress` } />;
@@ -105,30 +143,7 @@ class RecipeDetails extends Component {
     if (recipe.length === 0) {
       return <h2>Loading...</h2>;
     }
-    console.log(favoriteRecipes);
-    console.log(recipe);
-    const ingredientsKeys = Object.entries(recipe);
-    const ingredients = [];
-    const measures = [];
 
-    ingredientsKeys.forEach((key) => {
-      if (key[0].includes('strIngredient') && key[1] !== null && key[1] !== '') {
-        ingredients.push(key[1]);
-      }
-    });
-    ingredientsKeys.forEach((key) => {
-      if (key[0].match('strMeasure') && key[1] !== null && key[1] !== '') {
-        measures.push(key[1]);
-      }
-    });
-    const finalList = [];
-    for (let i = 0; i <= (ingredients.length - 1); i += 1) {
-      if (measures[i] === undefined) {
-        finalList.push(ingredients[i]);
-      } else {
-        finalList.push(`${measures[i]} - ${ingredients[i]}`);
-      }
-    }
     const {
       strMealThumb,
       idMeal,
@@ -137,7 +152,7 @@ class RecipeDetails extends Component {
       strYoutube,
       strInstructions } = recipe;
     return (
-      <div>
+      <div className="page-details">
         <img
           className="recipeImg"
           src={ strMealThumb }
@@ -145,29 +160,31 @@ class RecipeDetails extends Component {
           data-testid="recipe-photo"
         />
         <h2 data-testid="recipe-title">{strMeal}</h2>
-        <button type="button" data-testid="share-btn">
+        <button
+          type="button"
+          data-testid="share-btn"
+          onClick={ () => {
+            const { location: { pathname } } = this.props;
+            copy(`http://localhost:3000${pathname}`);
+            this.setState({ linkCopiado: true });
+          } }
+        >
+          { linkCopiado && <span>Link copiado!</span>}
           <img className="icons" src={ shareIcon } alt="shareButton" />
         </button>
         <button
           type="button"
-          data-testid="favorite-btn"
           onClick={ this.handleClickFavorite }
         >
-          <img className="icons" src={ heart } alt="whiteHeartIcon" />
+          <img
+            className="icons"
+            src={ heart }
+            alt="whiteHeartIcon"
+            data-testid="favorite-btn"
+          />
         </button>
         <h3 data-testid="recipe-category">{strCategory}</h3>
-        <div>
-          <h2>Ingredients</h2>
-          <ul>
-            {finalList.map((ingredient, index) => (
-              <li
-                data-testid={ `${index}-ingredient-name-and-measure` }
-                key={ index }
-              >
-                {ingredient}
-              </li>))}
-          </ul>
-        </div>
+        <Ingredients recipe={ recipe } />
         <h3>Instructions</h3>
         <p data-testid="instructions">
           {strInstructions}
@@ -202,6 +219,7 @@ class RecipeDetails extends Component {
           onClick={ this.handleClick }
           type="button"
           data-testid="start-recipe-btn"
+          disabled={ testRecipeDone }
         >
           iniciar receita
         </button>
@@ -212,6 +230,7 @@ class RecipeDetails extends Component {
 
 RecipeDetails.propTypes = {
   match: PropTypes.shape().isRequired,
-
+  location: PropTypes.shape().isRequired,
 };
+
 export default RecipeDetails;
