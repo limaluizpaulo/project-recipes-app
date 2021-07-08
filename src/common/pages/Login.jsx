655ffaef4653d41adf37a0f4e89505fe3fce5a06
Adão Benites <adaobjr@gmail.com>
2021-07-08 09:29:28 -0400

    style: :construction: added: styles | starting adjust in display on Login Page

--- src/common/pages/Login.jsx
+++ src/common/pages/Login.jsx
@@ -1,9 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { BiLockAlt, BiUser } from 'react-icons/bi';
+import { FaFacebookF, FaTwitter, FaGoogle } from 'react-icons/fa';
+import imgLogin from '../../images/img-login.svg';
 import store, { addLogin } from '../../context/store';
 import { setStorage } from '../../functions';
 
 export default function Login() {
+  const [signUp, setSignUp] = useState(false);
   const [disabledBtn, setDisabledBtn] = useState(true);
   const { user, setUser } = useContext(store);
   const { email, password } = user;
@@ -34,30 +38,119 @@ export default function Login() {
 
   // ------------------------------------------------------------------------------------------
   return (
-    <div>
-      <input
-        name="email"
-        placeholder="Insira seu e-mail"
-        data-testid="email-input"
-        onChange={ (e) => setUser(addLogin(e)) }
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Insira sua senha de 7 dígitos"
-        data-testid="password-input"
-        onChange={ (e) => setUser(addLogin(e)) }
-      />
-      <Link to="/comidas">
-        <button
-          type="button"
-          data-testid="login-submit-btn"
-          disabled={ disabledBtn }
-          onClick={ handleClick }
-        >
-          Entrar
-        </button>
-      </Link>
+    <div className="login">
+      {/* <div className="loginContent"> */}
+      <div className="loginImg">
+        <img src={ imgLogin } alt="img-login" />
+      </div>
+
+      <div className="loginForms">
+        <form className={ (signUp) ? 'loginRegister none' : 'loginRegister' }>
+          <h1 className="loginTitle">Sign In</h1>
+
+          <div className="loginBox">
+            <BiUser className="loginIcon" />
+            <input
+              name="email"
+              placeholder="Insira seu e-mail"
+              data-testid="email-input"
+              className="loginInput"
+              onChange={ (e) => setUser(addLogin(e)) }
+            />
+          </div>
+
+          <div className="loginBox">
+            <BiLockAlt className="loginIcon" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Insira sua senha de 7 dígitos"
+              data-testid="password-input"
+              className="loginInput"
+              onChange={ (e) => setUser(addLogin(e)) }
+            />
+          </div>
+
+          <a href className="loginForgot">Esqueceu sua senha?</a>
+
+          <Link to="/comidas">
+            <button
+              type="button"
+              data-testid="login-submit-btn"
+              className="loginButton"
+              disabled={ disabledBtn }
+              onClick={ handleClick }
+            >
+              Entrar
+            </button>
+          </Link>
+
+          <div>
+            <span className="loginAccount">Não tem uma conta ?</span>
+            <button
+              type="button"
+              onClick={ () => setSignUp(!signUp) }
+              className="loginSignin"
+            >
+              Registre-se
+            </button>
+          </div>
+        </form>
+
+        <form className={ (signUp) ? 'loginCreate' : 'loginCreate none' }>
+          <h1 className="loginTitle">Crie sua conta</h1>
+
+          <div className="loginBox">
+            <BiUser className="loginIcon" />
+            <input type="text" placeholder="Username" className="loginInput" />
+          </div>
+
+          <div className="loginBox">
+            <BiLockAlt className="loginIcon" />
+            <input
+              name="email"
+              placeholder="Insira um e-mail válido"
+              className="loginInput"
+            />
+          </div>
+
+          <div className="loginBox">
+            <BiLockAlt className="loginIcon" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Insira uma senha de 7 dígitos"
+              className="loginInput"
+            />
+          </div>
+
+          <button
+            type="button"
+            className="loginButton"
+            onClick={ () => setSignUp(!signUp) }
+          >
+            Criar
+          </button>
+
+          <div>
+            <span className="loginAccount">Já tem uma conta ?</span>
+            <button
+              type="button"
+              onClick={ () => setSignUp(!signUp) }
+              className="loginSignup"
+            >
+              Acessar conta
+            </button>
+          </div>
+
+          <div className="loginSocial">
+            <FaFacebookF className="loginSocial-icon" />
+            <FaTwitter className="loginSocial-icon" />
+            <FaGoogle className="loginSocial-icon" />
+          </div>
+        </form>
+      </div>
+      {/* </div> */}
     </div>
   );
 }
