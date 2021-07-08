116ca4b6134988adc075a8a2e84d06fda0cfea32
elano3004@gmail.com <elano3004@gmail.com>
2021-07-08 16:48:34 -0300

    lint

--- src/pages/Progresso.jsx
+++ src/pages/Progresso.jsx
@@ -1,114 +1,114 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchDrinkDetails, fetchFoodDetails, startRecipe } from '../action';
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+// import { fetchDrinkDetails, fetchFoodDetails, startRecipe } from '../action';
 
-import Ingredients from '../components/Ingredients';
-import '../css/Details.css';
-import shareIcon from '../images/shareIcon.svg';
-import whiteHeartIcon from '../images/whiteHeartIcon.svg';
-import blackHeartIcon from '../images/blackHeartIcon.svg';
-import Instructions from '../components/Instructions';
-import DetailsHeader from '../components/DetailsHeader';
+// import Ingredients from '../components/Ingredients';
+// import '../css/Details.css';
+// import shareIcon from '../images/shareIcon.svg';
+// import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+// import blackHeartIcon from '../images/blackHeartIcon.svg';
+// import Instructions from '../components/Instructions';
+// import DetailsHeader from '../components/DetailsHeader';
 
-class Progresso extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      favIcon: false,
-      favIconColor: whiteHeartIcon,
-    };
-    this.handleFavClick = this.handleFavClick.bind(this);
-  }
+// class Progresso extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       favIcon: false,
+//       favIconColor: whiteHeartIcon,
+//     };
+//     this.handleFavClick = this.handleFavClick.bind(this);
+//   }
 
-  componentDidMount() {
-    const { match: { params: { page, id } }, foodDetails, drinksDetails } = this.props;
-    if (page === 'comidas') {
-      return foodDetails(id);
-    }
-    return drinksDetails(id);
-  }
+//   componentDidMount() {
+//     const { match: { params: { page, id } }, foodDetails, drinksDetails } = this.props;
+//     if (page === 'comidas') {
+//       return foodDetails(id);
+//     }
+//     return drinksDetails(id);
+//   }
 
-  handleFavClick() {
-    const { favIcon } = this.state;
-    if (!favIcon) {
-      this.setState({
-        favIconColor: blackHeartIcon,
-        favIcon: true,
-      });
-    }
-    if (favIcon) {
-      this.setState({
-        favIconColor: whiteHeartIcon,
-        favIcon: false,
-      });
-    }
-  }
+//   handleFavClick() {
+//     const { favIcon } = this.state;
+//     if (!favIcon) {
+//       this.setState({
+//         favIconColor: blackHeartIcon,
+//         favIcon: true,
+//       });
+//     }
+//     if (favIcon) {
+//       this.setState({
+//         favIconColor: whiteHeartIcon,
+//         favIcon: false,
+//       });
+//     }
+//   }
 
-  render() {
-    const { isStart, details } = this.props;
-    const { favIconColor } = this.state;
-    return (
-      <section>
-        <DetailsHeader data={ details } />
-        <button
-          className="details-btn-share"
-          type="button"
-          data-testid="share-btn"
-        >
-          <img src={ shareIcon } alt={ shareIcon } />
-        </button>
-        <button
-          className="details-btn-favorite"
-          type="button"
-          data-testid="favorite-btn"
-          onClick={ this.handleFavClick }
-        >
-          <img src={ favIconColor } alt={ favIconColor } />
-        </button>
-        <section className="details-content">
-          <section>
-            <h3>Ingredients</h3>
-            <span className="details-ingredients">
-              <Ingredients data={ details } data-testid="ingredient-step" />
-            </span>
-          </section>
-          <section data-testid="instructions">
-            <h3>Instructions</h3>
-            <span className="details-intructions-text">
-              <Instructions data={ details } />
-            </span>
-          </section>
-          <button
-            className="details-btn-startRecipe"
-            type="button"
-            data-testid="finish-recipe-btn"
-            onClick={ () => isStart() }
-          >
-            Finalizar Receita
-          </button>
-        </section>
-      </section>
-    );
-  }
-}
+//   render() {
+//     const { isStart, details } = this.props;
+//     const { favIconColor } = this.state;
+//     return (
+//       <section>
+//         <DetailsHeader data={ details } />
+//         <button
+//           className="details-btn-share"
+//           type="button"
+//           data-testid="share-btn"
+//         >
+//           <img src={ shareIcon } alt={ shareIcon } />
+//         </button>
+//         <button
+//           className="details-btn-favorite"
+//           type="button"
+//           data-testid="favorite-btn"
+//           onClick={ this.handleFavClick }
+//         >
+//           <img src={ favIconColor } alt={ favIconColor } />
+//         </button>
+//         <section className="details-content">
+//           <section>
+//             <h3>Ingredients</h3>
+//             <span className="details-ingredients">
+//               <Ingredients data={ details } data-testid="ingredient-step" />
+//             </span>
+//           </section>
+//           <section data-testid="instructions">
+//             <h3>Instructions</h3>
+//             <span className="details-intructions-text">
+//               <Instructions data={ details } />
+//             </span>
+//           </section>
+//           <button
+//             className="details-btn-startRecipe"
+//             type="button"
+//             data-testid="finish-recipe-btn"
+//             onClick={ () => isStart() }
+//           >
+//             Finalizar Receita
+//           </button>
+//         </section>
+//       </section>
+//     );
+//   }
+// }
 
-const mapDispatchToProps = (dispatch) => ({
-  isStart: () => dispatch(startRecipe()),
-  drinksDetails: (id) => dispatch(fetchDrinkDetails(id)),
-  foodDetails: (id) => dispatch(fetchFoodDetails(id)),
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   isStart: () => dispatch(startRecipe()),
+//   drinksDetails: (id) => dispatch(fetchDrinkDetails(id)),
+//   foodDetails: (id) => dispatch(fetchFoodDetails(id)),
+// });
 
-const mapStateToProps = (state) => ({
-  details: state.recipeDetails.details,
-});
+// const mapStateToProps = (state) => ({
+//   details: state.recipeDetails.details,
+// });
 
-Detalhes.propTypes = {
-  isStart: PropTypes.func.isRequired,
-  drinksDetails: PropTypes.func.isRequired,
-  foodDetails: PropTypes.func.isRequired,
-  details: PropTypes.shape.isRequired,
-  match: PropTypes.shape.isRequired,
-};
+// Detalhes.propTypes = {
+//   isStart: PropTypes.func.isRequired,
+//   drinksDetails: PropTypes.func.isRequired,
+//   foodDetails: PropTypes.func.isRequired,
+//   details: PropTypes.shape.isRequired,
+//   match: PropTypes.shape.isRequired,
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Progresso);
+// export default connect(mapStateToProps, mapDispatchToProps)(Progresso);
