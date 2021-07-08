316abde256284b7f0b954056d1d7b363677d141e
Adão Benites <adaobjr@gmail.com>
2021-07-08 07:28:09 -0400

    fix: :art: proposal adjust to fix bug in Recipe Page Footer [commented]

--- src/common/components/Footer/Footer.jsx
+++ src/common/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import drinkIcon from '../../../images/drinkIcon.svg';
 import exploreIcon from '../../../images/exploreIcon.svg';
 import mealIcon from '../../../images/mealIcon.svg';
@@ -35,4 +36,33 @@ export default function Footer() {
       />
     </footer>
   );
+
+  // return (
+  //   <footer data-testid="footer">
+  //     <Link to="/bebidas">
+  //       <img
+  //         type="image"
+  //         data-testid="drinks-bottom-btn"
+  //         src={ drinkIcon }
+  //         alt="bebidas"
+  //       />
+  //     </Link>
+  //     <Link to="/explorar">
+  //       <img
+  //         type="image"
+  //         data-testid="explore-bottom-btn"
+  //         src={ exploreIcon }
+  //         alt="explorar"
+  //       />
+  //     </Link>
+  //     <Link to="/comidas">
+  //       <img
+  //         type="image"
+  //         data-testid="food-bottom-btn"
+  //         src={ mealIcon }
+  //         alt="comidas"
+  //       />
+  //     </Link>
+  //   </footer>
+  // );
 }
