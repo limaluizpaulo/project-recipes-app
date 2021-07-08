e66f90b855a4ec6de0248236417a8fddb9608529
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-07-08 11:53:43 -0300

    Logica inicial da doneRecipes + CardDone

---
+++ src/components/CardRecipesDone.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import shareIcon from '../images/shareIcon.svg';
+
+export default function CardRecipesDone({ mealOrDrink, index }) {
+  return (
+    <section data-testid={ `${index}-horizontal-card` }>
+      <p>{mealOrDrink.id}</p>
+      <p>{mealOrDrink.type}</p>
+      <p>{mealOrDrink.area}</p>
+      <p>{mealOrDrink.alcoholicOrNot}</p>
+      <img
+        width="150px"
+        data-testid={ `${index}-horizontal-image` }
+        src={ mealOrDrink.image }
+        alt={ mealOrDrink.name }
+      />
+      <p data-testid={ `${index}-horizontal-top-text` }>{mealOrDrink.category}</p>
+      <p data-testid={ `${index}-horizontal-top-name` }>{mealOrDrink.name}</p>
+      <p data-testid={ `${index}-horizontal-done-date` }>{mealOrDrink.doneDate}</p>
+      <img
+        data-testid={ `${index}-horizontal-share-btn` }
+        src={ shareIcon }
+        alt={ mealOrDrink.name }
+      />
+      <p
+        data-testid={ `${index}-${mealOrDrink.tags}-horizontal-tag` }
+      >
+        {mealOrDrink.tags}
+      </p>
+    </section>
+  );
+}
+
+CardRecipesDone.propTypes = {
+  mealOrDrink: PropTypes.shape(Object).isRequired,
+  index: PropTypes.number.isRequired,
+};
--- src/pages/CookedRecipes.js
+++ src/pages/CookedRecipes.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import CardRecipesDone from '../components/CardRecipesDone';
 import Header from '../components/Header';
 
 export default function CookedRecipes() {
+  const doneRecipes = localStorage.getItem('doneRecipes');
+
   return (
-    <Header />
+    <>
+      <Header />
+      <Button data-testid="filter-by-all-btn">All</Button>
+      <Button data-testid="filter-by-food-btn">Food</Button>
+      <Button data-testid="filter-by-drink-btn">Drinks</Button>
+      {doneRecipes && doneRecipes.map((item, i) => (
+        <Link
+          key={ i }
+          to={ item.type === 'comida' ? `/comida/${item.id}` : `/bebida/${item.id}` }
+        >
+          <CardRecipesDone mealOrDrink={ item } index={ i } />
+        </Link>
+      ))}
+    </>
   );
 }
