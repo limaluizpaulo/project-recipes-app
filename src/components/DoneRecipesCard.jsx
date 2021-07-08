ddfee581fa0de8b2f8449a6a18360809a8d421b0
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-07-08 12:44:02 -0300

    Feat: Imports necessarios para o uso da mensagem de link copiado

--- src/components/DoneRecipesCard.jsx
+++ src/components/DoneRecipesCard.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 import Context from '../context/Context';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { getRecipesDone } from '../services/helpers/localStorage';
 import shareRecipe from '../images/shareIcon.svg';
