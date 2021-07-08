ba6fe5b322f13114f098a27cc11271327cdf0d54
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-07-08 16:10:30 -0300

    update...

--- src/pages/RecipesMade.js
+++ src/pages/RecipesMade.js
@@ -5,6 +5,7 @@ const RecipesMade = () => {
   console.log('');
   return (
     <div>
+      <button type="button" data-testid="filter-by-all-btn" />
       <Header title="Receitas Feitas" />
       <h1>Recipe Mades</h1>
     </div>
