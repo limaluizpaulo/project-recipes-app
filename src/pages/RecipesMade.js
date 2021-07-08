6145d22ddddb03da5da55ce35d42f2a94ac9f3b0
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-07-08 16:19:43 -0300

    update...

--- src/pages/RecipesMade.js
+++ src/pages/RecipesMade.js
@@ -14,76 +14,12 @@ function RecipesMade() {
   }, []);
 
   const renderCards = () => dones
-    .filter(({ type }) => type === filter || !filter).map((item, index) => {
-      const { id, category, doneDate, name, tags = [], image, area, alcoholicOrNot,
-        type } = item;
-      return (
-        <div key={ `${index} - ${name}` }>
-          <p>{tags}</p>
-          <Link to={ `${type}s/${id}` }>
-            <button
-              data-testid={ `${index}-horizontal-image` }
-              src={ image }
-              alt="Receita"
-              type="button"
-            />
-          </Link>
-          <p data-testid={ `${index}-horizontal-top-text` }>
-            {alcoholicOrNot || `${area} - ${category}`}
-          </p>
-          <Link to={ `${type}s/${id}` }>
-            <p data-testid={ `${index}-horizontal-name` }>{name}</p>
-          </Link>
-          <p data-testid={ `${index}-horizontal-done-date` }>{doneDate}</p>
-          <p data-testid={ `${index}-Pasta-horizontal-tag` }>{ tags || tags[0]}</p>
-          { tags[1] && <p data-testid={ `${index}-Curry-horizontal-tag` }>{tags[1]}</p>}
-          <ShareButton
-            url={ `http://localhost:3000/${type}s/${id}` }
-            msgShare={
-              <img
-                data-testid={ `${index}-horizontal-share-btn` }
-                src={ shareIcon }
-                alt="shareIcon"
-              />
-            }
-          />
-        </div>
+    
       );
     });
 
   return (
     <div>
-      <button type="button" data-testid="filter-by-all-btn" />
-      <Header title="Receitas Feitas" />
-      <button
-        type="button"
-        data-testid="filter-by-all-btn"
-        onClick={ () => setFilter() }
-      >
-        All
-      </button>
-      <button
-        type="button"
-        data-testid="filter-by-food-btn"
-        onClick={ () => setFilter('comida') }
-      >
-        Food
-      </button>
-      <button
-        type="button"
-        data-testid="filter-by-drink-btn"
-        onClick={ () => setFilter('bebida') }
-      >
-        Drinks
-      </button>
-      {dones && renderCards()}
-      {/*       <Link to="/comidas/52771">
-        <img
-          data-testid="0-horizontal-image"
-          src=""
-          alt="Receita"
-        />
-      </Link> */}
       
     </div>
   );
