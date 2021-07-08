c2d9dc05d286041ff6df99df1785c640a8cb6207
legalvao1 <legalvao1@gmail.com>
2021-07-08 15:45:56 -0300

    Requisito 75 e 76

--- src/components/IngredientCard.jsx
+++ src/components/IngredientCard.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+
+function UseGetImages(array) {
+  const link = useHistory();
+  const db = link.location.pathname.includes('comidas');
+  if (db) {
+    return array.map(({ strIngredient }) => (
+      `www.themealdb.com/images/ingredients/${strIngredient}-Small.png`));
+  }
+  return array.map(({ strIngredient1 }) => (
+    `www.thecocktaildb.com/images/ingredients/${strIngredient1}-Small.png`));
+}
 
 function IngredientCard(props) {
   const { ingredients } = props;
   const totalRecipes = 12;
   const food = ingredients.filter((elem, index) => index < totalRecipes);
 
-  const images = food.map(({ strIngredient }) => (
-    `www.themealdb.com/images/ingredients/${strIngredient}-Small.png`));
+  const images = UseGetImages(food);
 
   return food.map((recipe, index) => (
-    <Link key={ recipe.idIngredient } to="/comidas">
+    <Link key={ recipe.idIngredient || recipe.strIngredient1 } to="/comidas">
       <div data-testid={ `${index}-ingredient-card` }>
         <img
           data-testid={ `${index}-card-img` }
           src={ `https://${images[index]}` }
-          alt={ recipe.strIngredient }
+          alt={ recipe.strIngredient || recipe.strIngredient1 }
           width="60px"
         />
-        <h4 data-testid={ `${index}-card-name` }>{ recipe.strIngredient }</h4>
+        <h4 data-testid={ `${index}-card-name` }>
+          { recipe.strIngredient || recipe.strIngredient1 }
+        </h4>
       </div>
     </Link>
   ));
