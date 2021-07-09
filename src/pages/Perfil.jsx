0619974d74f2e3d59cabdbbfb373844f6d62e02f
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-07-09 18:09:07 -0300

    perfil feito

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Button from '../helpers/Button';
+import { getItem } from '../helpers/HelperFunctions';
 
 function Perfil() {
+  const { email } = getItem('user');
+  const history = useHistory();
+
   return (
-    <div>
+    <>
       <Header title="Perfil" />
+      <span data-testid="profile-email">{ email }</span>
+      <Button
+        label="Receitas Feitas"
+        testid="profile-done-btn"
+        func={ () => history.push('/receitas-feitas') }
+      />
+      <Button
+        label="Receitas Favoritas"
+        testid="profile-favorite-btn"
+        func={ () => history.push('/receitas-favoritas') }
+      />
+      <Button
+        label="Sair"
+        testid="profile-logout-btn"
+        func={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+      />
       <Footer />
-    </div>
+    </>
   );
 }
 
