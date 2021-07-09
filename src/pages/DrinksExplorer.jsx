a5d6a59aa61ca74d3900144a6732116f453e9662
Daniel Alves <araujodaniel321@gmail.com>
2021-07-09 16:53:32 -0300

    explorers 2/4 done

--- src/pages/DrinksExplorer.jsx
+++ src/pages/DrinksExplorer.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import DownMenu from '../components/DownMenu';
 
 class DrinksExplorer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 0,
+      redirect: false,
+    };
+    this.getRandom = this.getRandom.bind(this);
+    this.redirect = this.redirect.bind(this);
+  }
+
+  componentDidMount() {
+    this.getRandom();
+  }
+
+  async getRandom() {
+    const random = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+    const { drinks } = await random.json();
+    this.setState({ id: drinks[0].idDrink });
+  }
+
+  redirect() {
+    this.setState({ redirect: true });
+  }
+
   render() {
+    const { id, redirect } = this.state;
+    if (redirect) {
+      return <Redirect to={ `/bebidas/${id}` } />;
+    }
     return (
       <div>
         <Header header="Explorar Bebidas" explorer={ false } />
@@ -17,14 +45,13 @@ class DrinksExplorer extends Component {
           </button>
         </Link>
 
-        <Link to="/comidas/123456">
-          <button
-            type="button"
-            data-testid="explore-surprise"
-          >
-            Me Surpreenda!
-          </button>
-        </Link>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ this.redirect }
+        >
+          Me Surpreenda!
+        </button>
         <DownMenu />
       </div>
     );
--- src/pages/RecipesExplorer.jsx
+++ src/pages/RecipesExplorer.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import DownMenu from '../components/DownMenu';
 
 class RecipesExplorer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 0,
+      redirect: false,
+    };
+    this.getRandom = this.getRandom.bind(this);
+    this.redirect = this.redirect.bind(this);
+  }
+
+  componentDidMount() {
+    this.getRandom();
+  }
+
+  async getRandom() {
+    const random = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const { meals } = await random.json();
+    this.setState({ id: meals[0].idMeal });
+  }
+
+  redirect() {
+    this.setState({ redirect: true });
+  }
+
   render() {
+    const { id, redirect } = this.state;
+    if (redirect) {
+      return <Redirect to={ `/comidas/${id}` } />;
+    }
     return (
       <div>
         <Header
@@ -29,14 +57,14 @@ class RecipesExplorer extends Component {
           </button>
         </Link>
 
-        <Link to="/comidas/123456">
-          <button
-            type="button"
-            data-testid="explore-surprise"
-          >
-            Me Surpreenda!
-          </button>
-        </Link>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ this.redirect }
+        >
+          Me Surpreenda!
+        </button>
+
         <DownMenu />
       </div>
     );
