49c830fcc0510d46bb8ae17095c5048f62a299e4
Adão Benites <adaobjr@gmail.com>
2021-07-09 08:20:00 -0400

    fix: :bug: footer disappearing on Click

--- src/common/components/Footer/Footer.jsx
+++ src/common/components/Footer/Footer.jsx
@@ -1,68 +1,37 @@
-import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../../../images/drinkIcon.svg';
 import exploreIcon from '../../../images/exploreIcon.svg';
 import mealIcon from '../../../images/mealIcon.svg';
 import './footer.css';
 
 export default function Footer() {
-  const [whatPath, setWhatPath] = useState('');
-
-  if (whatPath) return <Redirect to={ `/${whatPath}` } />;
-
   return (
     <footer data-testid="footer">
-      <input
-        type="image"
-        data-testid="drinks-bottom-btn"
-        src={ drinkIcon }
-        alt="bebidas"
-        onClick={ () => setWhatPath('bebidas') }
-      />
-      <input
-        type="image"
-        data-testid="explore-bottom-btn"
-        src={ exploreIcon }
-        alt="explorar"
-        onClick={ () => setWhatPath('explorar') }
-      />
-      <input
-        type="image"
-        data-testid="food-bottom-btn"
-        src={ mealIcon }
-        alt="comidas"
-        onClick={ () => setWhatPath('comidas') }
-      />
+      <Link to="/bebidas">
+        <img
+          type="image"
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="bebidas"
+        />
+      </Link>
+      <Link to="/explorar">
+        <img
+          type="image"
+          data-testid="explore-bottom-btn"
+          src={ exploreIcon }
+          alt="explorar"
+        />
+      </Link>
+      <Link to="/comidas">
+        <img
+          type="image"
+          data-testid="food-bottom-btn"
+          src={ mealIcon }
+          alt="comidas"
+        />
+      </Link>
     </footer>
   );
-
-  // return (
-  //   <footer data-testid="footer">
-  //     <Link to="/bebidas">
-  //       <img
-  //         type="image"
-  //         data-testid="drinks-bottom-btn"
-  //         src={ drinkIcon }
-  //         alt="bebidas"
-  //       />
-  //     </Link>
-  //     <Link to="/explorar">
-  //       <img
-  //         type="image"
-  //         data-testid="explore-bottom-btn"
-  //         src={ exploreIcon }
-  //         alt="explorar"
-  //       />
-  //     </Link>
-  //     <Link to="/comidas">
-  //       <img
-  //         type="image"
-  //         data-testid="food-bottom-btn"
-  //         src={ mealIcon }
-  //         alt="comidas"
-  //       />
-  //     </Link>
-  //   </footer>
-  // );
 }
