21d5779e2314d6408d7e6e984c69deece987e1c3
lucas <lucasportellaagu@gmail.com>
2021-07-09 00:32:53 -0300

    req 50

--- src/pages/FoodInProgress.jsx
+++ src/pages/FoodInProgress.jsx
@@ -28,13 +28,13 @@ function FoodInProgress({ match, match: { params: { id } }, history }) {
     return (
       mealArray.map((_a, index) => (
         <section
-          className={ storageCheckGenerator(id, index) ? 'showCss' : 'hideCss' }
+          className={ checkBoolean(id, index) ? 'showCss' : 'hideCss' }
           data-testid={ `${index}-ingredient-step` }
           key={ `ingredientAndMeasure${index + 1}` }
         >
           <input
             checked={ checkBoolean(id, index) }
-            className={ storageCheckGenerator(id, index) ? 'showCss' : 'hideCss' }
+            className={ checkBoolean(id, index) ? 'showCss' : 'hideCss' }
             key={ index }
             type="checkbox"
             onClick={ () => {
@@ -43,7 +43,7 @@ function FoodInProgress({ match, match: { params: { id } }, history }) {
           />
           {IngredientsAndMeasures.ingredient[`strIngredient${index + 1}`]}
           <span
-            className={ storageCheckGenerator(id, index) ? 'showCss' : 'hideCss' }
+            className={ checkBoolean(id, index) ? 'showCss' : 'hideCss' }
           >
             {IngredientsAndMeasures.measure[`strMeasure${index + 1}`]}
           </span>
