20d5703fdc894c7e93e6ed4c051b93e8b75b50c8
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-07-09 18:36:54 -0300

    FEAT: craindo botoês de compartilhar e favorite

--- src/pages/ReceitasFavoritas.jsx
+++ src/pages/ReceitasFavoritas.jsx
@@ -1,28 +1,61 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 import FiltersButtons from '../components/FiltersButtons';
 import RecipesContext from '../Context/RecipesContext';
+import ShareButton from '../components/ShareButton';
+import FavoriteButton from '../components/FavoriteButton';
 
 function ReceitasFavoritas() {
   const { favoriteFilters } = useContext(RecipesContext);
-  console.log(favoriteFilters);
+
+  const [stateChangeHeart, setStateChangeHeart] = useState(true);
+  const { pathname } = useLocation();
+  const id = pathname.split('/')[2];
+
+  const removeFavorited = () => {
+    const favorited = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (favorited) {
+      const filterLocalStorage = favorited.filter((element) => element.id !== id);
+      localStorage.setItem('favoriteRecipes', JSON.stringify(filterLocalStorage));
+    }
+  };
+
+  const verifyHeart = () => {
+    const favorited = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (favorited) {
+      const filterLocalStorage = favorited.some((element) => element.id === id);
+      if (filterLocalStorage) {
+        setStateChangeHeart(false);
+      }
+    }
+  };
+
+  useEffect(verifyHeart, []);
+
   return (
     <>
       <Header />
       <FiltersButtons />
-      {favoriteFilters.map(({ image, category, name, id }) => (
-        <div key={ id }>
+      {favoriteFilters.map(({ image, category, name, id: idFavorite, area }, index) => (
+        <div key={ idFavorite }>
           <img
             src={ image }
             alt="xxxx"
-            data-testid={ `${id}-horizontal-image` }
+            data-testid={ `${index}-horizontal-image` }
             width="50px"
           />
-          <span data-testid={ `${id}-horizontal-name` }>{ name }</span>
-          <span data-testid={ `${id}-horizontal-top-text` }>{ category }</span>
+          <span data-testid={ `${index}-horizontal-name` }>{ name }</span>
+          <span data-testid={ `${index}-horizontal-top-text` }>{ category }</span>
+          <span>{ area }</span>
+          <ShareButton index={ index } />
+          <FavoriteButton
+            stateChangeHeart={ stateChangeHeart }
+            setStateChangeHeart={ setStateChangeHeart }
+            removeFavorited={ removeFavorited }
+          />
         </div>
       ))}
-      ;
     </>
 
   );
