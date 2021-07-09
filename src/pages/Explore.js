b2bc7a49db9fd413d597e35230650f5241a206d3
Fernando <fhomaia@gmail.com>
2021-07-09 18:46:54 -0300

    Feat: Requisitos 67 ao 69

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -7,6 +8,12 @@ class Explore extends React.Component {
     return (
       <section>
         <Header title="Explorar" />
+        <Link to="/explorar/comidas">
+          <button type="button" data-testid="explore-food">Explorar Comidas</button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+        </Link>
         <Footer />
       </section>
     );
