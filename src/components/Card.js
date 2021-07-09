39134788d20af3db90acc1fe62d06c975f7b1949
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-07-09 19:00:34 -0300

    Tira todas as barras das 'verifications' das rotas.

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,21 +5,21 @@ import PropTypes from 'prop-types';
 export default function Card({ mealOrDrink, index, testId }) {
   const { path } = useRouteMatch();
   // const searchId = path === '/comidas' ? 'idMeal' : 'idDrink';
-  const imgSrcKey = path === '/comidas' ? 'strMealThumb' : 'strDrinkThumb';
-  const titleKey = path === '/comidas' ? 'strMeal' : 'strDrink';
+  const imgSrcKey = path.includes('comidas') ? 'strMealThumb' : 'strDrinkThumb';
+  const titleKey = path.includes('comidas') ? 'strMeal' : 'strDrink';
   return (
     <section data-testid={ `${index}-${testId}-card` }>
       <img
         width="150px"
         data-testid={ `${index}-card-img` }
-        src={ mealOrDrink[`${imgSrcKey}`] }
-        alt={ mealOrDrink[`${titleKey}`] }
+        src={ mealOrDrink[imgSrcKey] }
+        alt={ mealOrDrink[titleKey] }
       />
       <h3
         data-testid={ (testId === 'recipe') ? `${index}-card-name`
           : `${index}-recomendation-title` }
       >
-        {mealOrDrink[`${titleKey}`]}
+        {mealOrDrink[titleKey]}
       </h3>
     </section>
   );
