de78d038c9c59db97f4d594b45b0691a48bf7a63
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-07-09 16:29:31 -0300

    FEAT: Renderizando card de acordo com os filtros

--- src/components/FiltersButtons.jsx
+++ src/components/FiltersButtons.jsx
@@ -3,26 +3,21 @@ import RecipesContext from '../Context/RecipesContext';
 
 export default function FiltersButtons() {
   const favoriteRecipesStorage = JSON.parse(localStorage.getItem('favoriteRecipes'));
-  const { favoriteFilters, setFavoriteFilters } = useContext(RecipesContext);
+  const { setFavoriteFilters } = useContext(RecipesContext);
 
   const filterFood = () => {
     const filter = favoriteRecipesStorage.filter(({ type }) => type === 'comida');
     setFavoriteFilters(filter);
-    console.log(filter);
   };
 
   const filterDrink = () => {
     const filterDrinks = favoriteRecipesStorage.filter(({ type }) => type === 'bebida');
     setFavoriteFilters(filterDrinks);
-    console.log(filterDrinks);
   };
 
   const filterAll = () => {
-    const filters = favoriteRecipesStorage.filter(
-      ({ type }) => type === 'comida' && type === 'bebida',
-    );
+    const filters = favoriteRecipesStorage;
     setFavoriteFilters(filters);
-    console.log(filters);
   };
 
   return (
--- src/pages/ReceitasFavoritas.jsx
+++ src/pages/ReceitasFavoritas.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import FiltersButtons from '../components/FiltersButtons';
+import RecipesContext from '../Context/RecipesContext';
 
 function ReceitasFavoritas() {
-  // const { favoriteFilters } = useContext(RecipesContext);
+  const { favoriteFilters } = useContext(RecipesContext);
+  console.log(favoriteFilters);
   return (
     <>
       <Header />
       <FiltersButtons />
-      <div />
+      {favoriteFilters.map(({ image, category, name, id }) => (
+        <div key={ id }>
+          <img
+            src={ image }
+            alt="xxxx"
+            data-testid={ `${id}-horizontal-image` }
+            width="50px"
+          />
+          <span data-testid={ `${id}-horizontal-name` }>{ name }</span>
+          <span data-testid={ `${id}-horizontal-top-text` }>{ category }</span>
+        </div>
+      ))}
+      ;
     </>
 
   );
 }
 
 export default ReceitasFavoritas;
+
+// imagem, texto de categoria, nome, alcaholicOrNot (é pra dizer se é alcólico ou não )
