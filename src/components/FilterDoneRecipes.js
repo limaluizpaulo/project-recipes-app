26caf60a713f9c5fac9268a51a602d63f4a416a3
isTorres <ismaeltorres612@gmail.com>
2021-07-09 23:58:21 -0300

    add default css

--- src/components/FilterDoneRecipes.js
+++ src/components/FilterDoneRecipes.js
@@ -1,5 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import React from 'react';
+import '../styles/card.css';
 
 export default function FilterDoneRecipes() {
   const dale = JSON.parse(localStorage.getItem('doneRecipes'));
@@ -21,14 +22,14 @@ export default function FilterDoneRecipes() {
           Drinks
         </Button>
       </div>
-      <div>
+      <div className="card-container">
         { dale.length <= 1 ? <p>Não há receitas feitas</p>
           : dale.slice(1).map((el) => (
-            <div key={ el.id }>
-              <img src={ el.image } alt={ el.name } />
+            <button type="button" className="recipe-card" key={ el.id }>
+              <img className="card-img" src={ el.image } alt={ el.name } />
               <span>{ `${el.area} - ${el.category}` }</span>
               <h3>{ el.name }</h3>
-            </div>
+            </button>
           ))}
       </div>
     </div>
