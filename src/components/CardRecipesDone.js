37e2ed55fcb59f0099f3d6ebd1fc307fe6f139ce
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-07-10 16:54:16 -0300

    Some improvement on Cards!!

--- src/components/CardRecipesDone.js
+++ src/components/CardRecipesDone.js
@@ -1,38 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 import shareIcon from '../images/shareIcon.svg';
 
 export default function CardRecipesDone({ mealOrDrink, index }) {
+  const { id, type, image, name, category, doneDate, tags } = mealOrDrink;
   return (
-    <section data-testid={ `${index}-horizontal-card` }>
-      <p>{mealOrDrink.id}</p>
-      <p>{mealOrDrink.type}</p>
-      <p>{mealOrDrink.area}</p>
-      <p>{mealOrDrink.alcoholicOrNot}</p>
-      <img
-        width="150px"
-        data-testid={ `${index}-horizontal-image` }
-        src={ mealOrDrink.image }
-        alt={ mealOrDrink.name }
-      />
-      <p data-testid={ `${index}-horizontal-top-text` }>{mealOrDrink.category}</p>
-      <p data-testid={ `${index}-horizontal-top-name` }>{mealOrDrink.name}</p>
-      <p data-testid={ `${index}-horizontal-done-date` }>{mealOrDrink.doneDate}</p>
+    <div data-testid={ `${index}-horizontal-card` }>
+      <Link
+        to={ type === 'comida' ? `/comidas/${id}` : `/bebidas/${id}` }
+      >
+        <img
+          width="150px"
+          data-testid={ `${index}-horizontal-image` }
+          src={ image }
+          alt={ name }
+        />
+      </Link>
+
+      <p data-testid={ `${index}-horizontal-top-text` }>{category}</p>
+
+      <Link
+        to={ type === 'comida' ? `/comidas/${id}` : `/bebidas/${id}` }
+      >
+        <p data-testid={ `${index}-horizontal-name` }>{name}</p>
+      </Link>
+
+      <p data-testid={ `${index}-horizontal-done-date` }>{doneDate}</p>
+
       <Button>
         <img
           data-testid={ `${index}-horizontal-share-btn` }
           src={ shareIcon }
-          alt={ mealOrDrink.name }
+          alt={ name }
         />
       </Button>
-      <p
-        data-testid={ `${index}-${mealOrDrink.tags}-horizontal-tag` }
-      >
-        {mealOrDrink.tags}
-      </p>
-    </section>
+
+      {tags && tags.map((tag, i) => (
+        <p
+          data-testid={ `${index}-${tag}-horizontal-tag` }
+          key={ `${i} - ${tag}` }
+        >
+          {tag}
+        </p>
+      ))}
+
+    </div>
   );
 }
 
