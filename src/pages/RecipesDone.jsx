7472317b7fb08d83a3b1f43dce17d29f734ae394
Victor Lopes <victor.cabrera0209@gmail.com>
2021-07-09 21:39:26 -0300

    fixing a little bug

--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -4,7 +4,6 @@ import Header from '../components/Header';
 function RecipesDone() {
   return (
     <>
-      <div>Tela de receitas feitas</div>
       <Header title="Receitas Feitas" />
       <button data-testid="filter-by-all-btn" type="button">Todas</button>
       <button data-testid="filter-by-food-btn" type="button">Comidas</button>
