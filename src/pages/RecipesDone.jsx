a754b5c9685b71f0d72eac766569f85722c297eb
Victor Lopes <victor.cabrera0209@gmail.com>
2021-07-10 16:57:36 -0300

    Req 59

--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import { PropTypes } from 'prop-types';
 import { copyEachLink } from '../services/functions';
 
 import Header from '../components/Header';
 import shareIcon from '../images/shareIcon.svg';
 
-function RecipesDone() {
+function RecipesDone({ history }) {
   const [isCopied, setIsCopied] = useState([]);
   const rawDestructuredStorage = localStorage.getItem('doneRecipes');
   const destructuredStorage = JSON.parse(rawDestructuredStorage);
@@ -18,18 +19,32 @@ function RecipesDone() {
       }
       return (
         <section key={ index }>
-          <img
-            data-testid={ `${index}-horizontal-image` }
-            alt="horizontal"
-            src={ each.image }
-            width="200px"
-          />
+          <button
+            type="button"
+            onClick={ () => history.push(`/${each.type}s/${each.id}`) }
+          >
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              alt="horizontal"
+              src={ each.image }
+              width="200px"
+            />
+          </button>
           <h1
             data-testid={ `${index}-horizontal-top-text` }
           >
             {AlcoholicAreaCategory}
           </h1>
-          <h1 data-testid={ `${index}-horizontal-name` }>{each.name}</h1>
+          <h1
+            data-testid={ `${index}-horizontal-name` }
+          >
+            <button
+              type="button"
+              onClick={ () => history.push(`/${each.type}s/${each.id}`) }
+            >
+              {each.name}
+            </button>
+          </h1>
           <p data-testid={ `${index}-horizontal-done-date` }>{each.doneDate}</p>
           <button
             type="button"
@@ -59,5 +74,9 @@ function RecipesDone() {
     </>
   );
 }
+
+RecipesDone.propTypes = {
+  history: PropTypes.shape().isRequired,
+};
 
 export default RecipesDone;
