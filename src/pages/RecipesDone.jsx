197895a6f0fb9eb7b3a3134fd2f4ccb505aae82c
Victor Lopes <victor.cabrera0209@gmail.com>
2021-07-10 15:20:13 -0300

    Req 56

--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -4,41 +4,49 @@ import shareIcon from '../images/shareIcon.svg';
 
 function RecipesDone() {
   const rawDestructuredStorage = localStorage.getItem('doneRecipes');
-  const destructuredStorage = JSON.parse(rawDestructuredStorage)[0];
-  const { image, area, category, name, doneDate, tags } = destructuredStorage;
+  const destructuredStorage = JSON.parse(rawDestructuredStorage);
+  let a;
+  const checkAlcoholicOrNot = () => (
+    destructuredStorage.map((each, index) => {
+      if (each.alcoholicOrNot.length > 0) {
+        a = each.alcoholicOrNot;
+      } else {
+        a = `${each.area} - ${each.category}`;
+      }
+      return (
+        <section key={ index }>
+          <img
+            data-testid={ `${index}-horizontal-image` }
+            alt="horizontal"
+            src={ each.image }
+            width="200px"
+          />
+          <h1
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            {a}
+          </h1>
+          <h1 data-testid={ `${index}-horizontal-name` }>{each.name}</h1>
+          <p data-testid={ `${index}-horizontal-done-date` }>{each.doneDate}</p>
+          <button type="button">
+            <img
+              data-testid={ `${index}-horizontal-share-btn` }
+              src={ shareIcon }
+              alt="shareIcon"
+            />
+          </button>
+          <div data-testid={ `${index}-Pasta-horizontal-tag` }>{each.tags[0]}</div>
+          <div data-testid={ `${index}-Curry-horizontal-tag` }>{each.tags[1]}</div>
+        </section>
+      );
+    }));
   return (
     <>
       <Header title="Receitas Feitas" />
       <button data-testid="filter-by-all-btn" type="button">Todas</button>
       <button data-testid="filter-by-food-btn" type="button">Comidas</button>
       <button data-testid="filter-by-drink-btn" type="button">Bebidas</button>
-      <img
-        data-testid="0-horizontal-image"
-        alt="horizontal"
-        src={ image }
-        width="200px"
-      />
-      <h1
-        data-testid="0-horizontal-top-text"
-      >
-        {`${area} - ${category}`}
-      </h1>
-      <h1 data-testid="0-horizontal-name">{name}</h1>
-      <p data-testid="0-horizontal-done-date">{doneDate}</p>
-      <button type="button">
-        <img
-          data-testid="0-horizontal-share-btn"
-          src={ shareIcon }
-          alt="shareIcon"
-        />
-      </button>
-      <div data-testid="0-Pasta-horizontal-tag">{tags[0]}</div>
-      <div data-testid="0-Curry-horizontal-tag">{tags[1]}</div>
-      <img data-testid="1-horizontal-image" alt="horizontal" />
-      <h1 data-testid="1-horizontal-top-text">b</h1>
-      <h1 data-testid="1-horizontal-name">b</h1>
-      <button data-testid="1-horizontal-share-btn" type="button">b</button>
-      <p data-testid="1-horizontal-done-date">b</p>
+      {checkAlcoholicOrNot()}
     </>
   );
 }
