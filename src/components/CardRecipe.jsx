8337729012cced089ab057bb0149413513729791
Misael Bueno <misa.hbueno@gmail.com>
2021-07-11 10:59:36 -0300

    acertando lint

--- src/components/CardRecipe.jsx
+++ src/components/CardRecipe.jsx
@@ -1,20 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import FetchContext from '../context/FetchContext';
 import copy from 'clipboard-copy';
+import { Link } from 'react-router-dom';
+import FetchContext from '../context/FetchContext';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
-import blackHeartIcon from '../images/blackHeartIcon.svg'
-import { Link } from 'react-router-dom';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function CardRecipe({ id }) {
   const { data, imgRecipes, nameRecipes, typeFunc } = useContext(FetchContext);
   const [checkArr, setCheckArr] = useState([]);
-  const [msg, setMsg] = useState(false)
+  const [msg, setMsg] = useState(false);
   const [favorite, setFavorite] = useState(false);
+  const [ingredients, setIngredients] = useState([]);
+  const ingredientArr = ingredients;
 
   const progressObject = JSON.parse(localStorage.getItem('inProgressRecipes'));
-  const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'))
+  const recipesFavorite = JSON.parse(localStorage.getItem('favoriteRecipes'));
 
   if (progressObject === null) {
     const inProgressRecipes = {
@@ -24,27 +26,16 @@ function CardRecipe({ id }) {
     localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipes));
   }
 
-  if (favoriteRecipes === null) {
-    const favoriteRecipes = []
-    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes))
+  if (recipesFavorite === null) {
+    const favoriteRecipes = [];
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
   }
 
   useEffect(() => {
-    if (favoriteRecipes !== null) {
-      favoriteRecipes.map((recipe) => {
-        recipe.id === id && setFavorite(true)
-      })
+    if (recipesFavorite !== null) {
+      recipesFavorite.map((recipe) => recipe.id === id && setFavorite(true));
     }
-  }, [])
-
-  if (data.length > 0) {
-    const result = Object.keys(data[0]);
-    var filterIngredients = result.filter(
-      (res) => res.includes('strIngredient') && data[0][res],
-    );
-  }
-
-  console.log(filterIngredients)
+  }, [recipesFavorite, id]);
 
   const renderCheckbox = () => {
     const result = Object.keys(data[0]);
@@ -60,6 +51,7 @@ function CardRecipe({ id }) {
     if (checkArr.length === 0) {
       const newCheck = filterIngredients.map(() => false);
       setCheckArr(newCheck);
+      setIngredients(filterIngredients);
       if (progressObject[typeFunc][id] === undefined) {
         progressObject[typeFunc][id] = [];
         console.log(progressObject);
@@ -84,21 +76,21 @@ function CardRecipe({ id }) {
         return localStorage.setItem('inProgressRecipes', JSON.stringify(progressObject));
       }
 
-      console.log(progressObject[typeFunc][id])
+      console.log(progressObject[typeFunc][id]);
       progressObject[typeFunc][id].splice(progressObject[typeFunc][id].indexOf(num), 1);
       localStorage.setItem('inProgressRecipes', JSON.stringify(progressObject));
     }
 
     return filterIngredients.map((ingredient, index) => (
-      <li key={ingredient} data-testid={`${index}-ingredient-step`}>
-        <label htmlFor={ingredient}>
+      <li key={ ingredient } data-testid={ `${index}-ingredient-step` }>
+        <label htmlFor={ ingredient }>
           <input
             type="checkbox"
-            id={ingredient}
-            checked={checkArr[index]}
-            onClick={() => changeCheck(index)}
+            id={ ingredient }
+            checked={ checkArr[index] }
+            onClick={ () => changeCheck(index) }
           />
-          {`${data[0][ingredient]} - ${data[0][filterMeasures[index]]}`}
+          { `${data[0][ingredient]} - ${data[0][filterMeasures[index]]}` }
         </label>
       </li>
     ));
@@ -107,8 +99,7 @@ function CardRecipe({ id }) {
   const shareLink = () => {
     if (typeFunc === 'meals') {
       copy(`http://localhost:3000/comidas/${id}`);
-    }
-    else {
+    } else {
       copy(`http://localhost:3000/bebidas/${id}`);
     }
 
@@ -117,77 +108,80 @@ function CardRecipe({ id }) {
     const TWO_SECONDS = 2000;
     setTimeout(() => {
       setMsg(false);
-    }, TWO_SECONDS)
-  }
+    }, TWO_SECONDS);
+  };
 
   const favoriteBtn = () => {
-    setFavorite(!favorite)
+    setFavorite(!favorite);
 
     if (favorite) {
-      favoriteRecipes.map((recipe, index) => {
-        recipe.id === id && favoriteRecipes.splice(index, 1);
-      })
+      recipesFavorite.map((res, ind) => res.id === id && recipesFavorite.splice(ind, 1));
 
-      return localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes))
+      return localStorage.setItem('favoriteRecipes', JSON.stringify(recipesFavorite));
     }
 
     let newFavorite = [];
     if (typeFunc === 'meals') {
       newFavorite = [
-        ...favoriteRecipes,
+        ...recipesFavorite,
         {
-          id: id,
+          id,
           type: 'comida',
           area: data[0].strArea,
           category: data[0].strCategory,
           alcoholicOrNot: '',
           name: data[0].strMeal,
           image: data[0].strMealThumb,
-        }
-      ]
+        },
+      ];
     } else {
       newFavorite = [
-        ...favoriteRecipes,
+        ...recipesFavorite,
         {
-          id: id,
+          id,
           type: 'bebida',
           area: '',
           category: data[0].strCategory,
           alcoholicOrNot: data[0].strAlcoholic,
           name: data[0].strDrink,
           image: data[0].strDrinkThumb,
-        }
-      ]
+        },
+      ];
     }
 
-    return localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorite))
-  }
+    return localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorite));
+  };
 
   return (
     <div>
       {
         data.map((recipe) => (
-          <div key={recipe}>
-            <img data-testid="recipe-photo" src={recipe[imgRecipes]} alt="" />
-            <h2 data-testid="recipe-title">{recipe[nameRecipes]}</h2>
-            { msg && <div>Link copiado!</div>}
-            <button data-testid="share-btn" type="button" onClick={shareLink}>
-              <img src={shareIcon} alt="botão de compartilhar" />
+          <div key={ recipe }>
+            <img data-testid="recipe-photo" src={ recipe[imgRecipes] } alt="" />
+            <h2 data-testid="recipe-title">{ recipe[nameRecipes] }</h2>
+            { msg && <div>Link copiado!</div> }
+            <button data-testid="share-btn" type="button" onClick={ shareLink }>
+              <img src={ shareIcon } alt="botão de compartilhar" />
             </button>
-            <button onClick={favoriteBtn}>
-              <img data-testid="favorite-btn" type="button" src={favorite ? blackHeartIcon : whiteHeartIcon} alt="botão de favoritar" />
+            <button type="button" onClick={ favoriteBtn }>
+              <img
+                data-testid="favorite-btn"
+                type="button"
+                src={ favorite ? blackHeartIcon : whiteHeartIcon }
+                alt="botão de favoritar"
+              />
             </button>
-            <h4 data-testid="recipe-category">{recipe.strCategory}</h4>
+            <h4 data-testid="recipe-category">{ recipe.strCategory }</h4>
             <ul>
-              {renderCheckbox()}
+              { renderCheckbox() }
             </ul>
             <h2>Instructions</h2>
-            <p data-testid="instructions">{recipe.strInstructions}</p>
+            <p data-testid="instructions">{ recipe.strInstructions }</p>
             <Link to="/receitas-feitas">
               <button
                 type="button"
                 data-testid="finish-recipe-btn"
-                disabled={ checkArr.filter((res) => res).length !== filterIngredients.length }
+                disabled={ checkArr.filter((res) => res).length !== ingredientArr.length }
               >
                 Finalizar Receita
               </button>
