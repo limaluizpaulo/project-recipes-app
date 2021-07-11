05bd03394d1af3d9d91ce2ce981156c8a83de184
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-07-11 12:42:14 -0300

    FEAT: realizado requisitos , faltante somente o copy  e erros de lint

--- src/pages/ReceitasFavoritas.jsx
+++ src/pages/ReceitasFavoritas.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { /* useLocation */ useHistory } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import FiltersButtons from '../components/FiltersButtons';
 import RecipesContext from '../Context/RecipesContext';
@@ -8,72 +8,63 @@ import ScreenFavoriteButton from '../components/ScreenFavoriteButton';
 
 function ReceitasFavoritas() {
   const { favoriteFilters } = useContext(RecipesContext);
-
-  // const [stateChangeHeart, setStateChangeHeart] = useState(true);
-  // const { pathname } = useLocation();
   const history = useHistory();
-  // const idObj = pathname.split('/')[2];
 
-  // const removeFavorited = () => {
-  //   const favorited = JSON.parse(localStorage.getItem('favoriteRecipes'));
-  //   if (favorited) {
-  //     const filterLocalStorage = favorited.filter((element) => element.idObj !== idObj);
-  //     localStorage.setItem('favoriteRecipes', JSON.stringify(filterLocalStorage));
-  //   }
-  // };
-
-  // const verifyHeart = () => {
-  //   const favorited = JSON.parse(localStorage.getItem('favoriteRecipes'));
-  //   if (favorited) {
-  //     const filterLocalStorage = favorited.some((element) => element.idObj === idObj);
-  //     if (filterLocalStorage) {
-  //       setStateChangeHeart(false);
-  //     }
-  //   }
-  // };
-
-  // useEffect(verifyHeart, []);
   return (
     <>
       <Header />
       <FiltersButtons />
-      { favoriteFilters === null ? <p /> : favoriteFilters.map(({ image, category, name, id, area, type, alcoholicOrNot }, index) => (
-        <div key={ id }>
-          <img
-            src={ image }
-            alt="xxxx"
-            data-testid={ `${index}-horizontal-image` }
-            width="50px"
-            onClick={ () => history.push(`/${type}s/${id}`) }
-          />
-          <span
-            data-testid={ `${index}-horizontal-name` }
-            onClick={ () => history.push(`/${type}s/${id}`) }
-          >
-            { name }
-
-          </span>
+      { favoriteFilters === null
+        ? <p />
+        : favoriteFilters.map((
           {
-            area && <span data-testid={ `${index}-horizontal-top-text` }>{ `${area} - ${category}` }</span>
-          }
-          <ShareButtonPerfil type={ type } id={ id } index={ index } />
-          <ScreenFavoriteButton
-            // stateChangeHeart={ stateChangeHeart }
-            // setStateChangeHeart={ setStateChangeHeart }
-            // removeFavorited={ removeFavorited }
-            id={ id }
-            index={ index }
-          />
-          {
-            alcoholicOrNot && <h3 data-testid={ `${index}-horizontal-top-text` }>{ alcoholicOrNot }</h3>
-          }
-        </div>
-      ))}
+            image,
+            category,
+            name,
+            id,
+            area,
+            type,
+            alcoholicOrNot,
+          }, index,
+        ) => (
+          <div key={ id }>
+            <button type="button" onClick={ () => history.push(`/${type}s/${id}`) }>
+              <img
+                src={ image }
+                alt="xxxx"
+                data-testid={ `${index}-horizontal-image` }
+                width="50px"
+              />
+            </button>
+            <button type="button" onClick={ () => history.push(`/${type}s/${id}`) }>
+              <span
+                data-testid={ `${index}-horizontal-name` }
+              >
+                { name }
+              </span>
+            </button>
+            {
+              area
+            && <span
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              { `${area} - ${category}` }
+               </span>
+            }
+            <ShareButtonPerfil type={ type } id={ id } index={ index } />
+            <ScreenFavoriteButton
+              id={ id }
+              index={ index }
+            />
+            {
+              alcoholicOrNot
+            && <h3 data-testid={ `${index}-horizontal-top-text` }>{ alcoholicOrNot }</h3>
+            }
+          </div>
+        ))}
     </>
 
   );
 }
 
 export default ReceitasFavoritas;
-
-// imagem, texto de categoria, nome, alcaholicOrNot (é pra dizer se é alcólico ou não )
