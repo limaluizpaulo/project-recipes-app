3741a61e8ab80c589715da5d7edfeff54a43086e
Victor Lopes <victor.cabrera0209@gmail.com>
2021-07-10 23:18:53 -0300

    RecipeDone Screen Finished

--- src/pages/RecipesDone.jsx
+++ src/pages/RecipesDone.jsx
@@ -1,76 +1,67 @@
 import React, { useState } from 'react';
 import { PropTypes } from 'prop-types';
-import { copyEachLink } from '../services/functions';
 
 import Header from '../components/Header';
-import shareIcon from '../images/shareIcon.svg';
+import BodyRecipesDone from '../components/BodyRecipesDone';
 
-function RecipesDone({ history }) {
-  const [isCopied, setIsCopied] = useState([]);
+export default function RecipesDone({ history }) {
+  const [whatIsActivated, setWhatIsActivated] = useState(0);
   const rawDestructuredStorage = localStorage.getItem('doneRecipes');
   const destructuredStorage = JSON.parse(rawDestructuredStorage);
-  let AlcoholicAreaCategory;
-  const checkAlcoholicOrNot = () => (
-    destructuredStorage.map((each, index) => {
-      if (each.alcoholicOrNot.length > 0) {
-        AlcoholicAreaCategory = each.alcoholicOrNot;
-      } else {
-        AlcoholicAreaCategory = `${each.area} - ${each.category}`;
+  const attStateFilter = (selected) => {
+    setWhatIsActivated(selected);
+  };
+  const renderFilteredList = () => {
+    let filteredList = [];
+    if (whatIsActivated === 0) {
+      filteredList = destructuredStorage;
+    }
+    if (whatIsActivated === 1) {
+      filteredList = destructuredStorage
+        .filter((eachOne) => eachOne.type === 'comida');
+    }
+    if (whatIsActivated === 2) {
+      filteredList = destructuredStorage
+        .filter((eachOne) => eachOne.type === 'bebida');
+    }
+    return (filteredList.map((each, index) => {
+      if (whatIsActivated === 0) {
+        return <BodyRecipesDone index={ index } history={ history } each={ each } />;
       }
-      return (
-        <section key={ index }>
-          <button
-            type="button"
-            onClick={ () => history.push(`/${each.type}s/${each.id}`) }
-          >
-            <img
-              data-testid={ `${index}-horizontal-image` }
-              alt="horizontal"
-              src={ each.image }
-              width="200px"
-            />
-          </button>
-          <h1
-            data-testid={ `${index}-horizontal-top-text` }
-          >
-            {AlcoholicAreaCategory}
-          </h1>
-          <h1
-            data-testid={ `${index}-horizontal-name` }
-          >
-            <button
-              type="button"
-              onClick={ () => history.push(`/${each.type}s/${each.id}`) }
-            >
-              {each.name}
-            </button>
-          </h1>
-          <p data-testid={ `${index}-horizontal-done-date` }>{each.doneDate}</p>
-          <button
-            type="button"
-            onClick={ () => setIsCopied(
-              copyEachLink(`/${each.type}s/${each.id}`, index),
-            ) }
-          >
-            <img
-              data-testid={ `${index}-horizontal-share-btn` }
-              src={ shareIcon }
-              alt="shareIcon"
-            />
-            {isCopied[index] ? <p>Link copiado!</p> : null }
-          </button>
-          <div data-testid={ `${index}-Pasta-horizontal-tag` }>{each.tags[0]}</div>
-          <div data-testid={ `${index}-Curry-horizontal-tag` }>{each.tags[1]}</div>
-        </section>
-      );
+      if (whatIsActivated === 1/*  && each.type === 'comida' */) {
+        return <BodyRecipesDone index={ index } history={ history } each={ each } />;
+      }
+      if (whatIsActivated === 2/*  && each.type === 'bebida' */) {
+        return <BodyRecipesDone index={ index } history={ history } each={ each } />;
+      }
+      return null;
     }));
+  };
   return (
     <>
       <Header title="Receitas Feitas" />
-      <button data-testid="filter-by-all-btn" type="button">Todas</button>
-      <button data-testid="filter-by-food-btn" type="button">Comidas</button>
-      <button data-testid="filter-by-drink-btn" type="button">Bebidas</button>
-      {checkAlcoholicOrNot()}
+      <button
+        data-testid="filter-by-all-btn"
+        onClick={ () => attStateFilter(0) }
+        type="button"
+      >
+        Todas
+      </button>
+      <button
+        data-testid="filter-by-food-btn"
+        onClick={ () => attStateFilter(1) }
+        type="button"
+      >
+        Comidas
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        onClick={ () => attStateFilter(2) }
+        type="button"
+      >
+        Bebidas
+      </button>
+      {renderFilteredList()}
     </>
   );
 }
@@ -78,5 +69,3 @@ function RecipesDone({ history }) {
 RecipesDone.propTypes = {
   history: PropTypes.shape().isRequired,
 };
-
-export default RecipesDone;
