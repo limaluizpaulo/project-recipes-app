b254b7b93d0b238becbae188c5cf6233519f67f1
Daniel Alves <araujodaniel321@gmail.com>
2021-07-12 17:55:28 -0300

    re-trying to merge

--- src/pages/RecipesByOrigin.jsx
+++ src/pages/RecipesByOrigin.jsx
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import Dropdown from 'react-dropdown';
 import Header from '../components/Header';
 import DownMenu from '../components/DownMenu';
 
 class RecipesByOrigin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      areas: [],
+    };
+    this.getLocation = this.getLocation.bind(this);
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  async getLocation() {
+    const result = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
+    const { meals } = await result.json();
+    this.setState({ areas: meals });
+  }
+
   render() {
+    const { areas } = this.state;
+    const something = { ...areas };
+    console.log(...areas);
+    console.log(something[0]);
+    const options = something;
+
     return (
       <div>
         <Header header="Explorar Origem" explorer />
+        <div data-testid="explore-by-area-dropdown">
+          <Dropdown
+            options={ options }
+            placeholder="select something"
+          />
+        </div>
         <h2> presunto</h2>
         <DownMenu />
       </div>
