c339252aac750bc85edf1e928fa671cdac3322de
Flavio Andrade <78602566+Flavio-Oliveira-Andrade@users.noreply.github.com>
2021-07-12 20:09:14 -0300

    Revert "[ Group 19 ] -  explore screen"

--- src/App.js
+++ src/App.js
@@ -8,12 +8,6 @@ import Login from './common/pages/Login';
 import Profile from './common/pages/Profile';
 import Recipes from './common/pages/Recipes';
 import Provider from './context/Provider';
-import ExploreMeals from './common/pages/ExploreMeals';
-import ExploreDrinks from './common/pages/ExploreDrinks';
-import DrinksIngrediant from './common/pages/DrinksIngrediant';
-import MealsIngredient from './common/pages/MealsIngredient';
-import MealsOrigem from './common/pages/MealsOrigem';
-import NotFound from './common/pages/NotFound';
 import FavoriteRecipes from './common/pages/FavoriteRecipes';
 
 function App() {
@@ -30,13 +24,7 @@ function App() {
         <Route exact path="/receitas-feitas" component={ DoneRecipes } />
         <Route path="/receitas-favoritas" component={ FavoriteRecipes } />
         <Route path="/perfil" component={ Profile } />
-        <Route exact path="/explorar" component={ Explorer } />
-        <Route exact path="/explorar/comidas" component={ ExploreMeals } />
-        <Route exact path="/explorar/bebidas" component={ ExploreDrinks } />
-        <Route path="/explorar/bebidas/ingredientes" component={ DrinksIngrediant } />
-        <Route path="/explorar/comidas/ingredientes" component={ MealsIngredient } />
-        <Route exact path="/explorar/comidas/area" component={ MealsOrigem } />
-        <Route exact path="/explorar/bebidas/area" component={ NotFound } />
+        <Route path="/explorar" component={ Explorer } />
       </Switch>
     </Provider>
   );
