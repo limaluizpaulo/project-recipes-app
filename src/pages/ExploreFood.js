204079c8263d186d851a5817b4505f03f886d116
Daniel <dsouzaborini@gmail.com>
2021-07-12 16:18:47 -0300

    add all req

--- src/pages/ExploreFood.js
+++ src/pages/ExploreFood.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import fetchAPI from '../services/fetchApi';
 
 class ExploreFood extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      randomId: '',
+    };
+    this.fetchRandom = this.fetchRandom.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchRandom();
+  }
+
+  async fetchRandom() {
+    const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    const random = await fetchAPI(url);
+    const randomId = random.meals[0].idMeal;
+    this.setState({
+      randomId,
+    });
+  }
+
   render() {
+    const { randomId } = this.state;
+    const url = `/comidas/${randomId}`;
     return (
       <section>
         <Header title="Explorar Comidas" />
+        <Link to="/explorar/comidas/ingredientes">
+          <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
+        </Link>
+        <Link to="/explorar/comidas/area">
+          <button type="button" data-testid="explore-by-area">Por Local de Origem</button>
+        </Link>
+        <Link to={ url }>
+          <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
+        </Link>
         <Footer />
       </section>
     );
