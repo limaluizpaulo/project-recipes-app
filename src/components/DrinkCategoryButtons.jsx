ec5a6edc9517d4bf5932255b7a7047316db9e6f6
Camila Arruda <camilaam1802@gmail.com>
2021-07-12 16:29:32 -0300

    Requisito 29 completo

--- src/components/DrinkCategoryButtons.jsx
+++ src/components/DrinkCategoryButtons.jsx
@@ -3,7 +3,8 @@ import ContextRecipes from '../context/contextRecipes';
 
 function DrinkCategoryButtons() {
   const { setDrinks,
-    drinkCategoryName, setDrinkCategoryName } = useContext(ContextRecipes);
+    drinkCategoryName, setDrinkCategoryName, setToggleFood,
+    toggleFood } = useContext(ContextRecipes);
   const maxLength = 4;
 
   const fetchDrinkCategories = (category) => {
@@ -14,20 +15,28 @@ function DrinkCategoryButtons() {
       .catch((error) => console.log(error));
   };
 
-  const handleClick = (category) => fetchDrinkCategories(category);
+  const fetchDrinkRecipes = () => {
+    const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    fetch(endpoint)
+      .then((response) => response.json()
+        .then((results) => setDrinks(results.drinks)));
+  };
+
+  const handleClick = (category) => {
+    if (toggleFood === false) {
+      fetchDrinkCategories(category);
+      setToggleFood(true);
+    } else {
+      setToggleFood(false);
+      fetchDrinkRecipes();
+    }
+  };
 
   const fetchDrinkCategoryName = () => {
     const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
     fetch(endpoint)
       .then((response) => response.json()
         .then((results) => setDrinkCategoryName(results.drinks)));
-  };
-
-  const fetchDrinkRecipes = () => {
-    const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-    fetch(endpoint)
-      .then((response) => response.json()
-        .then((results) => setDrinks(results.drinks)));
   };
 
   useEffect(() => {
