22baa394cb325acdde72e7b806bd001c12ea9190
Holistic <paulofreire.si@outlook.com>
2021-07-12 14:27:57 -0300

    tela explorar

--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -3,10 +3,21 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExploreFoods() {
+  const exploreObj = {
+    "by-ingredient": "Por Ingredientes",
+    "by-area": "Por Local de Origem",
+    surprise: "Me Surpreenda!",
+  };
+  const exploreBtn = (term, word) => (
+    <button type="button" data-testid={`explore-${term}`}>{word}</button>
+  )
   return (
     <>
       <div>Tela de explorar comidas</div>
       <Header title="Explorar Comidas" />
+      {Object.entries(exploreObj).map((key) => (
+        exploreBtn(key[0], key[0])
+      ))}
       <Footer />
     </>
   );
