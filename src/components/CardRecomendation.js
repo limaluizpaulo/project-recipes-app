396f0d6ddf4986ca8186f63a6077b2447874f6bf
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-07-12 16:39:52 -0300

    New card: Recomendations

---
+++ src/components/CardRecomendation.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function Card({ mealOrDrink, index, testId }) {
+  const { path } = useRouteMatch();
+  const [imgSrcKey, titleKey] = path.includes('comidas')
+    ? [mealOrDrink.strDrinkThumb, mealOrDrink.strDrink]
+    : [mealOrDrink.strMealThumb, mealOrDrink.strMeal];
+  // const imgSrcKey = path.includes('comidas') ? mealOrDrink.strMealThumb : mealOrDrink.strDrinkThumb;
+  // const titleKey = path.includes('comidas') ? mealOrDrink.strMeal : mealOrDrink.strDrink;
+
+  return (
+    <section data-testid={ `${index}-${testId}-card` }>
+      <img
+        width="150px"
+        data-testid={ `${index}-card-img` }
+        src={ imgSrcKey }
+        alt={ titleKey }
+      />
+      <h3
+        data-testid={ (testId === 'recipe') ? `${index}-card-name`
+          : `${index}-recomendation-title` }
+      >
+        {titleKey}
+      </h3>
+    </section>
+  );
+}
+
+Card.propTypes = {
+  mealOrDrink: PropTypes.shape(Object).isRequired,
+  index: PropTypes.number.isRequired,
+  testId: PropTypes.string.isRequired,
+};
