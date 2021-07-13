13e841f3a02c8c32d99add3507b721f91ff662f1
isTorres <ismaeltorres612@gmail.com>
2021-07-13 15:44:18 -0300

    add filter and render done recipes

--- src/components/FilterDoneRecipes.js
+++ src/components/FilterDoneRecipes.js
@@ -1,35 +1,110 @@
 import Button from 'react-bootstrap/Button';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router';
 import '../styles/card.css';
+import ShareBtn from './ShareBtn';
 
 export default function FilterDoneRecipes() {
-  const dale = JSON.parse(localStorage.getItem('doneRecipes'));
-  console.log(dale);
-  // function DoneRecipes() {
-  //   return console.log('id-recipe', dale);
-  // }
+  const storage = JSON.parse(localStorage.getItem('doneRecipes'));
+  const { push } = useHistory();
+  const [doneRecipes, setDoneRecipes] = useState([]);
+  const [fil, setFil] = useState('');
+
+  useEffect(() => {
+    setDoneRecipes(storage);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function filter(param = '') {
+    if (param === '') {
+      return storage;
+    }
+    const result = doneRecipes.filter((rec) => rec.type === param);
+    return result;
+  }
+
+  function disable() {
+    if (storage.lentgh === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  function clickCard(type, id) {
+    console.log(type);
+    if (type === 'comida') {
+      push(`/comidas/${id}`);
+    } else {
+      push(`/bebidas/${id}`);
+    }
+  }
 
   return (
     <div>
       <div>
-        <Button data-testid="filter-by-all-btn">
+        <Button
+          disabled={ disable }
+          data-testid="filter-by-all-btn"
+          onClick={ () => setFil('') }
+        >
           All
         </Button>
-        <Button data-testid="filter-by-food-btn">
+        <Button
+          disabled={ disable }
+          data-testid="filter-by-food-btn"
+          onClick={ () => setFil('comida') }
+        >
           Food
         </Button>
-        <Button data-testid="filter-by-drink-btn">
+        <Button
+          disabled={ disable }
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setFil('bebida') }
+        >
           Drinks
         </Button>
       </div>
       <div className="card-container">
-        { dale.length <= 1 ? <p>Não há receitas feitas</p>
-          : dale.slice(1).map((el) => (
-            <button type="button" className="recipe-card" key={ el.id }>
-              <img className="card-img" src={ el.image } alt={ el.name } />
-              <span>{ `${el.area} - ${el.category}` }</span>
-              <h3>{ el.name }</h3>
-            </button>
+        { filter(fil) === null ? <p>Não há receitas feitas</p>
+          : filter(fil).map((el, i) => (
+            <div className="recipe-card" key={ el.id }>
+              <button
+                type="button"
+                onClick={ () => { clickCard(el.type, el.id); } }
+              >
+                <img
+                  data-testid={ `${i}-horizontal-image` }
+                  className="card-img"
+                  src={ el.image }
+                  alt={ el.name }
+                />
+              </button>
+              <ShareBtn type={ el.type } id={ el.id } index={ i } />
+              <span data-testid={ `${i}-horizontal-top-text` }>
+                { `${el.area ? `${el.area} -` : ''} ${el.category}` }
+                {' '}
+                { el.alcoholicOrNot }
+              </span>
+              <p data-testid={ `${i}-horizontal-done-date` }>
+                Feita em:
+                { el.doneDate }
+              </p>
+
+              <button
+                type="button"
+                onClick={ () => { clickCard(el.type, el.id); } }
+                data-testid={ `${i}-horizontal-name` }
+              >
+                { el.name }
+              </button>
+              <div>
+                { el.tags !== null ? el.tags.map((tag) => (
+                  <span data-testid={ `${i}-${tag}-horizontal-tag` } key={ tag }>
+                    { tag }
+                  </span>
+                )) : null }
+              </div>
+            </div>
           ))}
       </div>
     </div>
