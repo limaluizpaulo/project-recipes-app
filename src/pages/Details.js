d6e6af570d873d2bbbea47fc618a35b74374bb97
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-07-13 15:32:13 -0300

    Retira alta complexidade

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -14,6 +14,26 @@ import { getDataById, getRandomData } from '../services/apiRequest';
 
 const SIX = 6;
 
+function handleIngredientsData(Lista) {
+  const ingredientFormated = Lista.map((el, i, arr) => (
+    (el[0].includes('Ingredient')) && ([`${el[1]
+      + arr.filter((elt) => elt[0] === (`strMeasure${i + 1}`))
+        .map((result) => (` - ${result[1]}`))}`,
+    ]))).filter((fil) => fil);
+  return ingredientFormated;
+}
+
+function handleRecipeInProgress(history, path, id) {
+  history.push(`${path.replace(':id', `${id}`)}/in-progress`);
+}
+
+function handleFavorite(state, setState, path, content) {
+  setState((prevState) => ({ status: !state.status,
+    imagem: prevState.imagem === whiteHeartIcon ? blackHeartIcon : whiteHeartIcon }));
+  saveFavoriteRecipe(path, content);
+  // saveFavoritRecipes(path, singleContent[0], favorit.status);
+}
+
 export default function Details() {
   const history = useHistory();
   const { path } = useRouteMatch();
@@ -46,109 +66,104 @@ export default function Details() {
     getRecipeDetails();
   }, [id, domain, firstKey, recDomain, recFirstKey]);
 
-  function handleIngredientsData() {
-    const ingredientFormated = ingredientsList.map((el, i, arr) => (
-      (el[0].includes('Ingredient')) && ([`${el[1]
-        + arr.filter((elt) => elt[0] === (`strMeasure${i + 1}`))
-          .map((result) => (` - ${result[1]}`))}`,
-      ]))).filter((fil) => fil);
-    return ingredientFormated;
-  }
-
-  function handleFavorite() {
-    setFavorit({ status: !favorit.status,
-      imagem: favorit.imagem === whiteHeartIcon ? blackHeartIcon : whiteHeartIcon });
-    saveFavoriteRecipe(path, singleContent[0]);
-    // saveFavoritRecipes(path, singleContent[0], favorit.status);
-  }
-
-  function handleRecipeInProgress() {
-    history.push(`${path.replace(':id', `${id}`)}/in-progress`);
-  }
+  useEffect(() => {
+    function getInitialFavorits() {
+      if (localStorage.getItem('favoriteRecipes')) {
+        const arrayFavorit = JSON.parse(localStorage.getItem('favoriteRecipes'));
+        setFavorit({
+          status: arrayFavorit.find((el) => el.id === id),
+          imagem: arrayFavorit.find((el) => el.id === id)
+            ? blackHeartIcon : whiteHeartIcon });
+      }
+    }
+    getInitialFavorits();
+  }, [id]);
 
   if (!singleContent[0]) return <h1>Loading...</h1>;
-  if (singleContent[0] && recomendations) {
-    const { strAlcoholic, strCategory, strInstructions, strYoutube } = singleContent[0];
-    const { imagem } = favorit;
-    return (
-      <>
-        <img
-          data-testid="recipe-photo"
-          src={ singleContent[0][imgSrc] }
-          alt={ singleContent[0][title] }
-          width="200px"
-        />
-        <div className="recipe-heading-container">
-          <div className="info-heading">
-            <h3 data-testid="recipe-title">{singleContent[0][title]}</h3>
-            <p data-testid="recipe-category">
-              {
-                firstKey === 'drinks'
-                  ? strAlcoholic
-                  : strCategory
-              }
+  return (
+    <>
+      <img
+        data-testid="recipe-photo"
+        src={ singleContent[0][imgSrc] }
+        alt={ singleContent[0][title] }
+        width="200px"
+      />
+      <div className="recipe-heading-container">
+        <div className="info-heading">
+          <h3 data-testid="recipe-title">{singleContent[0][title]}</h3>
+          <p data-testid="recipe-category">
+            {
+              firstKey === 'drinks'
+                ? singleContent[0].strAlcoholic
+                : singleContent[0].strCategory
+            }
+          </p>
+        </div>
+        <Button>
+          <img data-testid="share-btn" src={ shareIcon } alt="Favorit" />
+        </Button>
+        <Button
+          onClick={ () => handleFavorite(favorit, setFavorit, path, singleContent[0]) }
+        >
+          <img
+            data-testid="favorite-btn"
+            src={ favorit.imagem }
+            alt=""
+          />
+        </Button>
+      </div>
+      <div className="ingredients-container">
+        <h4>
+          Ingredients
+        </h4>
+        {handleIngredientsData(ingredientsList).map((string, i) => (
+          <div key={ i }>
+            <p data-testid={ `${i}-ingredient-name-and-measure` }>
+              { string }
             </p>
           </div>
-          <Button>
-            <img data-testid="share-btn" src={ shareIcon } alt="" />
-          </Button>
-          <Button onClick={ handleFavorite }>
-            <img data-testid="favorite-btn" src={ imagem } alt="" />
-          </Button>
-        </div>
-        <div className="ingredients-container">
-          <h4>
-            Ingredients
-          </h4>
-          {handleIngredientsData().map((string, i) => (
-            <div key={ i }>
-              <p data-testid={ `${i}-ingredient-name-and-measure` }>
-                { string }
-              </p>
-            </div>
-          ))}
-        </div>
-        <div className="instructions-video-container">
-          <p data-testid="instructions">
-            {strInstructions}
-          </p>
-          { path.includes('/comidas') && (
-            <VideoPlayer
-              testID="video"
-              videoLink={ strYoutube }
-              recipeTitle={ singleContent[0][title] }
+        ))}
+      </div>
+      <div className="instructions-video-container">
+        <p data-testid="instructions">
+          {singleContent[0].strInstructions}
+        </p>
+        { path.includes('/comidas') && (
+          <VideoPlayer
+            testID="video"
+            videoLink={ singleContent[0].strYoutube }
+            recipeTitle={ singleContent[0][title] }
+          />
+        ) }
+      </div>
+      <Carousel>
+        { recomendations.map((item, i) => (
+          <Carousel.Item
+            className="recomendations-container"
+            key={ i }
+          >
+            <Card
+              mealOrDrink={ item }
+              index={ i }
+              testId="recomendation"
             />
-          ) }
-        </div>
-        <Carousel>
-          { recomendations.map((item, i) => (
-            <Carousel.Item
-              className="recomendations-container"
-              key={ i }
-            >
-              <Card
-                mealOrDrink={ item }
-                index={ i }
-                testId="recomendation"
-              />
 
-              {/* <CardRecomendation
+            {/* <CardRecomendation
                 mealOrDrink={ array[i + 1] || array[0] }
                 index={ i + 1 }
                 testId="recomendation"
               /> */}
-            </Carousel.Item>
-          ))}
-        </Carousel>
+          </Carousel.Item>
+        ))}
+      </Carousel>
 
-        <Button
-          className="start-recipe-btn"
-          onClick={ handleRecipeInProgress }
-          data-testid="start-recipe-btn"
-        >
-          Iniciar Receita
-        </Button>
-      </>
-    );
-  }
+      <Button
+        className="start-recipe-btn"
+        onClick={ () => handleRecipeInProgress(history, path, id) }
+        data-testid="start-recipe-btn"
+      >
+        Iniciar Receita
+      </Button>
+    </>
+  );
 }
