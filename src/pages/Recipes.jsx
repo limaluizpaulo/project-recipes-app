a27a02f4700999bc443294eaa0261496abd6b654
Jessie <miss_sc_jessi@hotmail.com>
2021-07-14 17:05:01 -0300

    fix bugs message error

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -64,7 +64,7 @@ class Recipes extends Component {
     if (listRecipes.length === 1) {
       return <Redirect to={ `/comidas/${listRecipes[0].idMeal}` } />;
     }
-    // mudança2
+    // mudança3
     return (
       <>
         <Header header="Comidas" explorer />
