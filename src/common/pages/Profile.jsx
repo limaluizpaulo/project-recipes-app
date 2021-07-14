70d05e995ad56af636d28cf6bc5050d99bdddbb0
Adão Benites <adaobjr@gmail.com>
2021-07-14 16:15:10 -0400

    adjusting Profile Page

--- src/common/pages/Profile.jsx
+++ src/common/pages/Profile.jsx
@@ -1,20 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaCamera, FaFacebookF, FaTwitter, FaGoogle } from 'react-icons/fa';
+
+import { getStorage } from '../../functions';
+// import profileImg from '../../images/profile.jpg';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 
 export default function Profile() {
-  return (
-    <div>
-      <Header pageName="Perfil" />
+  const [showImg, setShowImg] = useState(false);
+  const [{ email }] = useState(() => getStorage('user'));
+
+  const renderBtnsProfile = () => (
+    <div className="buttonsInProfile">
+      <Link to="/receitas-feitas">
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          className="buttonsRecipes"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
       <Link to="/receitas-favoritas">
         <button
-          type="submit"
+          data-testid="profile-favorite-btn"
+          type="button"
+          className="buttonsRecipes"
         >
           Receitas Favoritas
         </button>
       </Link>
-      <Footer />
+      <Link to="/">
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          className="buttonExit"
+          onClick={ () => localStorage.clear() }
+        >
+          Sair
+        </button>
+      </Link>
     </div>
+  );
+
+  return (
+    <main>
+      <Header pageName="Perfil" />
+      <div className={ (showImg) ? 'bigImgProfile showImage' : 'bigImgProfile' }>
+        <img src={ profileImg } alt="img-profile" />
+        <button
+          type="button"
+          className="close"
+          onClick={ () => setShowImg(!showImg) }
+        >
+          <div />
+        </button>
+      </div>
+      <div className="container">
+        <div className="card">
+          <div className="mainContentProfile">
+            <div className="imageProfile">
+              <button
+                type="button"
+                className="bgCamera"
+                onClick={ () => setShowImg(!showImg) }
+              >
+                {/* eslint-disable-next-line react/jsx-max-depth */}
+                <FaCamera className="cameraIcon" />
+              </button>
+            </div>
+            <h3 className="userName">{ `@${email.split('@')[0]}` }</h3>
+            <h3 data-testid="profile-email" className="email">{ email }</h3>
+            <div className="loginSocial">
+              {/* eslint-disable-next-line react/jsx-max-depth */}
+              <FaFacebookF className="loginSocial-icon" />
+              {/* eslint-disable-next-line react/jsx-max-depth */}
+              <FaTwitter className="loginSocial-icon" />
+              {/* eslint-disable-next-line react/jsx-max-depth */}
+              <FaGoogle className="loginSocial-icon" />
+            </div>
+          </div>
+          {renderBtnsProfile()}
+        </div>
+      </div>
+      <Footer />
+    </main>
   );
 }
