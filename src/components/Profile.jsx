8a8a2208238c78837fc558270ec51a040b7ba873
Felippe <felippecorrea2010@gmail.com>
2021-07-14 19:16:04 -0300

    Estilizaçao 70% da pagina de perfil

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,38 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 export default function Profile() {
   const user = JSON.parse(localStorage.getItem('user'));
   const email = user ? user.email : 'a@a.com';
   return (
-    <div>
-      <span data-testid="profile-email">{ email }</span>
+    <div className="profile">
+      <h4 className="headingProfile" data-testid="profile-email">{ email }</h4>
       <Link to="/receitas-feitas">
-        <button
+        <Button
+          variant="outline-danger"
+          size="lg"
+          className="buttonsProfile"
           type="button"
           data-testid="profile-done-btn"
         >
           Receitas Feitas
 
-        </button>
+        </Button>
       </Link>
       <Link to="/receitas-favoritas">
-        <button
+        <Button
+          variant="outline-danger"
+          size="lg"
+          className="buttonsProfile"
           type="button"
           data-testid="profile-favorite-btn"
         >
           Receitas Favoritas
-        </button>
+        </Button>
       </Link>
       <Link to="/">
-        <button
+        <Button
+          variant="outline-danger"
+          size="lg"
+          className="buttonsProfile"
           type="button"
           data-testid="profile-logout-btn"
           onClick={ () => localStorage.clear() }
         >
           Sair
 
-        </button>
+        </Button>
       </Link>
     </div>
   );
