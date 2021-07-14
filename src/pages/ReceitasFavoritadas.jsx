a064c449943e682453b52ff17e7e3663f496e149
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-07-14 00:20:58 -0300

    add css to favourites

--- src/pages/ReceitasFavoritadas.jsx
+++ src/pages/ReceitasFavoritadas.jsx
@@ -3,6 +3,9 @@ import { Link, useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
+
+import '../css/receitasFavoritas.css';
+
 // import Header from '../components/Header';
 
 const copy = require('clipboard-copy');
@@ -78,79 +81,84 @@ function ReceitasFavoritadas() {
             to={ item.type === 'comida'
               ? `/comidas/${item.id}` : `/bebidas/${item.id}` }
           >
-            <img
-              width="50 px"
-              alt="recipe-img"
-              data-testid={ `${index}-horizontal-image` }
-              src={ item.image }
-            />
-            <p data-testid={ `${index}-horizontal-name` }>{ item.name }</p>
+            <div className="recipes">
+              <img
+                width="50 px"
+                alt="recipe-img"
+                data-testid={ `${index}-horizontal-image` }
+                src={ item.image }
+              />
+              <p data-testid={ `${index}-horizontal-name` }>{ item.name }</p>
+            </div>
           </Link>
           <p data-testid={ `${index}-horizontal-top-text` }>{item.alcoholicOrNot}</p>
           <p data-testid={ `${index}-horizontal-top-text` }>
             {item.area}
-            {' '}
             -
-            {' '}
             {item.category}
           </p>
-          <input
-            type="image"
-            src={ shareIcon }
-            alt="share"
-            data-testid={ `${index}-horizontal-share-btn` }
-            onClick={ () => setShareCopy([true, index]) || copy(`http://localhost:3000/${item.type}s/${item.id}`) }
-          />
-          {shareCopy ? copyLink(index, shareCopy[1]) : null}
-          <input
-            type="image"
-            src={ blackHeartIcon }
-            alt="share"
-            data-testid={ `${index}-horizontal-favorite-btn` }
-            onClick={ () => retiraFav(item.id) }
-          />
+          <div className="input-btn">
+            <input
+              type="image"
+              src={ shareIcon }
+              alt="share"
+              data-testid={ `${index}-horizontal-share-btn` }
+              onClick={ () => setShareCopy([true, index]) || copy(`http://localhost:3000/${item.type}s/${item.id}`) }
+            />
+            {shareCopy ? copyLink(index, shareCopy[1]) : null}
+            <input
+              type="image"
+              src={ blackHeartIcon }
+              alt="share"
+              data-testid={ `${index}-horizontal-favorite-btn` }
+              onClick={ () => retiraFav(item.id) }
+            />
+          </div>
         </li>))
     );
   };
 
   return (
     <>
-      <Link to="/perfil">
-        <input
-          type="image"
-          alt="profile icon"
-          src={ profileIcon }
-          data-testid="profile-top-btn"
-        />
-      </Link>
-      <p data-testid="page-title">Receitas Favoritas</p>
+      <div className="header_bar favourite-bar">
+        <Link to="/perfil">
+          <input
+            type="image"
+            alt="profile icon"
+            src={ profileIcon }
+            data-testid="profile-top-btn"
+          />
+        </Link>
+        <p data-testid="page-title">Receitas Favoritas</p>
+        <div className="buttons">
+          <button
+            label="All"
+            type="button"
+            data-testid="filter-by-all-btn"
+            onClick={ () => setEstado('') }
+          >
+            All
+          </button>
 
-      <button
-        label="All"
-        type="button"
-        data-testid="filter-by-all-btn"
-        onClick={ () => setEstado('') }
-      >
-        All
-      </button>
+          <button
+            label="Food"
+            type="button"
+            data-testid="filter-by-food-btn"
+            onClick={ () => setEstado('comida') }
+          >
+            Food
+          </button>
 
-      <button
-        label="Food"
-        type="button"
-        data-testid="filter-by-food-btn"
-        onClick={ () => setEstado('comida') }
-      >
-        Food
-      </button>
-
-      <button
-        label="Drinks"
-        type="button"
-        data-testid="filter-by-drink-btn"
-        onClick={ () => setEstado('bebida') }
-      >
-        Drinks
-      </button>
+          <button
+            label="Drinks"
+            type="button"
+            data-testid="filter-by-drink-btn"
+            onClick={ () => setEstado('bebida') }
+          >
+            Drinks
+          </button>
+        </div>
+      </div>
       <ol>
         {localGetFav && handleFilter()}
       </ol>
