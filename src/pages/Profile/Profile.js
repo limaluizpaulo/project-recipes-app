8b5442e3ef4e016df192d3b9f6dbf4c7edef7b81
bidaffonso <bidaffondo@gmail.com>
2021-07-14 17:44:21 -0400

    Requisitos 82, 83 e 84 concluidos

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -3,11 +3,30 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
 function Profile() {
+  const userInfos = JSON.parse(localStorage.user);
   return (
     <div id="page-drinks">
       <div>
         <Header title="Perfil" />
-        <h1>profile</h1>
+        <span data-testid="profile-email">{userInfos.email}</span>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Sair
+        </button>
         <Footer />
       </div>
     </div>
