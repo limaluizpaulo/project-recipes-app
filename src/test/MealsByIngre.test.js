921cf2977eff91d59d0bfd548a706a1dea5d6c44
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-07-14 16:01:19 -0300

    fazendo teste na branch errada masok

---
+++ src/test/MealsByIngre.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import MealsByIngre from '../pages/MealsByIngre';
+import renderWithRouter from './renderWithRouter';
+import mockMealsIngre from './mocks/mockMealsIngre';
+import { fetchIngreMeals, fetchMealsByName } from '../services/mealsApi';
+
+jest.mock('../services/mealsApi');
+
+describe('Teste página MealsByIngre', () => {
+  fetchIngreMeals.mockResolvedValue(mockMealsIngre);
+  fetchMealsByName.mockResolvedValue(mockMealsIngre);
+  it('Testa se o título esta correto', () => {
+    const { getByTestId } = renderWithRouter(<MealsByIngre />);
+    const titulo = getByTestId('page-title');
+    expect(titulo.textContent).toBe('Explorar Ingredientes');
+  });
+  it('Testa se todos os componentes existem na página', async () => {
+    const { findByTestId } = renderWithRouter(<MealsByIngre />);
+    const ingreCard0 = await findByTestId('0-ingredient-card');
+    const imgCard0 = await findByTestId('0-card-img');
+    const nameCard0 = await findByTestId('0-card-name');
+    expect(ingreCard0).toBeInTheDocument();
+    expect(imgCard0).toBeInTheDocument();
+    expect(nameCard0).toBeInTheDocument();
+
+    const ingreCard11 = await findByTestId('1-ingredient-card');
+    const imgCard11 = await findByTestId('1-card-img');
+    const nameCard11 = await findByTestId('1-card-name');
+    expect(ingreCard11).toBeInTheDocument();
+    expect(imgCard11).toBeInTheDocument();
+    expect(nameCard11).toBeInTheDocument();
+  });
+});
